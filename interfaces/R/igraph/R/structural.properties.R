e980198b6a239b97ac8966c44dd738098c24c739
Gabor Csardi <csardi@rmki.kfki.hu>
2006-06-28 03:52:30 +0000

    Structural holes / constraint implemented in R Patches applied:
     * csardi@szaffi/igraph--main--0.2--patch-157
       Structural holes / constraint implemented in R
    
    Thanks to Jeroen Bruggeman who actually did it.

--- interfaces/R/igraph/R/structural.properties.R
+++ interfaces/R/igraph/R/structural.properties.R
@@ -253,4 +253,36 @@ graph.laplacian <- function(graph, normalized=FALSE) {
   
   M
 }
+
+# Structural holes a'la Burt, code contributed by
+# Jeroen Bruggeman
+
+constraint <- function(graph, nodes=V(graph)) {
+
+  if (!is.igraph(graph)) {
+    stop("Not a graph object")
+  }
+
+  idx <- degree(graph) != 0
+  A <- get.adjacency(graph)
+  A <- A[idx, idx]
+  n <- sum(idx)
   
+  one <- c(rep(1,n))
+  CZ <- A + t(A)
+  cs <- CZ %*% one
+  ics <- 1/cs
+  CS <- ics %*% t(one)
+  P <- CZ * CS  #intermediate result: proportionate tie strengths
+  PSQ <- P%*%P #sum paths of length two
+  P.bi <- as.numeric(P>0)  #exclude paths to non-contacts (& reflexive):
+  PC <- (P + (PSQ*P.bi))^2  #dyadic constraint
+  ci <- PC %*% one      #overall constraint
+  dim(ci) <- NULL
+
+  ci2 <- numeric(vcount(graph))
+  ci2[idx] <- ci
+  ci2[!idx] <- NaN
+  ci2[nodes+1]
+}
+
