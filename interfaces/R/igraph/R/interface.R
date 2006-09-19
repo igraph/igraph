186b96866a0911818274a046575f8c1fe0085a1e
Gabor Csardi <csardi@rmki.kfki.hu>
2006-09-19 16:24:32 +0000

    R interface: attribute handling added to add.edges Patches applied:
     * csardi@szaffi/igraph--main--0.3--patch-14
       R interface: attribute handling added to add.edges

--- interfaces/R/igraph/R/interface.R
+++ interfaces/R/igraph/R/interface.R
@@ -29,12 +29,32 @@ graph.empty <- function(n=0, directed=TRUE) {
         PACKAGE="igraph")
 }
 
-add.edges <- function(graph, edges) {
+add.edges <- function(graph, edges, ...) {
   if (!is.igraph(graph)) {
     stop("Not a graph object")
   }
-  .Call("R_igraph_add_edges", graph, as.numeric(edges),
-        PACKAGE="igraph")
+
+  attrs <- list(...)
+  nam <- names(attrs)
+  if (length(attrs) != 0 && (is.null(nam) || any(nam==""))) {
+    stop("please supply names for attributes")
+  }
+  
+  edges.orig <- ecount(graph)  
+  graph <- .Call("R_igraph_add_edges", graph, as.numeric(edges),
+                 PACKAGE="igraph")
+  edges.new <- ecount(graph)
+  
+  if (edges.new-edges.orig != 0) {
+    idx <- seq(edges.orig+1, edges.new)
+  } else {
+    idx <- numeric()
+  }
+  for (i in seq(attrs)) {
+    graph[[9]][[4]][[nam[i]]][idx] <- attrs[[nam[i]]]
+  }  
+  
+  graph
 }
 
 add.vertices <- function(graph, nv) {
