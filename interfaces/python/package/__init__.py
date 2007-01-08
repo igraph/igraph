bc0b043f572623488643b0993eaf647d737c63b5
Tamas Nepusz <ntamas@rmki.kfki.hu>
2007-01-08 17:12:57 +0000

    Python documentation finally ready to be published online

--- interfaces/python/package/__init__.py
+++ interfaces/python/package/__init__.py
@@ -18,6 +18,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc.,  51 Franklin Street, Fifth Floor, Boston, MA 
 02110-1301 USA
 
+@undocumented: test
 """
 
 from core import *
