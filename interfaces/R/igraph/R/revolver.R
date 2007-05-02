e45a201ef33d682063c7fdad96dc5e380ba266be
Gabor Csardi <csardi@rmki.kfki.hu>
2007-05-02 09:56:26 +0000

    Evolver: 'air' model added Patches applied:
     * csardi@guzu/igraph--main--0.4--patch-134
       Evolver: 'air' model added

--- interfaces/R/igraph/R/revolver.R
+++ interfaces/R/igraph/R/revolver.R
@@ -214,3 +214,19 @@ evolver.ir <- function(graph, cats, window, niter=5,
         PACKAGE="igraph")
 }
 
+evolver.air <- function(graph, cats, window,
+                        niter=5, agebins=max(vcount(graph)/7100, 10),
+                        sd=FALSE, norm=FALSE, cites=FALSE, expected=FALSE,
+                        error=TRUE, debug=matrix(nc=2, nr=0),
+                        verbose=igraph.par("verbose")) {
+  if (!is.igraph(graph)) {
+    stop("Not a graph object!")
+  }
+
+  .Call("R_igraph_evolver_air", graph, as.numeric(cats), as.numeric(window),
+        as.numeric(niter),
+        as.numeric(agebins), as.logical(sd), as.logical(norm), as.logical(cites),
+        as.logical(expected), as.logical(error),
+        structure(as.numeric(debug), dim=dim(debug)), as.logical(verbose),
+        PACKAGE="igraph")
+}
