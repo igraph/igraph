a81f227727cd26efddfe406485a3af6a07884d7f
Tamas Nepusz <ntamas@rmki.kfki.hu>
2007-06-06 12:45:43 +0000

    Fixed a bug allowing users to mess with the internal attribute representation in the Python interface

--- interfaces/python/src/edgeseqobject.c
+++ interfaces/python/src/edgeseqobject.c
@@ -59,7 +59,7 @@ PyObject* igraphmodule_EdgeSeq_New(igraphmodule_GraphObject *g) {
  * other \c PyObject pointers and they might point back to itself.
  */
 int igraphmodule_EdgeSeq_traverse(igraphmodule_EdgeSeqObject *self,
-				  visitproc visit, void *arg) {
+                  visitproc visit, void *arg) {
   int vret;
 
   RC_TRAVERSE("EdgeSeq", self);
@@ -118,7 +118,7 @@ int igraphmodule_EdgeSeq_sq_length(igraphmodule_EdgeSeqObject* self) {
  * \brief Returns the item at the given index in the sequence
  */
 PyObject* igraphmodule_EdgeSeq_sq_item(igraphmodule_EdgeSeqObject* self,
-				       int i) {
+                       int i) {
   igraphmodule_GraphObject *o;
   igraph_t *g;
   
@@ -147,18 +147,44 @@ PyObject* igraphmodule_EdgeSeq_attributes(igraphmodule_EdgeSeqObject* self) {
 }
 
 /** \ingroup python_interface_edgeseq
+ * \brief Returns the count of attribute names
+ */
+PyObject* igraphmodule_EdgeSeq_attribute_count(igraphmodule_EdgeSeqObject* self) {
+  PyObject *list, *result;
+  long int size;
+  igraphmodule_GraphObject *o;
+  o=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
+  if (!o) return NULL;
+
+  list=igraphmodule_Graph_edge_attributes(o);
+  size=PySequence_Size(list);
+  Py_DECREF(list);
+
+  return Py_BuildValue("i", size);
+}
+
+/** \ingroup python_interface_edgeseq
  * \brief Returns the list of values for a given attribute
  */
 PyObject* igraphmodule_EdgeSeq_get_attribute_values(igraphmodule_EdgeSeqObject* self, PyObject* o) {
   igraphmodule_GraphObject *gr;
-  PyObject* result;
-  
+  PyObject *result, *values, *item;
+
   gr=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
   if (!gr) return NULL;
   
-  result=PyDict_GetItem(((PyObject**)gr->g.attr)[2], o);
-  if (result) {
-    Py_INCREF(result);
+  values=PyDict_GetItem(((PyObject**)gr->g.attr)[ATTRHASH_IDX_EDGE], o);
+  if (values) {
+    long int i, n;
+    n = PyList_Size(values);
+    result = PyList_New(n);
+    if (!result) return result;
+
+    for (i=0; i<n; i++) {
+        item = PyList_GET_ITEM(values, i);
+        Py_INCREF(item);
+        PyList_SET_ITEM(result, i, item);
+    }
     return result;
   }
   
@@ -167,31 +193,77 @@ PyObject* igraphmodule_EdgeSeq_get_attribute_values(igraphmodule_EdgeSeqObject* 
   return NULL;
 }
 
+PyObject* igraphmodule_EdgeSeq_get_attribute_values_mapping(igraphmodule_EdgeSeqObject *self, PyObject *o) {
+  /* Handle integer indices according to the sequence protocol */
+  if (PyInt_Check(o)) return igraphmodule_EdgeSeq_sq_item(self, PyInt_AsLong(o));
+  return igraphmodule_EdgeSeq_get_attribute_values(self, o);
+}
+
 /** \ingroup python_interface_edgeseq
  * \brief Sets the list of values for a given attribute
  */
-PyObject* igraphmodule_EdgeSeq_set_attribute_values(igraphmodule_EdgeSeqObject* self, PyObject* args, PyObject* kwds) {
+int igraphmodule_EdgeSeq_set_attribute_values_mapping(igraphmodule_EdgeSeqObject* self, PyObject* attrname, PyObject* values) {
+  PyObject *dict, *list, *item;
   igraphmodule_GraphObject *gr;
-  PyObject *attrname, *values;
-  long n;
-  static char* kwlist[] = { "attrname", "values", NULL };
-
-  if (!PyArg_ParseTupleAndKeywords(args, kwds, "OO", kwlist,
-				   &attrname, &values))
-    return NULL;
-
+  long i, n;
+  
   gr=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
-  if (!gr) return NULL;
+  if (!gr) return -1;
 
   n=PySequence_Size(values);
-  if (n<0) return NULL;
+  if (n<0) return -1;
   if (n != (long)igraph_ecount(&gr->g)) {
     PyErr_SetString(PyExc_ValueError, "value list length must be equal to the number of edges in the graph");
-    return NULL;
+    return -1;
   }
 
-  if (PyDict_SetItem(((PyObject**)gr->g.attr)[ATTRHASH_IDX_EDGE],
-		     attrname, values) == -1)
+  /* Check if we already have attributes with the given name */
+  dict = ((PyObject**)gr->g.attr)[ATTRHASH_IDX_EDGE];
+  list = PyDict_GetItem(dict, attrname);
+  if (list != 0) {
+    /* Yes, we have. Modify its items to the items found in values */
+    for (i=0; i<n; i++) {
+      item = PyList_GetItem(values, i);
+      if (item == 0) return -1;
+      Py_INCREF(item);
+      if (PyList_SetItem(list, i, item)) {
+        Py_DECREF(item);
+        return -1;
+      } /* PyList_SetItem stole a reference to the item automatically */ 
+    }
+  } else {
+    /* We don't have attributes with the given name yet. Create an entry
+     * in the dict, create a new list and copy everything */
+    list = PyList_New(n);
+    if (list == 0) return -1;
+    for (i=0; i<n; i++) {
+      item = PyList_GetItem(values, i);
+      if (item == 0) {
+        Py_DECREF(list);
+        return -1;
+      }
+      Py_INCREF(item);
+      PyList_SET_ITEM(list, i, item);
+    }
+    if (PyDict_SetItem(dict, attrname, list)) {
+      Py_DECREF(list);
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+PyObject* igraphmodule_EdgeSeq_set_attribute_values(igraphmodule_EdgeSeqObject *self,
+    PyObject *args, PyObject *kwds) {
+  static char* kwlist[] = { "attrname", "values", NULL };
+  PyObject *attrname, *values;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "OO", kwlist,
+                   &attrname, &values))
+    return NULL;
+
+  if (igraphmodule_EdgeSeq_set_attribute_values_mapping(self, attrname, values))
     return NULL;
 
   Py_RETURN_NONE;
@@ -242,33 +314,47 @@ static PySequenceMethods igraphmodule_EdgeSeq_as_sequence = {
   0,                                          /* sq_inplace_repeat */
 };
 
+/**
+ * \ingroup python_interface_edgeseq
+ * This is the collection of functions necessary to implement the
+ * edge sequence as a mapping (which maps attribute names to values)
+ */
+static PyMappingMethods igraphmodule_EdgeSeq_as_mapping = {
+    /* returns the number of edge attributes */
+    (inquiry) igraphmodule_EdgeSeq_attribute_count,
+    /* returns the values of an attribute by name */
+    (binaryfunc) igraphmodule_EdgeSeq_get_attribute_values_mapping,
+    /* sets the values of an attribute by name */
+    (objobjargproc) igraphmodule_EdgeSeq_set_attribute_values_mapping,
+};
+
 /** \ingroup python_interface_edgeseq
  * Python type object referencing the methods Python calls when it performs various operations on
  * an edge sequence of a graph
  */
 PyTypeObject igraphmodule_EdgeSeqType =
 {
-  PyObject_HEAD_INIT(NULL)                  //
-  0,                                        // ob_size
-  "igraph.EdgeSeq",                         // tp_name
-  sizeof(igraphmodule_EdgeSeqObject),       // tp_basicsize
-  0,                                        // tp_itemsize
-  (destructor)igraphmodule_EdgeSeq_dealloc, // tp_dealloc
-  0,                                        // tp_print
-  0,                                        // tp_getattr
-  0,                                        // tp_setattr
-  0,                                        // tp_compare
-  0,                                        // tp_repr
-  0,                                        // tp_as_number
-  &igraphmodule_EdgeSeq_as_sequence,        // tp_as_sequence
-  0,                                        // tp_as_mapping
-  0,                                        // tp_hash
-  0,                                        // tp_call
-  0,                                        // tp_str
-  0,                                        // tp_getattro
-  0,                                        // tp_setattro
-  0,                                        // tp_as_buffer
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, // tp_flags
+  PyObject_HEAD_INIT(NULL)                  /* */
+  0,                                        /* ob_size */
+  "igraph.EdgeSeq",                         /* tp_name */
+  sizeof(igraphmodule_EdgeSeqObject),       /* tp_basicsize */
+  0,                                        /* tp_itemsize */
+  (destructor)igraphmodule_EdgeSeq_dealloc, /* tp_dealloc */
+  0,                                        /* tp_print */
+  0,                                        /* tp_getattr */
+  0,                                        /* tp_setattr */
+  0,                                        /* tp_compare */
+  0,                                        /* tp_repr */
+  0,                                        /* tp_as_number */
+  &igraphmodule_EdgeSeq_as_sequence,        /* tp_as_sequence */
+  &igraphmodule_EdgeSeq_as_mapping,         /* tp_as_mapping */
+  0,                                        /* tp_hash */
+  0,                                        /* tp_call */
+  0,                                        /* tp_str */
+  0,                                        /* tp_getattro */
+  0,                                        /* tp_setattro */
+  0,                                        /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, /* tp_flags */
   "Class representing the edge set of a graph.\n\n"
   "The individual edges can be accessed by indexing the edge sequence\n"
   "object. It can be used as an iterable as well, or even in a list\n"
@@ -281,13 +367,25 @@ PyTypeObject igraphmodule_EdgeSeqType =
   "  (0, 2)\n"
   "  (1, 2)\n"
   "  >>> [max(e.tuple) for e in g.es]\n"
-  "  [1, 2, 2]\n", // tp_doc
-  0,                                          // tp_traverse
-  0,                                          // tp_clear
-  0,                                          // tp_richcompare
-  0,                                          // tp_weaklistoffset
-  0,                                          // tp_iter
-  0,                                          // tp_iternext
-  igraphmodule_EdgeSeq_methods,             // tp_methods
+  "  [1, 2, 2]\n\n"
+  "The edge set can also be used as a dictionary where the keys are the\n"
+  "attribute names. The values corresponding to the keys are the values\n"
+  "of the given attribute of every edge in the graph:\n\n"
+  "  >>> g=igraph.Graph.Full(3)\n"
+  "  >>> for idx, e in enumerate(g.es):\n"
+  "  ...   e[\"weight\"] = idx*(idx+1)\n"
+  "  ...\n"
+  "  >>> g.es[\"weight\"]\n"
+  "  [0, 2, 6]\n"
+  "  >>> g.es[\"weight\"] = range(3)\n"
+  "  >>> g.es[\"weight\"]\n"
+  "  [0, 1, 2]\n", /* tp_doc */
+  0,                                          /* tp_traverse */
+  0,                                          /* tp_clear */
+  0,                                          /* tp_richcompare */
+  0,                                          /* tp_weaklistoffset */
+  0,                                          /* tp_iter */
+  0,                                          /* tp_iternext */
+  igraphmodule_EdgeSeq_methods,             /* tp_methods */
 };
 
--- interfaces/python/src/vertexseqobject.c
+++ interfaces/python/src/vertexseqobject.c
@@ -59,7 +59,7 @@ PyObject* igraphmodule_VertexSeq_New(igraphmodule_GraphObject *g) {
  * other \c PyObject pointers and they might point back to itself.
  */
 int igraphmodule_VertexSeq_traverse(igraphmodule_VertexSeqObject *self,
-				    visitproc visit, void *arg) {
+                    visitproc visit, void *arg) {
   int vret;
 
   RC_TRAVERSE("VertexSeq", self);
@@ -118,7 +118,7 @@ int igraphmodule_VertexSeq_sq_length(igraphmodule_VertexSeqObject* self) {
  * \brief Returns the item at the given index in the sequence
  */
 PyObject* igraphmodule_VertexSeq_sq_item(igraphmodule_VertexSeqObject* self,
-					 int i) {
+                     int i) {
   igraphmodule_GraphObject *o;
   igraph_t *g;
   
@@ -134,7 +134,7 @@ PyObject* igraphmodule_VertexSeq_sq_item(igraphmodule_VertexSeqObject* self,
   return igraphmodule_Vertex_New(self->gref, i);
 }
 
-/** \ingroup python_interface_vertex
+/** \ingroup python_interface_vertexseq
  * \brief Returns the list of attribute names
  */
 PyObject* igraphmodule_VertexSeq_attributes(igraphmodule_VertexSeqObject* self) {
@@ -146,19 +146,45 @@ PyObject* igraphmodule_VertexSeq_attributes(igraphmodule_VertexSeqObject* self) 
   return igraphmodule_Graph_vertex_attributes(o);
 }
 
-/** \ingroup python_interface_edgeseq
+/** \ingroup python_interface_vertexseq
+ * \brief Returns the count of attribute names
+ */
+PyObject* igraphmodule_VertexSeq_attribute_count(igraphmodule_VertexSeqObject* self) {
+  PyObject *list, *result;
+    long int size;
+    igraphmodule_GraphObject *o;
+  o=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
+  if (!o) return NULL;
+
+  list=igraphmodule_Graph_vertex_attributes(o);
+  size=PySequence_Size(list);
+  Py_DECREF(list);
+
+  return Py_BuildValue("i", size);
+}
+
+/** \ingroup python_interface_vertexseq
  * \brief Returns the list of values for a given attribute
  */
 PyObject* igraphmodule_VertexSeq_get_attribute_values(igraphmodule_VertexSeqObject* self, PyObject* o) {
   igraphmodule_GraphObject *gr;
-  PyObject* result;
+  PyObject *result, *values, *item;
   
   gr=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
   if (!gr) return NULL;
   
-  result=PyDict_GetItem(((PyObject**)gr->g.attr)[ATTRHASH_IDX_VERTEX], o);
-  if (result) {
-    Py_INCREF(result);
+  values=PyDict_GetItem(((PyObject**)gr->g.attr)[ATTRHASH_IDX_VERTEX], o);
+  if (values) {
+    long int i, n;
+    n = PyList_Size(values);
+    result = PyList_New(n);
+    if (!result) return result;
+
+    for (i=0; i<n; i++) {
+        item = PyList_GET_ITEM(values, i);
+        Py_INCREF(item);
+        PyList_SET_ITEM(result, i, item);
+    }
     return result;
   }
   
@@ -167,32 +193,77 @@ PyObject* igraphmodule_VertexSeq_get_attribute_values(igraphmodule_VertexSeqObje
   return NULL;
 }
 
+PyObject* igraphmodule_VertexSeq_get_attribute_values_mapping(igraphmodule_VertexSeqObject *self, PyObject *o) {
+  /* Handle integer indices according to the sequence protocol */
+  if (PyInt_Check(o)) return igraphmodule_VertexSeq_sq_item(self, PyInt_AsLong(o));
+  return igraphmodule_VertexSeq_get_attribute_values(self, o);
+}
+
 /** \ingroup python_interface_vertexseq
  * \brief Sets the list of values for a given attribute
  */
-PyObject* igraphmodule_VertexSeq_set_attribute_values(igraphmodule_VertexSeqObject* self, PyObject* args, PyObject* kwds) {
+int igraphmodule_VertexSeq_set_attribute_values_mapping(igraphmodule_VertexSeqObject* self, PyObject* attrname, PyObject* values) {
+  PyObject *dict, *list, *item;
   igraphmodule_GraphObject *gr;
-  PyObject *attrname, *values;
-  long n;
+  long i, n;
+
+  gr=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
+  if (!gr) return -1;
+
+  n=PySequence_Size(values);
+  if (n<0) return -1;
+  if (n != (long)igraph_vcount(&gr->g)) {
+    PyErr_SetString(PyExc_ValueError, "value list length must be equal to the number of vertices in the graph");
+    return -1;
+  }
+
+  /* Check if we already have attributes with the given name */
+  dict = ((PyObject**)gr->g.attr)[ATTRHASH_IDX_VERTEX];
+  list = PyDict_GetItem(dict, attrname);
+  if (list != 0) {
+    /* Yes, we have. Modify its items to the items found in values */
+    for (i=0; i<n; i++) {
+      item = PyList_GetItem(values, i);
+      if (item == 0) return -1;
+      Py_INCREF(item);
+      if (PyList_SetItem(list, i, item)) {
+        Py_DECREF(item);
+        return -1;
+      } /* PyList_SetItem stole a reference to the item automatically */ 
+    }
+  } else {
+    /* We don't have attributes with the given name yet. Create an entry
+     * in the dict, create a new list and copy everything */
+    list = PyList_New(n);
+    if (list == 0) return -1;
+    for (i=0; i<n; i++) {
+      item = PyList_GetItem(values, i);
+      if (item == 0) {
+        Py_DECREF(list);
+        return -1;
+      }
+      Py_INCREF(item);
+      PyList_SET_ITEM(list, i, item);
+    }
+    if (PyDict_SetItem(dict, attrname, list)) {
+      Py_DECREF(list);
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+PyObject* igraphmodule_VertexSeq_set_attribute_values(igraphmodule_VertexSeqObject *self, PyObject *args, PyObject *kwds) {
   static char* kwlist[] = { "attrname", "values", NULL };
+  PyObject *attrname, *values;
 
   if (!PyArg_ParseTupleAndKeywords(args, kwds, "OO", kwlist,
 				   &attrname, &values))
-    return NULL;
+	return NULL;
 
-  gr=(igraphmodule_GraphObject*)igraphmodule_resolve_graph_weakref(self->gref);
-  if (!gr) return NULL;
-
-  n=PySequence_Size(values);
-  if (n<0) return NULL;
-  if (n != (long)igraph_vcount(&gr->g)) {
-    PyErr_SetString(PyExc_ValueError, "value list length must be equal to the number of vertices in the graph");
-    return NULL;
-  }
-
-  if (PyDict_SetItem(((PyObject**)gr->g.attr)[ATTRHASH_IDX_VERTEX],
-		     attrname, values) == -1)
-    return NULL;
+  if (igraphmodule_VertexSeq_set_attribute_values_mapping(self, attrname, values))
+	return NULL;
 
   Py_RETURN_NONE;
 }
@@ -210,7 +281,12 @@ PyMethodDef igraphmodule_VertexSeq_methods[] = {
   {"get_attribute_values", (PyCFunction)igraphmodule_VertexSeq_get_attribute_values,
    METH_O,
    "get_attribute_values(attrname) -> list\n"
-   "Returns the value of a given vertex attribute for all vertices\n"
+   "Returns the value of a given vertex attribute for all vertices in a list.\n\n"
+   "The values stored in the list are exactly the same objects that are stored\n"
+   "in the vertex attribute, meaning that in the case of mutable objects,\n"
+   "the modification of the list element does affect the attribute stored in\n"
+   "the vertex. In the case of immutable objects, modification of the list\n"
+   "does not affect the attribute values.\n\n"
    "@param attrname: the name of the attribute\n"
   },
   {"set_attribute_values", (PyCFunction)igraphmodule_VertexSeq_set_attribute_values,
@@ -242,33 +318,47 @@ static PySequenceMethods igraphmodule_VertexSeq_as_sequence = {
   0,                                          /* sq_inplace_repeat */
 };
 
+/**
+ * \ingroup python_interface_vertexseq
+ * This is the collection of functions necessary to implement the
+ * vertex sequence as a mapping (which maps attribute names to values)
+ */
+static PyMappingMethods igraphmodule_VertexSeq_as_mapping = {
+	/* returns the number of vertex attributes */
+	(inquiry) igraphmodule_VertexSeq_attribute_count,
+	/* returns the values of an attribute by name */
+	(binaryfunc) igraphmodule_VertexSeq_get_attribute_values_mapping,
+	/* sets the values of an attribute by name */
+	(objobjargproc) igraphmodule_VertexSeq_set_attribute_values_mapping,
+};
+
 /** \ingroup python_interface_vertexseq
  * Python type object referencing the methods Python calls when it performs various operations on
  * a vertex sequence of a graph
  */
 PyTypeObject igraphmodule_VertexSeqType =
 {
-  PyObject_HEAD_INIT(NULL)                    //
-  0,                                          // ob_size
-  "igraph.VertexSeq",                         // tp_name
-  sizeof(igraphmodule_VertexSeqObject),       // tp_basicsize
-  0,                                          // tp_itemsize
-  (destructor)igraphmodule_VertexSeq_dealloc,  // tp_dealloc
-  0,                                          // tp_print
-  0,                                          // tp_getattr
-  0,                                          // tp_setattr
-  0,                                          // tp_compare
-  0,                                          // tp_repr
-  0,                                          // tp_as_number
-  &igraphmodule_VertexSeq_as_sequence,        // tp_as_sequence
-  0,                                          // tp_as_mapping
-  0,                                          // tp_hash
-  0,                                          // tp_call
-  0,                                          // tp_str
-  0,                                          // tp_getattro
-  0,                                          // tp_setattro
-  0,                                          // tp_as_buffer
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, // tp_flags
+  PyObject_HEAD_INIT(NULL)                    /* */
+  0,                                          /* ob_size */
+  "igraph.VertexSeq",                         /* tp_name */
+  sizeof(igraphmodule_VertexSeqObject),       /* tp_basicsize */
+  0,                                          /* tp_itemsize */
+  (destructor)igraphmodule_VertexSeq_dealloc, /* tp_dealloc */
+  0,                                          /* tp_print */
+  0,                                          /* tp_getattr */
+  0,                                          /* tp_setattr */
+  0,                                          /* tp_compare */
+  0,                                          /* tp_repr */
+  0,                                          /* tp_as_number */
+  &igraphmodule_VertexSeq_as_sequence,        /* tp_as_sequence */
+  &igraphmodule_VertexSeq_as_mapping,         /* tp_as_mapping */
+  0,                                          /* tp_hash */
+  0,                                          /* tp_call */
+  0,                                          /* tp_str */
+  0,                                          /* tp_getattro */
+  0,                                          /* tp_setattro */
+  0,                                          /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, /* tp_flags */
   "Class representing the vertex set of a graph.\n\n"
   "The individual vertices can be accessed by indexing the vertex sequence\n"
   "object. It can be used as an iterable as well, or even in a list\n"
@@ -278,13 +368,25 @@ PyTypeObject igraphmodule_VertexSeqType =
   "  ...   v[\"value\"] = v.index ** 2\n"
   "  ...\n"
   "  >>> [v[\"value\"] ** 0.5 for v in g.vs]\n"
-  "  [0.0, 1.0, 2.0]\n", // tp_doc
-  0,                                          // tp_traverse
-  0,                                          // tp_clear
-  0,                                          // tp_richcompare
-  0,                                          // tp_weaklistoffset
-  0,                                          // tp_iter
-  0,                                          // tp_iternext
-  igraphmodule_VertexSeq_methods,             // tp_methods
+  "  [0.0, 1.0, 2.0]\n"
+  "The vertex set can also be used as a dictionary where the keys are the\n"
+  "attribute names. The values corresponding to the keys are the values\n"
+  "of the given attribute of every vertex in the graph:\n\n"
+  "  >>> g=igraph.Graph.Full(3)\n"
+  "  >>> for idx, v in enumerate(g.vs):\n"
+  "  ...   v[\"weight\"] = idx*(idx+1)\n"
+  "  ...\n"
+  "  >>> g.vs[\"weight\"]\n"
+  "  [0, 2, 6]\n"
+  "  >>> g.vs[\"weight\"] = range(3)\n"
+  "  >>> g.vs[\"weight\"]\n"
+  "  [0, 1, 2]\n", /* tp_doc */
+  0,                                          /* tp_traverse */
+  0,                                          /* tp_clear */
+  0,                                          /* tp_richcompare */
+  0,                                          /* tp_weaklistoffset */
+  0,                                          /* tp_iter */
+  0,                                          /* tp_iternext */
+  igraphmodule_VertexSeq_methods,             /* tp_methods */
 };
 
