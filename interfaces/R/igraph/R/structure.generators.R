67add3430906f4b99b62d943bb62bdf5d7324711
Gabor Csardi <csardi@rmki.kfki.hu>
2007-08-13 21:34:01 +0000

    R interface: graph.formula added. Patches applied:
     * csardi@szaffi2/igraph--main--0.5--patch-20
       R interface: graph.formula added.

--- interfaces/R/igraph/R/structure.generators.R
+++ interfaces/R/igraph/R/structure.generators.R
@@ -26,6 +26,94 @@ graph <- function( edges, n=max(edges)+1, directed=TRUE ) {
         PACKAGE="igraph")
 }
 
+graph.formula <- function(...) {
+  mf <- as.list(match.call())[-1]
+
+  ## Operators first
+  f <- function(x) {
+    if (is.call(x)) {
+      return (list(as.character(x[[1]]), lapply(x[-1], f)))
+    } else {
+      return (NULL)
+    }
+  }
+  ops <- unlist(lapply(mf, f))
+  if (all(ops %in% c("-", ":"))) {
+    directed <- FALSE
+  } else if (all(ops %in% c("-", "+", ":"))) {
+    directed <- TRUE
+  } else {
+    stop("Invalid operator in formula")
+  }
+
+  f <- function(x) {
+    if (is.call(x)) {
+      if (length(x)==3) {
+        return( list(f(x[[2]]), op=as.character(x[[1]]), f(x[[3]])) )
+      } else {
+        return( list(op=as.character(x[[1]]), f(x[[2]])) )
+      }
+    } else {
+      return( c(sym=as.character(x)) )
+    }
+  }
+  
+  ret <- lapply(mf, function(x) unlist(f(x)))
+
+  v <- unique(unlist(lapply(ret, function(x) { x[ names(x)=="sym" ] })))
+
+  ## Merge symbols for ":"
+  ret <- lapply(ret, function(x) {
+    res <- list()
+    for (i in seq(along=x)) {
+      if (x[i]==":" && names(x)[i]=="op") {
+        ## SKIP
+      } else if (i>1 && x[i-1]==":" && names(x)[i-1]=="op") {
+        res[[length(res)]] <- c(res[[length(res)]], unname(x[i]))
+      } else {
+        res <- c(res, x[i])
+      }
+    }
+    res
+  })
+
+  ## Ok, create the edges
+  edges <- numeric()
+  for (i in seq(along=ret)) {
+    prev.sym <- character()
+    lhead <- rhead <- character()
+    for (j in seq(along=ret[[i]])) {
+      act <- ret[[i]][[j]]
+      if (names(ret[[i]])[j]=="op") {
+        if (length(lhead)==0) {
+          lhead <- rhead <- act
+        } else {
+          rhead <- act
+        }
+      } else if (names(ret[[i]])[j]=="sym") {
+        for (ps in prev.sym) {
+          for (ps2 in act) {
+            if (lhead=="+") {
+              edges <- c(edges, unname(c(ps2, ps)))
+            }            
+            if (!directed || rhead=="+") {
+              edges <- c(edges, unname(c(ps, ps2)))
+            }
+          }
+        }
+        lhead <- rhead <- character()
+        prev.sym <- act
+      }
+    }
+  }
+
+  ids <- seq(along=v)-1
+  names(ids) <- v
+  res <- graph( unname(ids[edges] ), n=length(v), directed=directed)
+  res <- set.vertex.attribute(res, "name", value=v)
+  res  
+}
+
 graph.adjacency <- function( adjmatrix, mode="directed", weighted=NULL ) {
   mode <- switch(mode,
                  "directed"=0,
