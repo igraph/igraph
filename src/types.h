dcc315df9dda3638d48b4d4ebc5a75f1123a4884
Gabor Csardi <csardi@rmki.kfki.hu>
2007-08-13 20:05:30 +0000

    Added igraph_vector_bool_t type Patches applied:
     * csardi@szaffi2/igraph--main--0.5--patch-11
       Added igraph_vector_bool_t type

--- src/types.h
+++ src/types.h
@@ -96,6 +96,12 @@ typedef int    igraph_bool_t;
 #include "igraph_pmt_off.h"
 #undef BASE_CHAR
 
+#define BASE_BOOL
+#include "igraph_pmt.h"
+#include "vector.h"
+#include "igraph_pmt_off.h"
+#undef BASE_BOOL
+
 int igraph_vector_order2(igraph_vector_t *v);
 
 /* -------------------------------------------------- */
--- src/vector.c
+++ src/vector.c
@@ -41,6 +41,12 @@
 #include "igraph_pmt_off.h"
 #undef BASE_CHAR
 
+#define BASE_BOOL
+#include "igraph_pmt.h"
+#include "vector.pmt"
+#include "igraph_pmt_off.h"
+#undef BASE_BOOL
+
 int igraph_vector_order2(igraph_vector_t *v) {
 
   igraph_indheap_t heap;
