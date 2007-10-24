40187280ad0ebbb7841357067b31d9a1258e764f
Gabor Csardi <csardi@rmki.kfki.hu>
2007-10-24 16:33:43 +0000

    Ooops, missing arpack.h added Patches applied:
     * csardi@szaffi2/igraph--main--0.5--patch-129
       Ooops, missing arpack.h added

---
+++ src/arpack.h
@@ -0,0 +1,79 @@
+/* -*- mode: C -*-  */
+/* 
+   IGraph library.
+   Copyright (C) 2007  Gabor Csardi <csardi@rmki.kfki.hu>
+   MTA RMKI, Konkoly-Thege Miklos st. 29-33, Budapest 1121, Hungary
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+   02110-1301 USA
+
+*/
+
+#include "types.h"
+
+#ifndef ARPACK_H
+#define ARPACK_H
+
+typedef struct igraph_arpack_options_t {
+  /* INPUT */
+  char bmat[1];			/* I-standard problem, G-generalized */
+  int n; 			/* Dimension of the eigenproblem */
+  char which[2];		/* LA, SA, LM, SM, BE */
+  int nev;                      /* Number of eigenvalues to be computed */
+  igraph_real_t tol;		/* Stopping criterion */
+  int ncv;			/* Number of columns in V */
+  int ldv;			/* Leading dimension of V */
+  int ishift;			/* 0-reverse comm., 1-exact with tridiagonal */
+  int mxiter;                   /* Maximum number of update iterations to take */
+  int nb;			/* Block size on the recurrence, only 1 works */
+  int mode;			/* The kind of problem to be solved (1-5)
+				   1: A*x=l*x, A symmetric
+				   2: A*x=l*M*x, A symm. M pos. def.
+				   3: K*x = l*M*x, K symm., M pos. semidef.
+				   4: K*x = l*KG*x, K s. pos. semidef. KG s. indef.
+				   5: A*x = l*M*x, A symm., M symm. pos. semidef. */
+  int start;			/* 0: random, 1: use the supplied vector */
+  int lworkl;			/* Size of temporary storage, default is fine */
+  igraph_real_t sigma;          /* The shift for modes 3,4,5 */  
+  /* OUTPUT */
+  int info;			/* What happened, see docs */
+  int ierr;			/* What happened  in the dseupd call */
+  int noiter;			/* The number of iterations taken */
+  int numop;			/* Number of OP*x operations */
+  int numopb;			/* Number of B*x operations if BMAT='G' */
+  int numreo;			/* Number of steps of re-orthogonalizations */
+
+  /* INTERNAL */
+  igraph_vector_t *v;
+  igraph_vector_t *workl;
+  igraph_vector_t *workd;
+  igraph_vector_t *d;
+  igraph_vector_t *resid;
+  igraph_vector_t *ax;
+  int *select;
+  int iparam[11];
+  int ipntr[11];
+} igraph_arpack_options_t;
+
+void igraph_arpack_options_init(igraph_arpack_options_t *o);
+
+typedef int igraph_arpack_function_t(igraph_real_t *to, const igraph_real_t *from,
+				     int n, void *extra);
+
+int igraph_arpack_rssolve(igraph_arpack_function_t *fun, void *extra,
+			  igraph_arpack_options_t *options, 
+			  igraph_vector_t *values, igraph_matrix_t *vectors);
+
+#endif
