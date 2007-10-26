3a8e8ff59de85990437bed3393f2406e2c8b7da5
Tamas Nepusz <ntamas@rmki.kfki.hu>
2007-10-26 14:23:10 +0000

    ARPACK: some structures have been made static to resolve multiple symbol definitions on OS X

--- src/arpack/dsgets.c
+++ src/arpack/dsgets.c
@@ -16,7 +16,7 @@
 
 /* Common Block Declarations */
 
-struct {
+static struct {
     integer logfil, ndigit, mgetv0, msaupd, msaup2, msaitr, mseigt, msapps, 
 	    msgets, mseupd, mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, 
 	    mneupd, mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd;
@@ -24,7 +24,7 @@ struct {
 
 #define debug_1 debug_
 
-struct {
+static struct {
     integer nopx, nbx, nrorth, nitref, nrstrt;
     real tsaupd, tsaup2, tsaitr, tseigt, tsgets, tsapps, tsconv, tnaupd, 
 	    tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv, tcaupd, tcaup2, 
