6abe55a75dc92eeb6a884299869196a3fa38cfd9
Tamas Nepusz <ntamas@rmki.kfki.hu>
2008-02-18 16:47:46 +0000

    Python interface: OS X DMG creation script added, homepage updated

---
+++ interfaces/python/scripts/release-osx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Creates the OS X installer package and puts it in a disk image
+which hdiutil >/dev/null || ( echo "This script must be run on OS X"; exit 1 )
+CWD=`pwd`
+while [ ! -f setup.py ]; do cd ..; done
+VERSION=`cat setup.py | grep "version =" | cut -d '=' -f 2 | tr -d "', "`
+python setup.py bdist_mpkg || exit 2
+MPKG="dist/python_igraph-${VERSION}-py2.5-macosx10.5.mpkg"
+DMG=dist/`basename $DIR .mpkg`.dmg
+rm -f ${DMG}
+hdiutil create -volname "python-igraph ${VERSION}" -layout NONE -srcfolder $MPKG $DMG
+rm -f ${MPKG}
+cd $CWD
