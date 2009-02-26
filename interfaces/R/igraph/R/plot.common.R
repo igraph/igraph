18da15eedff9d45075d2bfedd90f0a0f017a1c15
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-02-26 15:15:42 +0100

    R interface: better error message for unknown vertex shapes.

--- interfaces/R/igraph/R/plot.common.R
+++ interfaces/R/igraph/R/plot.common.R
@@ -245,6 +245,16 @@ i.get.arrow.mode <- function(graph, arrow.mode=NULL) {
   arrow.mode
 }
 
+igraph.check.shapes <- function(x) {
+  xx <- unique(x)
+  bad.shapes <- ! xx %in% names(.igraph.shapes)
+  if (any(bad.shapes)) {
+    bs <- paste(xx[bad.shapes], collapse=", ")
+    stop("Bad vertex shape(s): ", bs, ".")
+  }
+  x
+}
+
 i.default.values <- list(vertex=list(color="SkyBlue2",
                            size=15,
                            size2=15,
