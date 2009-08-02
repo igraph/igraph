9afce3d534536983006a511a0cd54b58a07f389a
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-08-02 13:05:07 +0200

    Cosmetics: added a GPL header to a source file.

--- examples/simple/centralization.c
+++ examples/simple/centralization.c
@@ -1,3 +1,25 @@
+/* -*- mode: C -*-  */
+/* 
+   IGraph library.
+   Copyright (C) 2009  Gabor Csardi <Gabor.Csardi@unil.ch>
+   UNIL DGM, Rue de Bugnon 27, CH-1005 Lausanne, Switzerland
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+   02110-1301 USA
+
+*/
 
 #include <igraph.h>
 #include <math.h>
@@ -22,7 +44,7 @@ int main() {
   }
 
   igraph_centralization_betweenness(&g, /*res=*/ 0,
-				    IGRAPH_UNDIRECTED, &cent, 
+				    IGRAPH_UNDIRECTED, /*nobigint=*/ 1, &cent, 
 				    /*theoretical_max=*/ 0,
 				    /*normalized=*/ 1);
   if (cent != 1.0) { 
@@ -70,7 +92,7 @@ int main() {
   }
 
   igraph_centralization_betweenness(&g, /*res=*/ 0,
-				    IGRAPH_UNDIRECTED, &cent,
+				    IGRAPH_UNDIRECTED, /*nobigint=*/ 1, &cent,
 				    /*theoretical_max=*/ 0,
 				    /*normalized=*/ 1);
   if (cent != 1.0) { 
@@ -104,7 +126,7 @@ int main() {
   }
 
   igraph_centralization_betweenness(&g, /*res=*/ 0,
-				    IGRAPH_UNDIRECTED, &cent,
+				    IGRAPH_UNDIRECTED, /*nobigint=*/ 1, &cent,
 				    /*theoretical_max=*/ 0,
 				    /*normalized=*/ 1);
   if (cent != 1.0) { 
