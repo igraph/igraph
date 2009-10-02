2e60001284563c2694c6da4cdee7ced6b2a10cf6
Tamas Nepusz <tamas@cs.rhul.ac.uk>
2009-10-02 16:52:08 +0100

    fixed FSF address in src/walktrap*

--- src/walktrap_communities.h
+++ src/walktrap_communities.h
@@ -22,7 +22,8 @@
 */
 
 /* The original version of this file was written by Pascal Pons
-   The original copyright notice follows here */
+   The original copyright notice follows here. The FSF address was
+   fixed by Tamas Nepusz */
 
 // File: communities.h
 //-----------------------------------------------------------------------------
@@ -41,7 +42,8 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+// 02110-1301 USA
 //-----------------------------------------------------------------------------
 // Author   : Pascal Pons 
 // Email    : pons@liafa.jussieu.fr
--- src/walktrap_graph.h
+++ src/walktrap_graph.h
@@ -41,7 +41,8 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+// 02110-1301 USA
 //-----------------------------------------------------------------------------
 // Author   : Pascal Pons 
 // Email    : pons@liafa.jussieu.fr
@@ -50,6 +51,8 @@
 // Time	    : June 2005
 //-----------------------------------------------------------------------------
 // see readme.txt for more details
+
+/* FSF address above was fixed by Tamas Nepusz */
 
 
 #ifndef GRAPH_H
