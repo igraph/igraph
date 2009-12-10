a87aadb77916528c3036a5d082a79f21b33c29c9
Tamas Nepusz <tamas@cs.rhul.ac.uk>
2009-12-10 11:04:23 +0000

    Python interface: fixing a memory leak in Graph.Adjacency (thanks to Jonathan F. Donges)

--- interfaces/python/src/graphobject.c
+++ interfaces/python/src/graphobject.c
@@ -1398,7 +1398,7 @@ PyObject *igraphmodule_Graph_Adjacency(PyTypeObject * type,
   PyObject *matrix, *mode_o = Py_None;
   igraph_adjacency_t mode = IGRAPH_ADJ_DIRECTED;
 
-  char *kwlist[] = { "matrix", "mode", NULL };
+  static char *kwlist[] = { "matrix", "mode", NULL };
 
   if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!|O", kwlist,
                                    &PyList_Type, &matrix, &mode_o))
@@ -1416,6 +1416,8 @@ PyObject *igraphmodule_Graph_Adjacency(PyTypeObject * type,
     igraph_matrix_destroy(&m);
     return NULL;
   }
+
+  igraph_matrix_destroy(&m);
 
   CREATE_GRAPH_FROM_TYPE(self, g, type);
 
