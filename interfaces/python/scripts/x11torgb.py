53ca8d437903e3c524404122c8051fd33de2e146
Tamas Nepusz <ntamas@rmki.kfki.hu>
2010-06-14 19:51:03 +0100

    Python: updated known color list with alpha values

--- interfaces/python/scripts/x11torgb.py
+++ interfaces/python/scripts/x11torgb.py
@@ -8,22 +8,22 @@ if len(sys.argv)<2:
     sys.exit(1)
 
 colors = {
-  "black":    (0.  , 0.  , 0.  ),
-  "silver":   (0.75, 0.75, 0.75),
-  "gray":     (0.5 , 0.5 , 0.5 ),
-  "white":    (1.  , 1.  , 1.  ),
-  "maroon":   (0.5 , 0.  , 0.  ),
-  "red":      (1.  , 0.  , 0.  ),
-  "purple":   (0.5 , 0.  , 0.5 ),
-  "fuchsia":  (1.  , 0.  , 1.  ),
-  "green":    (0.  , 0.5 , 0.  ),
-  "lime":     (0.  , 1.  , 0.  ),
-  "olive":    (0.5 , 0.5 , 0.  ),
-  "yellow":   (1.  , 1.  , 0.  ),
-  "navy":     (0.  , 0.  , 0.5 ),
-  "blue":     (0.  , 0.  , 1.  ),
-  "teal":     (0.  , 0.5 , 0.5 ),
-  "aqua":     (0.  , 1.  , 1.  ),
+  "black":    (0.  , 0.  , 0.  , 1.),
+  "silver":   (0.75, 0.75, 0.75, 1.),
+  "gray":     (0.5 , 0.5 , 0.5 , 1.),
+  "white":    (1.  , 1.  , 1.  , 1.),
+  "maroon":   (0.5 , 0.  , 0.  , 1.),
+  "red":      (1.  , 0.  , 0.  , 1.),
+  "purple":   (0.5 , 0.  , 0.5 , 1.),
+  "fuchsia":  (1.  , 0.  , 1.  , 1.),
+  "green":    (0.  , 0.5 , 0.  , 1.),
+  "lime":     (0.  , 1.  , 0.  , 1.),
+  "olive":    (0.5 , 0.5 , 0.  , 1.),
+  "yellow":   (1.  , 1.  , 0.  , 1.),
+  "navy":     (0.  , 0.  , 0.5 , 1.),
+  "blue":     (0.  , 0.  , 1.  , 1.),
+  "teal":     (0.  , 0.5 , 0.5 , 1.),
+  "aqua":     (0.  , 1.  , 1.  , 1.),
 }
 
 f = open(sys.argv[1])
@@ -31,8 +31,9 @@ for line in f:
     if line[0] == '!': continue
     parts = line.strip().split(None, 3)
     for x in xrange(3):
-	parts[x] = float(parts[x])/255.
-    colors[parts[3].lower()] = tuple(parts[0:3])
+        parts[x] = float(parts[x])/255.
+    parts[3:3] = [1.]
+    colors[parts[4].lower()] = tuple(parts[0:4])
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(colors)
