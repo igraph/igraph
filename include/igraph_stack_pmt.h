8d3558ef76a2b4dffc06a8851408e17441c3c49a
Gabor Csardi <csardi.gabor@gmail.com>
2010-08-13 17:03:57 +0200

    Added a 'const' to igraph_stack*_size().

--- include/igraph_stack_pmt.h
+++ include/igraph_stack_pmt.h
@@ -36,7 +36,7 @@ int FUNCTION(igraph_stack,init)(TYPE(igraph_stack)* s, long int size);
 void FUNCTION(igraph_stack,destroy)(TYPE(igraph_stack)* s);
 int FUNCTION(igraph_stack,reserve)(TYPE(igraph_stack)* s, long int size);
 igraph_bool_t FUNCTION(igraph_stack,empty)(TYPE(igraph_stack)* s);
-long int FUNCTION(igraph_stack,size)(TYPE(igraph_stack)* s);
+long int FUNCTION(igraph_stack,size)(const TYPE(igraph_stack)* s);
 void FUNCTION(igraph_stack,clear)(TYPE(igraph_stack)* s);
 int FUNCTION(igraph_stack,push)(TYPE(igraph_stack)* s, BASE elem);
 BASE FUNCTION(igraph_stack,pop)(TYPE(igraph_stack)* s);
