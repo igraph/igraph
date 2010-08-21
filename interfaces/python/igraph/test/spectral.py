9bc012ef6f4321084655675ce31961f9a6916887
Tamas Nepusz <ntamas@rmki.kfki.hu>
2010-08-21 16:08:23 +0100

    Python: fixed test case for Graph.laplacian

--- interfaces/python/igraph/test/spectral.py
+++ interfaces/python/igraph/test/spectral.py
@@ -3,26 +3,25 @@ import unittest
 from igraph import *
 
 class SpectralTests(unittest.TestCase):
+    def assertAlmostEqualsMatrix(self, mat1, mat2, eps = 1e-7):
+        self.failUnless(all(
+            abs(obs-exp) < eps
+            for obs, exp in zip(sum(mat1, []), sum(mat2, []))
+        ))
+
     def testLaplacian(self):
         g=Graph.Full(3)
         g.es["weight"] = [1, 2, 3]
         self.failUnless(g.laplacian() == [[ 2, -1, -1],\
                                           [-1,  2, -1],\
                                           [-1, -1,  2]])
-        self.failUnless(g.laplacian(normalized=True) == [[ 1.0, -0.5, -0.5],\
-                                                        [-0.5,  1.0, -0.5],\
-                                                        [-0.5, -0.5,  1.0]])
-        self.failUnless(g.laplacian("weight") == [[ 3, -1, -2],\
-                                                  [-1,  4, -3],\
-                                                  [-2, -3,  5]])
+        self.assertAlmostEqualsMatrix(g.laplacian(normalized=True),
+                [[1, -0.5, -0.5], [-0.5, 1, -0.5], [-0.5, -0.5, 1]])
 
         mx0 = [[1., -1/(12**0.5), -2/(15**0.5)],
               [-1/(12**0.5), 1., -3/(20**0.5)],
               [-2/(15**0.5), -3/(20**0.5), 1.]]
-        mx1 = g.laplacian("weight", True)
-        for i in xrange(3):
-            for j in xrange(3):
-                self.failUnless(abs(mx0[i][j]-mx1[i][j]) < 0.001)
+        self.assertAlmostEqualsMatrix(g.laplacian("weight", True), mx0)
 
         g=Graph.Tree(5, 2)
         g.add_vertices(1)
