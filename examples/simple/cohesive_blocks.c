018fd1775f3fd7ca3aa3a62b10f8e06c935e5645
Gabor Csardi <csardi.gabor@gmail.com>
2010-09-14 10:45:42 +0200

    Added more cohesive blocking test cases.

--- examples/simple/cohesive_blocks.c
+++ examples/simple/cohesive_blocks.c
@@ -23,16 +23,47 @@
 
 #include <igraph.h>
 
-int main() {
-  
-  igraph_t g;
+int doit(igraph_t *g) {
+
   igraph_vector_ptr_t blocks;
   igraph_vector_t cohesion;
   igraph_vector_t parent;
   igraph_t block_tree;
   long int i;
 
-  /* The graph from the Moody-White paper */
+  igraph_vector_ptr_init(&blocks, 0);
+  igraph_vector_init(&cohesion, 0);
+  igraph_vector_init(&parent, 0);
+
+  igraph_cohesive_blocks(g, &blocks, &cohesion, &parent, 
+			 &block_tree);
+  
+  printf("Blocks:\n");
+  for (i=0; i<igraph_vector_ptr_size(&blocks); i++) {
+    igraph_vector_t *sg=VECTOR(blocks)[i];
+    printf("  "); igraph_vector_print(sg);
+    igraph_vector_destroy(sg);
+    igraph_free(sg);
+  }
+  printf("Cohesion:\n  ");  igraph_vector_print(&cohesion);
+  printf("Parents:\n  ");   igraph_vector_print(&parent);
+  printf("Block graph:\n"); igraph_write_graph_edgelist(&block_tree, stdout);
+
+  igraph_vector_ptr_destroy(&blocks);
+  igraph_vector_destroy(&cohesion);
+  igraph_vector_destroy(&parent);
+  igraph_destroy(&block_tree);
+
+  return 0;
+}
+
+int main() {
+  
+  igraph_t g;
+  int ret;
+
+  /* --------------------------------------------------------*/
+  /* The graph from the Moody-White paper                    */
 
   igraph_small(&g, 23, IGRAPH_UNDIRECTED,
 	       0,1, 0,2, 0,3, 0,4, 0,5,
@@ -58,28 +89,48 @@ int main() {
 	       21,22,
 	       -1);
   
-  igraph_vector_ptr_init(&blocks, 0);
-  igraph_vector_init(&cohesion, 0);
-  igraph_vector_init(&parent, 0);
-  igraph_cohesive_blocks(&g, &blocks, &cohesion, &parent, 
-			 &block_tree);
-  
-  for (i=0; i<igraph_vector_ptr_size(&blocks); i++) {
-    igraph_vector_t *sg=VECTOR(blocks)[i];
-    igraph_vector_print(sg);
-    igraph_vector_destroy(sg);
-    igraph_free(sg);
-  }
-  igraph_vector_print(&cohesion);
-  igraph_vector_print(&parent);
-  igraph_write_graph_edgelist(&block_tree, stdout);
-
-  igraph_vector_ptr_destroy(&blocks);
-  igraph_vector_destroy(&cohesion);
-  igraph_vector_destroy(&parent);
-  igraph_destroy(&block_tree);
-
+  if ( (ret=doit(&g)) ) { return ret; }
   igraph_destroy(&g);
+  printf("--\n");
+
+  /* --------------------------------------------------------*/
+  /* A tricky graph, where the separators themselves         */
+  /* form a block.                                           */
+
+  igraph_small(&g, 8, IGRAPH_UNDIRECTED,
+	       0,1,0,4,0,5, 1,2,1,4,1,5,1,6, 2,3,2,5,2,6,2,7,
+	       3,6,3,7, 4,5, 5,6, 6,7, 
+	       -1);
+  
+  if ( (ret=doit(&g)) ) { return ret; }
+  igraph_destroy(&g);
+  printf("--\n");  
+
+  /* --------------------------------------------------------*/
+  /* The science camp graph from http://intersci.ss.uci.edu/ */
+  /* wiki/index.php/Cohesive_blocking                        */
+
+  igraph_small(&g, 18, IGRAPH_UNDIRECTED, 
+	       0,1,0,2,0,3, 
+	       1,2,1,3,1,16,1,17, 
+	       2,3, 
+	       3,17, 
+	       4,5,4,6,4,7,4,8,
+	       5,6,5,7, 
+	       6,7,6,8,
+	       7,8,7,16,
+	       8,9,8,10,
+	       9,11,9,12,9,13,9,14,
+	       10,11,10,12,10,13,
+	       11,14,
+	       12,13,12,14,12,15,
+	       15,16,15,17,
+	       16,17,
+	       -1);
+	       
+  if ( (ret=doit(&g)) ) { return ret; }
+  igraph_destroy(&g);
+  printf("--\n");  
   
   return 0;
 }
