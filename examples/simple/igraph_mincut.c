944d30c9343a848c466f9c3c7336ab2e03eca65a
Gabor Csardi <csardi.gabor@gmail.com>
2010-09-24 18:56:30 +0200

    Added some test cases for igraph_mincut.

--- examples/simple/igraph_mincut.c
+++ examples/simple/igraph_mincut.c
@@ -23,12 +23,33 @@
 
 #include <igraph.h>
 
-int print_vector(const igraph_vector_t *v) {
-  long int i, size=igraph_vector_size(v);
-  for (i=0; i<size; i++) {
-    printf("%g ", (double) VECTOR(*v)[i]);
+int print_mincut(const igraph_t *graph, igraph_real_t value, 
+		 const igraph_vector_t *partition, 
+		 const igraph_vector_t *partition2, 
+		 const igraph_vector_t *cut, 
+		 const igraph_vector_t *capacity) {
+  
+  long int i, nc=igraph_vector_size(cut);
+  igraph_bool_t directed=igraph_is_directed(graph);
+
+  printf("mincut value: %g\n", (double) value);
+  printf("first partition:  ");  igraph_vector_print(partition);
+  printf("second partition: ");  igraph_vector_print(partition2);
+  printf("edges in the cut: ");
+  for (i=0; i<nc; i++) {
+    long int edge=VECTOR(*cut)[i];
+    long int from=IGRAPH_FROM(graph, edge);
+    long int to  =IGRAPH_TO  (graph, edge);
+    if (!directed && from > to) {
+      igraph_integer_t tmp=from;
+      from=to;
+      to=tmp;
+    }
+    printf("%li-%li (%g), ", from, to, VECTOR(*capacity)[edge]);
   }
   printf("\n");
+  
+  return 0;
 }
 
 int main() {
@@ -36,46 +57,53 @@ int main() {
   igraph_t g;
   igraph_vector_t weights, partition, partition2, cut;
   igraph_integer_t value;
-  long int i;
+
+  igraph_vector_init(&partition, 0);
+  igraph_vector_init(&partition2, 0);
+  igraph_vector_init(&cut, 0);
+
+  /* -------------------------------------------- */
   
   igraph_small(&g, 0, IGRAPH_UNDIRECTED, 
 	       0,1, 0,4, 1,2, 1,4, 1,5, 2,3, 2,6, 3,6, 3,7, 4,5, 5,6, 6,7,
 	       -1);
-  
   igraph_vector_init_int_end(&weights, -1, 2,3,3,2,2, 4,2,2,2,3, 1,3, -1);
   
-  igraph_vector_init(&partition, 0);
-  igraph_vector_init(&partition2, 0);
-  igraph_vector_init(&cut, 0);
-  
   igraph_mincut(&g, &value, &partition, &partition2, &cut, &weights);
+  print_mincut(&g, value, &partition, &partition2, &cut, &weights);
   
-  printf("mincut value: %g\n", (double) value);
-  
-  printf("first partition:  ");
-  igraph_vector_sort(&partition);
-  print_vector(&partition);
-  printf("second partition: ");
-  igraph_vector_sort(&partition2);
-  print_vector(&partition2);
-  printf("edges in the cut: ");
-  for (i=0; i<igraph_vector_size(&cut); i++) {
-    igraph_integer_t from, to;
-    igraph_edge(&g, VECTOR(cut)[i], &from, &to);
-    if (from > to) {
-      igraph_integer_t tmp=from;
-      from=to;
-      to=tmp;
-    }
-    printf("%li-%li ", (long int)from, (long int)to);
-  }
-  printf("\n");
+  igraph_vector_destroy(&weights);
+  igraph_destroy(&g);
+
+  /* -------------------------------------------- */
+
+  igraph_small(&g, 6, IGRAPH_DIRECTED,
+	       0,1,1,2,2,3, 0,5,5,4,4,3, 3,0, -1);
+  igraph_vector_init_int_end(&weights, -1, 3,1,2, 10,1,3, 2, -1);
+
+  igraph_mincut(&g, &value, &partition, &partition2, &cut, &weights);
+  print_mincut(&g, value, &partition, &partition2, &cut, &weights);
+
+  igraph_vector_destroy(&weights);
+  igraph_destroy(&g);
+
+  /* -------------------------------------------- */
+
+  igraph_small(&g, 5, IGRAPH_DIRECTED,
+	       4,3, 3,2, 2,1, 1,0, 
+	       -1);
+  igraph_vector_init_int_end(&weights, -1, 1,1,1,1, -1);
+  igraph_mincut(&g, &value, &partition, &partition2, &cut, &weights);
+  print_mincut(&g, value, &partition, &partition2, &cut, &weights);
+
+  igraph_vector_destroy(&weights);
+  igraph_destroy(&g);
+
+  /* -------------------------------------------- */
 
   igraph_vector_destroy(&cut);
   igraph_vector_destroy(&partition2);
   igraph_vector_destroy(&partition);
-  igraph_vector_destroy(&weights);
-  igraph_destroy(&g);
  
   return 0;
 }
