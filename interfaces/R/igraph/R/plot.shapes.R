22cee710fdf5458c3de1a1ea87b8787a6d27ee7f
Gabor Csardi <csardi.gabor@gmail.com>
2010-10-17 23:49:22 +0200

    R: added 'pie' vertex shape.

--- interfaces/R/igraph/R/plot.shapes.R
+++ interfaces/R/igraph/R/plot.shapes.R
@@ -526,11 +526,135 @@
     invisible(NULL)
   }
 }
+
+mypie <- function(x, y, values, radius, edges=200, col=NULL, angle=45,
+                  density=NULL, border=NULL, lty=NULL, init.angle=90, ...) {
+  values <- c(0, cumsum(values)/sum(values))
+  dx <- diff(values)
+  nx <- length(dx)
+  twopi <- 2 * pi
+  if (is.null(col)) 
+    col <- if (is.null(density)) 
+      c("white", "lightblue", "mistyrose", "lightcyan", 
+        "lavender", "cornsilk")
+    else par("fg")
+  col <- rep(col, length.out = nx)
+  border <- rep(border, length.out = nx)
+  lty <- rep(lty, length.out = nx)
+  angle <- rep(angle, length.out = nx)
+  density <- rep(density, length.out = nx)
+  t2xy <- function(t) {
+    t2p <- twopi * t + init.angle * pi/180
+    list(x = radius * cos(t2p), y = radius * sin(t2p))
+  }
+  for (i in 1:nx) {
+    n <- max(2, floor(edges * dx[i]))
+    P <- t2xy(seq.int(values[i], values[i + 1], length.out = n))
+    polygon(x+c(P$x, 0), y+c(P$y, 0), density = density[i], angle = angle[i], 
+            border = border[i], col = col[i], lty = lty[i], ...)
+  }
+}
+
+.igraph.shape.pie <- function(coords, el=NULL, v=NULL, mode=c("clip", "plot"),
+                                 params, end=c("both", "from", "to")) {
+
+  mode=match.arg(mode)
+  end =match.arg(end)
+
+  #####################################################################
+  ## clipping mode, this is the same as for circle
+  
+  if (mode=="clip") {
+    if (length(coords)==0) { return (coords) }     
+
+    vertex.size <- 1/200 * params("vertex", "size")
+
+    if (end=="from") {
+      phi <- atan2(coords[,4] - coords[,2], coords[,3] - coords[,1])
+      vsize.from <- if (length(vertex.size)==1) {
+        vertex.size
+      } else {
+        vertex.size[ el[,1] ]
+      }
+      res <- cbind(coords[,1] + vsize.from*cos(phi),
+                   coords[,2] + vsize.from*sin(phi) )
+    } else if (end=="to") {
+      phi <- atan2(coords[,4] - coords[,2], coords[,3] - coords[,1])
+      r <- sqrt( (coords[,3] - coords[,1])^2 + (coords[,4] - coords[,2])^2 )
+      vsize.to <- if (length(vertex.size)==1) {
+        vertex.size
+      } else {
+        vertex.size[ el[,2] ]
+      }
+      res <- cbind(coords[,1] + (r-vsize.to)*cos(phi),
+                   coords[,2] + (r-vsize.to)*sin(phi) )
+    } else if (end=="both") {
+      phi <- atan2(coords[,4] - coords[,2], coords[,3] - coords[,1])
+      r <- sqrt( (coords[,3] - coords[,1])^2 + (coords[,4] - coords[,2])^2 )
+      vsize.from <- if (length(vertex.size)==1) {
+        vertex.size
+      } else {
+        vertex.size[ el[,1] ]
+      }
+      vsize.to <- if (length(vertex.size)==1) {
+        vertex.size
+      } else {
+        vertex.size[ el[,2] ]
+      }
+      res <- cbind(coords[,1] + vsize.from*cos(phi),
+                   coords[,2] + vsize.from*sin(phi),
+                   coords[,1] + (r-vsize.to)*cos(phi),
+                   coords[,2] + (r-vsize.to)*sin(phi) )
+    }
+
+    res
+
+  #####################################################################
+  ## plotting mode
     
+  } else if (mode=="plot") {
+    getparam <- function(pname) {
+      p <- params("vertex", pname)
+      if (length(p) != 1 && !is.null(v)) {
+        p <- p[v]
+      }
+      p
+    }
+    vertex.color       <- getparam("color")
+    vertex.frame.color <- getparam("frame.color")
+    vertex.size        <- rep(1/200 * getparam("size"), length=nrow(coords))
+    vertex.pie         <- getparam("pie")
+    vertex.pie.color   <- getparam("pie.color")
+    vertex.pie.angle   <- getparam("pie.angle")
+    vertex.pie.density <- getparam("pie.density")
+    vertex.pie.lty     <- getparam("pie.lty")
+
+    for (i in seq_len(nrow(coords))) {
+      pie <- if(length(vertex.pie)==1) {
+        vertex.pie
+      } else {
+        vertex.pie[[i]]
+      }
+      col <- if (length(vertex.pie.color)==1) {
+        vertex.pie.color[[1]]
+      } else {
+        vertex.pie.color[[i]]
+      }
+      mypie(x=coords[i,1], y=coords[i,2], pie,
+            radius=vertex.size[i], edges=200, col=col,
+            angle=na.omit(vertex.pie.angle[c(i,1)])[1],
+            density=na.omit(vertex.pie.density[c(i,1)])[1],
+            border=na.omit(vertex.frame.color[c(i,1)])[1],
+            lty=na.omit(vertex.pie.lty[c(i,1)])[1])
+    }
+  }
+}
+
 .igraph.shapes <- list("circle" = .igraph.shape.circle,
                        "square" = .igraph.shape.square,
                        "csquare" = .igraph.shape.csquare,
                        "rectangle" = .igraph.shape.rectangle,
                        "crectangle" = .igraph.shape.crectangle,
                        "vrectangle" = .igraph.shape.vrectangle,
-                       "none"   = .igraph.shape.none)
+                       "none"   = .igraph.shape.none,
+                       "pie"    = .igraph.shape.pie)
