bde487a125d55fb590754376127665d2bf9a910e
Gabor Csardi <csardi.gabor@gmail.com>
2010-10-27 11:28:43 +0200

    R: corrected a test case.

--- interfaces/R/igraph/tests/get.edge.R
+++ interfaces/R/igraph/tests/get.edge.R
@@ -3,6 +3,6 @@ library(igraph)
 
 g <- erdos.renyi.game(100, 3/100)
 edges <- unlist(lapply(seq_len(ecount(g)), get.edge, graph=g))
-g2 <- graph(edges, dir=FALSE)
+g2 <- graph(edges, dir=FALSE, n=vcount(g))
 graph.isomorphic(g, g2)
 
