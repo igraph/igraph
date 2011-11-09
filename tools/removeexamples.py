a24212069d9344f01422746c251de7c28db4df8c
Gabor Csardi <csardi.gabor@gmail.com>
2011-11-09 08:38:50 -0500

    Added missing file tools/removeexamples.py.

---
+++ tools/removeexamples.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import sys
+from xml.etree.ElementTree import ElementTree
+
+def usage():
+    print sys.argv[0], "<infile> <outfile>"
+
+def main():
+    if len(sys.argv) != 3:
+        usage()
+        sys.exit(2)
+    
+    # Read in
+    tree=ElementTree()
+    tree.parse(sys.argv[1])
+    
+    # Remove examples
+    examples=tree.findall("//example")
+    for ex in examples:
+        prog=ex.find("programlisting")
+        ex.remove(prog)
+
+    # Write result
+    tree.write(sys.argv[2])    
+
+if __name__ == "__main__":
+    main()
