69a3293a070a14629e75c18f9152421fed0a371a
Gabor Csardi <csardi.gabor@gmail.com>
2011-11-18 12:16:58 -0500

    SCG: remove two forgotten print statements;

--- src/scg.c
+++ src/scg.c
@@ -1319,12 +1319,10 @@ int igraph_scg_stochastic(const igraph_t *graph,
     mymatrix=&real_matrix;
     IGRAPH_CHECK(igraph_i_matrix_stochastic(matrix, mymatrix, norm));
     IGRAPH_FINALLY(igraph_matrix_destroy, mymatrix);
-    igraph_matrix_print(mymatrix);
   } else { /* sparsemat */
     mysparsemat=&real_sparsemat;
     IGRAPH_CHECK(igraph_i_sparsemat_stochastic(sparsemat, mysparsemat, norm));
     IGRAPH_FINALLY(igraph_sparsemat_destroy, mysparsemat);
-    igraph_sparsemat_print(mysparsemat, stdout);
   }
 
   /* -------------------------------------------------------------------- */
