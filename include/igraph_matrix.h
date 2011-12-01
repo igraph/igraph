1b5e16a52d1c57bb0f97a559b96ddbd62f748e15
Gabor Csardi <csardi.gabor@gmail.com>
2011-11-30 19:27:55 -0500

    Renamed igraph_matrix_e_tol to igraph_matrix_all_e_tol.

--- include/igraph_matrix.h
+++ include/igraph_matrix.h
@@ -98,9 +98,9 @@ __BEGIN_DECLS
  */
 #define MATRIX(m,i,j) ((m).data.stor_begin[(m).nrow*(j)+(i)])
 
-igraph_bool_t igraph_matrix_e_tol(const igraph_matrix_t *lhs,
-				  const igraph_matrix_t *rhs,
-				  igraph_real_t tol);
+igraph_bool_t igraph_matrix_all_e_tol(const igraph_matrix_t *lhs,
+				      const igraph_matrix_t *rhs,
+				      igraph_real_t tol);
 
 __END_DECLS
 
--- src/matrix.c
+++ src/matrix.c
@@ -141,8 +141,8 @@ int igraph_matrix_complex_create_polar(igraph_matrix_complex_t *v,
   return 0;
 }
 
-igraph_bool_t igraph_matrix_e_tol(const igraph_matrix_t *lhs,
-				  const igraph_matrix_t *rhs,
-				  igraph_real_t tol) {
+igraph_bool_t igraph_matrix_all_e_tol(const igraph_matrix_t *lhs,
+				      const igraph_matrix_t *rhs,
+				      igraph_real_t tol) {
   return igraph_vector_e_tol(&lhs->data, &rhs->data, tol);
 }
