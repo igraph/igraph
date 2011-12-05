d98e02b5365bb3ff30d525575cace59bfc5ec319
Tamas Nepusz <ntamas@gmail.com>
2011-12-05 16:35:03 +0100

    better documentation generation script

--- interfaces/python/scripts/mkdoc.sh
+++ interfaces/python/scripts/mkdoc.sh
@@ -4,6 +4,14 @@
 #
 # Usage: ./mkdoc.sh [--sync] [directory]
 
+SCRIPTS_FOLDER=`dirname $0`
+ROOT_FOLDER=`readlink -m ${SCRIPTS_FOLDER}/..`
+DOC_API_FOLDER=`readlink -m ${ROOT_FOLDER}/doc/api`
+
+cd ${ROOT_FOLDER}
+mkdir -p ${DOC_API_FOLDER}/pdf
+mkdir -p ${DOC_API_FOLDER}/html
+
 EPYDOC=`which epydoc`
 if [ x$EPYDOC = x ]; then
   echo "epydoc not found, exiting..."
@@ -27,7 +35,7 @@ echo "Removing existing documentation..."
 rm -rf html
 
 echo "Generating HTML documentation..."
-epydoc --html -o html -v \
+epydoc --html -o ${DOC_API_FOLDER}/html -v \
        --name="IGraph library" \
 	   --url="http://igraph.sourceforge.net" \
 	   --no-private \
@@ -39,16 +47,15 @@ which latex >/dev/null && PDF=1
 
 if [ $PDF -eq 1 ]; then
   echo "Generating PDF documentation..."
-  epydoc --pdf -o pdf --exclude=igraph.test --inheritance=listed -v --name="IGraph library" --url="http://igraph.sourceforge.net" $PACKAGES
+  epydoc --pdf -o ${DOC_API_FOLDER}/pdf --exclude=igraph.test --inheritance=listed -v --name="IGraph library" --url="http://igraph.sourceforge.net" $PACKAGES
 
-  echo "Moving PDF documentation to the HTML subdirectory..."
-  mv pdf/api.pdf html/igraph.pdf
-  rm -rf pdf
 fi
 
 if [ $SYNC -eq 1 ]; then
   echo "Syncing documentation to web"
-  rsync --delete -avz html/ ntamas,igraph@web.sourceforge.net:/home/groups/i/ig/igraph/htdocs/doc/python/
+  cp ${DOC_API_FOLDER}/pdf/igraph.pdf ${DOC_API_FOLDER}/html
+  rsync --delete -avz ${DOC_API_FOLDER}/html/ ntamas,igraph@web.sourceforge.net:/home/groups/i/ig/igraph/htdocs/doc/python/
+  rm ${DOC_API_FOLDER}/html/igraph.pdf
 fi
 
 cd "$PWD"
