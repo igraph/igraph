cfd629b0fd76659fbda7ba1ba571e35f976a4ffd
Tamas Nepusz <ntamas@rmki.kfki.hu>
2012-02-16 17:23:54 +0100

    added docs about the mapping between line graph vertices and the original edges

--- src/structural_properties.c
+++ src/structural_properties.c
@@ -4696,6 +4696,10 @@ int igraph_i_linegraph_directed(const igraph_t *graph, igraph_t *linegraph) {
  * is the same as the source of the second vertex's corresponding edge.
  * 
  * </para><para>
+ * Edge <em>i</em> in the original graph will correspond to vertex <em>i</em>
+ * in the line graph.
+ *
+ * </para><para>
  * The first version of this function was contributed by Vincent Matossian, 
  * thanks.
  * \param graph The input graph, may be directed or undirected.
