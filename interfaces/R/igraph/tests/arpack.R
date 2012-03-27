d887cd58334d84c2e31e966b79b0554df5790406
Gabor Csardi <csardi.gabor@gmail.com>
2012-03-27 11:35:23 -0400

    R: added some ARPACK tests.

--- interfaces/R/igraph/tests/arpack.R
+++ interfaces/R/igraph/tests/arpack.R
@@ -26,5 +26,31 @@ max(abs(r1$values - r2$values[1])) < 1e-14
 max(abs(correctSign(r1$vectors) - correctSign(r2$vectors[,1]))) < 1e-14
 
 ####
+# Complex case
+
+A <- structure(c(-6, -6, 7, 6, 1, -9, -3, 2, -9, -7, 0, 1, -7, 8, 
+-7, 10, 0, 0, 1, 1, 10, 0, 8, -4, -4, -5, 8, 9, -6, 9, 3, 8, 
+6, -1, 9, -9, -6, -3, -1, -7, 8, -4, -4, 10, 0, 5, -2, 0, 7, 
+10, 1, 4, -8, 3, 5, 3, -7, -9, 10, -1, -4, -7, -1, 7, 5, -5, 
+1, -4, 9, -2, 10, 1, -7, 7, 6, 7, -3, 0, 9, -5, -8, 1, -3, -3, 
+-8, -7, -8, 10, 8, 7, 0, 6, -7, -8, 10, 10, 1, 0, -2, 6), .Dim = c(10L, 
+10L))
+
+f <- function(x, extra=NULL) A %*% x
+res <- arpack(f, options=list(n=10, nev=3, ncv=7, sym=FALSE))
+res$values
+(res$values[1] * res$vectors[,1]) / (A %*% res$vectors[,1])
+(res$values[2] * res$vectors[,2]) / (A %*% res$vectors[,2])
+abs((res$values[3] * res$vectors[,3]) / (A %*% res$vectors[,3]))
+
+f <- function(x, extra=NULL) A %*% x
+res <- arpack(f, options=list(n=10, nev=4, ncv=9, sym=FALSE))
+res$values
+(res$values[1] * res$vectors[,1]) / (A %*% res$vectors[,1])
+(res$values[2] * res$vectors[,2]) / (A %*% res$vectors[,2])
+abs((res$values[3] * res$vectors[,3]) / (A %*% res$vectors[,3]))
+abs((res$values[4] * res$vectors[,4]) / (A %*% res$vectors[,4]))
+
+####
 
 # TODO: further tests for typically hard cases
