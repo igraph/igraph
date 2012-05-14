11cef4d25ed1e87d53333181a95f374543716531
Gabor Csardi <csardi.gabor@gmail.com>
2012-05-14 16:50:50 -0400

    Document the status bar API.

--- include/igraph_statusbar.h
+++ include/igraph_statusbar.h
@@ -36,13 +36,65 @@
 
 __BEGIN_DECLS
 
+/**
+ * \section about_status_handlers Status reporting
+ * 
+ * <para>
+ * In addition to the possibility of reporting the progress of an 
+ * igraph computation via \ref igraph_progress(), it is also possible 
+ * to report simple status messages from within igraph functions, 
+ * without having to judge how much of the computation was performed
+ * already. For this one needs to install a status handler function.
+ * </para>
+ * 
+ * <para>
+ * Status handler functions must be of type \ref igraph_status_handler_t 
+ * and they can be install by a call to \ref igraph_set_status_handler().
+ * Currently there is a simple predefined status handler function,
+ * called \ref igraph_status_handler_stderr(), but the user can define
+ * new ones.
+ * </para>
+ * 
+ * <para>
+ * Igraph functions report their status via a call to the 
+ * \ref IGRAPH_STATUS() or the \ref IGRAPH_STATUSF() macro.
+ * </para>
+ */
+ 
+/** 
+ * \typedef igraph_status_handler_t
+ * 
+ * The type of the igraph status handler functions
+ * \param message The status message.
+ * \param data Additional context, with user-defined semantics.
+ *        Existing igraph functions pass a null pointer here.
+ */
+
 typedef int igraph_status_handler_t(const char *message, void *data);
+
 extern igraph_status_handler_t igraph_status_handler_stderr;
 
 igraph_status_handler_t *
 igraph_set_status_handler(igraph_status_handler_t new_handler);
 
 int igraph_status(const char *message, void *data);
+
+/**
+ * \define IGRAPH_STATUS
+ * Report the status of an igraph function.
+ * 
+ * Typically this function is called only a handful of times from
+ * an igraph function. E.g. if an algorithm has three major 
+ * steps, then it is logical to call it three times, to 
+ * signal the three major steps.
+ * \param message The status message.
+ * \param data Additional context, with user-defined semantics.
+ *        Existing igraph functions pass a null pointer here.
+ * \return If the status handler returns with a value other than
+ *        \c IGRAPH_SUCCESS, then the function that called this 
+ *        macro returns as well, with error code 
+ *        \c IGRAPH_INTERRUPTED.
+ */
 
 #define IGRAPH_STATUS(message, data) \
   do { \
@@ -53,6 +105,22 @@ int igraph_status(const char *message, void *data);
   } while (0)
 
 int igraph_statusf(const char *message, void *data, ...);
+
+/** 
+ * \define IGRAPH_STATUSF
+ * Report the status from an igraph function 
+ *
+ * This is the more flexible version of \ref IGRAPH_STATUS(), 
+ * having a printf-like syntax. As this macro takes variable 
+ * number of arguments, they must be all supplied as a single 
+ * argument, enclosed in parentheses. Then \ref igraph_statusf() 
+ * is called with the given arguments.
+ * \param args The arguments to pass to \ref igraph_statusf().
+ * \return If the status handler returns with a value other than
+ *        \c IGRAPH_SUCCESS, then the function that called this 
+ *        macro returns as well, with error code 
+ *        \c IGRAPH_INTERRUPTED.
+ */
 
 #define IGRAPH_STATUSF(args) \
   do { \
--- src/statusbar.c
+++ src/statusbar.c
@@ -29,6 +29,26 @@
 
 static IGRAPH_THREAD_LOCAL igraph_status_handler_t *igraph_i_status_handler=0;
 
+/** 
+ * \function igraph_status
+ * Report status from an igraph function.
+ * 
+ * It calls the installed status handler function, if there is 
+ * one. Otherwise it does nothing. Note that the standard way to 
+ * report the status from an igraph function is the 
+ * \ref IGRAPH_STATUS or \ref IGRAPH_STATUSF macro, as these 
+ * take care of the termination of the calling function if the 
+ * status handler returns with \c IGRAPH_INTERRUPTED.
+ * \param message The status message.
+ * \param data Additional context, with user-defined semantics.
+ *        Existing igraph functions pass a null pointer here.
+ * \return Error code. If a status handler function was called 
+ *        and it did not return with \c IGRAPH_SUCCESS, then 
+ *        \c IGRAPH_INTERRUPTED is returned by \c igraph_status().
+ * 
+ * Time complexity: O(1).
+ */
+
 int igraph_status(const char *message, void *data) {
   if (igraph_i_status_handler) {
     if (igraph_i_status_handler(message, data) != IGRAPH_SUCCESS) { 
@@ -37,6 +57,25 @@ int igraph_status(const char *message, void *data) {
   }
   return IGRAPH_SUCCESS;
 }
+
+/**
+ * \function igraph_statusf
+ * Report status, more flexible printf-like version.
+ *
+ * This is the more flexible version of \ref igraph_status(), 
+ * that has a syntax similar to the \c printf standard C library function.
+ * It substitutes the values of the additional arguments into the 
+ * \p message template string and calls \ref igraph_status().
+ * \param message Status message template string, the syntax is the same 
+ *        as for the \c printf function.
+ * \param data Additional context, with user-defined semantics. 
+ *        Existing igraph functions pass a null pointer here.
+ * \param ... The additional arguments to fill the template given in the 
+ *        \p message argument.
+ * \return Error code. If a status handler function was called 
+ *        and it did not return with \c IGRAPH_SUCCESS, then 
+ *        \c IGRAPH_INTERRUPTED is returned by \c igraph_status().
+ */
 
 int igraph_statusf(const char *message, void *data, ...) {
   char buffer[300];
@@ -47,11 +86,38 @@ int igraph_statusf(const char *message, void *data, ...) {
 }
 
 #ifndef USING_R
+
+/**
+ * \function igraph_status_handler_stderr
+ * A simple predefined status handler function.
+ * 
+ * A simple status handler function, that writes the status 
+ * message to the standard errror.
+ * \param message The status message.
+ * \param data Additional context, with user-defined semantics.
+ *        Existing igraph functions pass a null pointer here.
+ * \return Error code.
+ * 
+ * Time complexity: O(1).
+ */
+
 int igraph_status_handler_stderr(const char *message, void *data) {
   fputs(message, stderr);
   return 0;
 }
 #endif
+
+/**
+ * \function igraph_set_status_handler
+ * Install of uninstall a status handler function.
+ *
+ * To uninstall the currently installed status handler, call
+ * this function with a null pointer.
+ * \param new_handler The status handler function to install.
+ * \return The previously installed status handler function.
+ * 
+ * Time complexity: O(1).
+ */
 
 igraph_status_handler_t *
 igraph_set_status_handler(igraph_status_handler_t new_handler) {
