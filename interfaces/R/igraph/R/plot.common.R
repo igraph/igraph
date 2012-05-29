c7fd7a4eb7c18014beecda907ae4cce12d61e868
Gabor Csardi <csardi.gabor@gmail.com>
2012-05-29 12:08:04 -0400

    R: removed some unused functions.

--- interfaces/R/igraph/R/plot.common.R
+++ interfaces/R/igraph/R/plot.common.R
@@ -110,88 +110,6 @@ i.parse.plot.params <- function(graph, params) {
   return (func)
 }
 
-i.get.layout <- function(graph, layout, layout.par) {
-
-  if (is.function(layout)) {
-    layout <- layout(graph, layout.par)
-  } else if (is.character(layout) && length(layout)==1 &&
-             substr(layout, 1, 2)=="a:") {
-    layout <- matrix(unlist(get.vertex.attribute(graph, substring(layout,3))),
-                     nrow=vcount(graph), byrow=TRUE)[,1:2]
-  }  
-
-  layout
-}
-
-i.get.vertex.color <- function(graph, vertex.color) {
-
-  if (length(vertex.color)==1 && substr(vertex.color, 1, 2)=="a:") {
-    vertex.color <- unlist(get.vertex.attribute(graph,
-                                                substring(vertex.color,3)))
-  }
-
-  if (is.numeric(vertex.color)) {
-    vertex.color <- vertex.color %% length(palette())
-    vertex.color[vertex.color==0] <- length(palette())
-    vertex.color <- palette()[vertex.color]
-  }
-
-  vertex.color  
-}
-
-i.get.vertex.frame.color <- function(graph, vertex.frame.color) {
-
-  if (length(vertex.frame.color)==1 &&
-      substr(vertex.frame.color, 1, 2)=="a:") {
-    vertex.frame.color <-
-      unlist(get.vertex.attribute(graph,
-                                  substring(vertex.frame.color,3)))
-  }
-
-  if (is.numeric(vertex.frame.color)) {
-    vertex.frame.color <- vertex.frame.color %% length(palette())
-    vertex.frame.color[vertex.frame.color==0] <- length(palette())
-    vertex.frame.color <- palette()[vertex.frame.color]
-  }
-
-  vertex.frame.color  
-}
-
-i.get.vertex.size <- function(graph, vertex.size) {
-
-  if (is.character(vertex.size) &&
-      length(vertex.size)==1 && substr(vertex.size, 1, 2)=="a:") {
-    vertex.size <- as.numeric(get.vertex.attribute
-                                 (graph, substring(vertex.size,3)))
-  }
-  vertex.size
-}
-
-i.get.edge.color <- function(graph, edge.color) {
-
-  if (length(edge.color)==1 && substr(edge.color, 1, 2)=="a:") {
-    edge.color <- as.character(get.edge.attribute
-                               (graph, substring(edge.color,3)))
-  }
-
-  if (is.numeric(edge.color)) {
-    edge.color <- edge.color %% length(palette())
-    edge.color[edge.color==0] <- length(palette())
-    edge.color <- palette()[edge.color]
-  }
-  edge.color
-}
-
-i.get.edge.width <- function(graph, edge.width) {
-
-  if (is.character(edge.width) &&
-      length(edge.width)==1 && substr(edge.width, 1, 2)=="a:") {
-    edge.width <- as.character(get.edge.attribute
-                               (graph, substring(edge.width,3)))
-  }
-  edge.width
-}
-
 i.get.edge.labels <- function(graph, edge.labels=NULL) {
 
   if (is.null(edge.labels)) {
@@ -199,16 +117,6 @@ i.get.edge.labels <- function(graph, edge.labels=NULL) {
   }
 
   edge.labels
-}
-
-i.get.label.degree <- function(graph, label.degree) {
-
-  if (is.character(label.degree) &&
-      length(label.degree)==1 && substr(label.degree, 1, 2)=="a:") {
-    label.degree <- as.numeric(get.vertex.attribute
-                               (graph, substring(label.degree,3)))
-  }
-  label.degree
 }
 
 i.get.labels <- function(graph, labels=NULL) {
