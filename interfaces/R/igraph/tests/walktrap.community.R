b42263df38945f91e1e5bbc64a8be91959a4ab34
Gabor Csardi <csardi.gabor@gmail.com>
2012-06-05 13:23:06 -0400

    R: fixed a test case.

--- interfaces/R/igraph/tests/walktrap.community.R
+++ interfaces/R/igraph/tests/walktrap.community.R
@@ -5,7 +5,7 @@ g <- graph.famous("Zachary")
 set.seed(42)
 wc <- walktrap.community(g)
 wc
-abs(modularity(g, membership(wc)) - modularity(wc)) < 1e-14
+abs(modularity(g, membership(wc)) - modularity(wc)) < 1e-7
 membership(wc)
 modularity(wc)
 length(wc)
@@ -15,4 +15,4 @@ d
 d[[1]]
 d[[2]]
 m2 <- cutat(wc, no=3)
-abs(modularity(g, m2) - wc$modularity[length(wc$modularity)-2]) < 1e-14
+abs(modularity(g, m2) - wc$modularity[length(wc$modularity)-2]) < 1e-7
