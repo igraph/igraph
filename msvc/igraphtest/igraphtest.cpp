f2625a0111209a5d94a9f07891405fe8bed44028
Gabor Csardi <csardi.gabor@gmail.com>
2012-06-13 13:49:03 -0400

    Updated MSVC example.

--- msvc/igraphtest/igraphtest.cpp
+++ msvc/igraphtest/igraphtest.cpp
@@ -8,8 +8,11 @@ int main(int argc, char* argv[])
 	igraph_t g;
 	FILE *outfile;
 
-	igraph_barabasi_game(&g, 1000, /*m=*/2, /*outseq=*/0, /*outpref=*/0, /*directed=*/1);
-	igraph_simplify(&g, /*multiple=*/1, /*loops=*/0);
+	igraph_barabasi_game(&g, 1000, /*power=*/ 1,/*m=*/2, /*outseq=*/0, 
+			     /*outpref=*/0, /*A=*/ 1, /*directed=*/1, 
+			     IGRAPH_BARABASI_PSUMTREE, 
+			     /*start_from=*/ 0);
+	igraph_simplify(&g, /*multiple=*/1, /*loops=*/0, /*edge_comb=*/ 0);
 	outfile=fopen("out.txt", "w");
 	igraph_write_graph_edgelist(&g, outfile);
 	fclose(outfile);
