e0b75f01d5a18fbcc46451656d96853ec44facd9
Gabor Csardi <csardi.gabor@gmail.com>
2012-06-21 15:52:42 -0400

    Removed unused internal function igraph_i_strdup.

--- src/other.c
+++ src/other.c
@@ -257,15 +257,3 @@ double igraph_i_fdiv(const double a, const double b)
    return a / b;
 }
 
-/**
- * Internal function, drop-in replacement for stdup
- * Used only in compilers that do not have strdup or _strdup
- */
-char* igraph_i_strdup(const char *s) {
-    size_t n = strlen(s) + 1;
-    char* result = (char*)malloc(sizeof(char) * n);
-    if (result)
-        memcpy(result, s, n);
-    return result;
-}
-
