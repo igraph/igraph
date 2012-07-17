103cd20f377cb8aab623836af183de699569ecc9
Tamas Nepusz <ntamas@gmail.com>
2012-07-17 12:11:14 +0200

    Python: update Graph.community_optimal for new API

--- interfaces/python/src/graphobject.c
+++ interfaces/python/src/graphobject.c
@@ -10601,23 +10601,17 @@ PyObject *igraphmodule_Graph_community_multilevel(igraphmodule_GraphObject *self
  * Optimal modularity by integer programming
  */
 PyObject *igraphmodule_Graph_community_optimal_modularity(
-	igraphmodule_GraphObject *self, PyObject *args, PyObject *kwds) {
-  static char *kwlist[] = { "verbose", NULL };
+	igraphmodule_GraphObject *self) {
   igraph_real_t modularity;
   igraph_vector_t membership;
-  PyObject *res, *verbose_o = Py_False;
-
-  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|O", kwlist, &verbose_o)) {
-    return NULL;
-  }
+  PyObject *res;
 
   if (igraph_vector_init(&membership, igraph_vcount(&self->g))) {
 	igraphmodule_handle_igraph_error();
     return NULL;
   }
 
-  if (igraph_community_optimal_modularity(&self->g, &modularity, &membership,
-        PyObject_IsTrue(verbose_o))) {
+  if (igraph_community_optimal_modularity(&self->g, &modularity, &membership)) {
 	igraphmodule_handle_igraph_error();
 	igraph_vector_destroy(&membership);
     return NULL;
@@ -14313,8 +14307,8 @@ struct PyMethodDef igraphmodule_Graph_methods[] = {
   },
   {"community_optimal_modularity",
    (PyCFunction) igraphmodule_Graph_community_optimal_modularity,
-   METH_VARARGS | METH_KEYWORDS,
-   "community_optimal_modularity(verbose=False)\n\n"
+   METH_NOARGS,
+   "community_optimal_modularity()\n\n"
    "Calculates the optimal modularity score of the graph and the\n"
    "corresponding community structure.\n\n"
    "This function uses the GNU Linear Programming Kit to solve a large\n"
