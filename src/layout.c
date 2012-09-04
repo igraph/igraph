c07f2799f3f4fbc644e370f2e1e816d103e50fbe
Gabor Csardi <csardi.gabor@gmail.com>
2012-09-04 15:19:44 -0400

    Two-row (or column) layout generator for bipartite graphs.

--- src/layout.c
+++ src/layout.c
@@ -3132,3 +3132,54 @@ int igraph_layout_mds(const igraph_t* graph, igraph_matrix_t *res,
   return IGRAPH_SUCCESS;
 }
 
+/**
+ * \function igraph_layout_bipartite
+ * Simple layout for bipartite graphs
+ * 
+ * The layout is created by first placing the vertices in two rows,
+ * according to their types. Then the positions within the rows are
+ * optimized to minimize edge crossings, by calling \ref
+ * igraph_layout_sugiyama().
+ * 
+ * \param graph The input graph.
+ * \param types A boolean vector containing ones and zeros, the vertex
+ *     types. Its length must match the number of vertices in the graph.
+ * \param res Pointer to an initialized matrix, the result, the x and
+ *     y coordinates are stored here.
+ * \param hgap The preferred minimum horizontal gap between vertices
+ *     in the same layer (i.e. vertices of the same type).
+ * \param vgap  The distance between layers.
+ * \param maxiter Maximum number of iterations in the crossing
+ *     minimization stage. 100 is a reasonable default; if you feel
+ *     that you have too many edge crossings, increase this.
+ * \return Error code.
+ * 
+ * \sa \ref igraph_layout_sugiyama().
+ */
+
+int igraph_layout_bipartite(const igraph_t *graph, 
+			    const igraph_vector_bool_t *types,
+			    igraph_matrix_t *res, igraph_real_t hgap, 
+			    igraph_real_t vgap, long int maxiter) {
+
+  long int i, no_of_nodes=igraph_vcount(graph);
+  igraph_vector_t layers;
+
+  if (igraph_vector_bool_size(types) != no_of_nodes) { 
+    IGRAPH_ERROR("Invalid vertex type vector size", IGRAPH_EINVAL);
+  }
+
+  IGRAPH_VECTOR_INIT_FINALLY(&layers, no_of_nodes);
+  for (i=0; i<no_of_nodes; i++) {
+    VECTOR(layers)[i] = 1 - VECTOR(*types)[i];
+  }
+
+  IGRAPH_CHECK(igraph_layout_sugiyama(graph, res, /*extd_graph=*/ 0, 
+				      /*extd_to_orig_eids=*/ 0, &layers, hgap,
+				      vgap, maxiter, /*weights=*/ 0));
+  
+  igraph_vector_destroy(&layers);
+  IGRAPH_FINALLY_CLEAN(1);
+  
+  return 0;
+}
