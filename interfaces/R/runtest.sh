b90d017076bbd7a3f4a34941b66d6212ce8491e5
Gabor Csardi <csardi.gabor@gmail.com>
2012-09-07 00:38:42 -0400

    R: make it possible to run individual test cases, fixes bug #1008756.

---
+++ interfaces/R/runtest.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+cd igraph/tests
+tmpdir=`mktemp -d $TMPDIR/igraph-XXXXXX`
+
+## Copy unknown files to the test directory, just to be sure
+
+otherfiles=`ls | grep -v '\.R$' | grep -v '\.Rout$' | grep -v '\.Rout.save'`
+cp $otherfiles $tmpdir
+
+if [ -z "$@" ]; then
+    ## If no arguments, then run all tests
+    cp * $tmpdir
+else
+    ## Else copy all test files that match any of the arguments
+    for arg in "$@"; do
+	cp `ls | grep $arg` $tmpdir
+    done
+fi
+
+## Run the tests
+cd $tmpdir && echo "tools:::.runPackageTestsR()" | R --no-save && echo
