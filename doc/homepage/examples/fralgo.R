12ea7113f3749e982dfc587b98b54ee41f488a8b
Gabor Csardi <csardi.gabor@gmail.com>
2012-10-19 01:00:13 -0400

    Put the homepage screenshot examples into seperate files. First step towards testing them, bug #1047183.

---
+++ doc/homepage/examples/fralgo.R
@@ -0,0 +1,18 @@
+library(igraph)
+library(Cairo)
+
+graphs <- frgraphs()
+lay <- lapply(graphs, layout.fruchterman.reingold, niter=3000)
+
+CairoX11()
+# CairoPNG(file="frplots.png")
+par(mai=c(0,0,0,0))
+layout(matrix(1:16, nr=4, byrow=TRUE))
+layout.show(16)
+for (i in seq(along=graphs)) {
+  plot(graphs[[i]], layout=lay[[i]],
+       vertex.label=NA, vertex.size=13, edge.color="black",
+       vertex.color="red")
+}
+# dev.off()
+
---
+++ doc/homepage/examples/tkplot.R
@@ -0,0 +1,15 @@
+library(igraph)
+g <- read.graph("karate.net", format="pajek")
+
+cs <- leading.eigenvector.community.step(g)
+V(g)$color <- ifelse(cs$membership==0, "lightblue", "green")
+
+scale <- function(v, a, b) {
+  v <- v-min(v) ; v <- v/max(v) ; v <- v * (b-a) ; v+a
+}
+
+V(g)$size <- scale(abs(cs$eigenvector), 10, 20)
+E(g)$color <- "grey"
+E(g)[ V(g)[ color=="lightblue" ] %--% V(g)[ color=="green" ] ]$color <- "red"
+
+tkplot(g, layout=layout.kamada.kawai, vertex.label.font=2)
