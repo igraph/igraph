91230240c59bc839adede03ec195888edecdce01
Tamas Nepusz <ntamas@gmail.com>
2012-10-24 17:25:02 +0200

    updated Python examples on homepage

--- doc/homepage/examples/mst.py
+++ doc/homepage/examples/mst.py
@@ -3,8 +3,8 @@ from igraph import *
 def distance(p1, p2):
     return ((p1[0]-p2[0]) ** 2 + (p1[1]-p2[1]) ** 2) ** 0.5
     
-g, xs, ys = Graph.GRG(100, 0.2, return_coordinates=True)
-layout = Layout(zip(xs, ys))
+g = Graph.GRG(100, 0.2)
+layout = Layout(zip(g.vs["x"], g.vs["y"]))
 
 weights = [distance(layout[edge.source], layout[edge.target]) for edge in g.es]
 max_weight = max(weights)
