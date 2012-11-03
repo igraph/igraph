54de7d68d2e85a98bbc872ac0546756adc8949e7
Tamas Nepusz <ntamas@gmail.com>
2012-11-03 18:45:13 +0100

    fixed failing igraph_k_regular_game test case

--- src/games.c
+++ src/games.c
@@ -1215,7 +1215,9 @@ int igraph_degree_sequence_game(igraph_t *graph, const igraph_vector_t *out_deg,
 
   int retval;
 
-  if (in_deg && igraph_vector_size(in_deg)==0) { in_deg=0; }
+  if (in_deg && igraph_vector_empty(in_deg) && !igraph_vector_empty(out_deg)) {
+    in_deg=0;
+  }
   if (method==IGRAPH_DEGSEQ_SIMPLE) {
     retval=igraph_degree_sequence_game_simple(graph, out_deg, in_deg);
   } else if (method==IGRAPH_DEGSEQ_VL) {
