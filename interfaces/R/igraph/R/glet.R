1fe06c525614258c897813b834145ceaf0a392ec
Gabor Csardi <csardi.gabor@gmail.com>
2013-01-22 17:41:38 -0500

    GL: add some example code.

--- interfaces/R/igraph/R/glet.R
+++ interfaces/R/igraph/R/glet.R
@@ -119,3 +119,44 @@ Summary.net <- function(Data.adj) {
   ## hist(Data.adj[which(Data.adj>0)])
   ## dev.off() 
 }
+
+#################
+## Example code
+
+function() {
+  D1 <- matrix(0, 5, 5)
+  D2 <- matrix(0, 5, 5)
+  D3 <- matrix(0, 5, 5)
+  D1[1:3, 1:3] <- 2
+  D2[3:5, 3:5] <- 3
+  D3[2:5, 2:5] <- 1
+  
+  D <- D1 + D2 + D3
+  
+  gl <- graphlets(D, iter=1000)
+  
+  g <- graph.adjacency(D, weighted=TRUE, mode="undirected", diag=FALSE)
+  V(g)$color <- "white"
+  E(g)$label <- E(g)$weight
+  E(g)$label.cex <- 2
+  E(g)$color <- "black"
+
+  plot.new()
+  layout(matrix(1:6, nrow=2, byrow=TRUE))
+  co <- layout.kamada.kawai(g)
+  par(mar=c(1,1,1,1))
+  plot(g, layout=co)
+  for (i in 1:ncol(gl$Bc)) {
+    sel <- gl$Bc[,i] != 0
+    V(g)$color <- "white"
+    V(g)[sel]$color <- "#E495A5"
+    E(g)$width <- 1
+    E(g)[ V(g)[sel] %--% V(g)[sel] ]$width <- 2
+    E(g)$label <- ""
+    E(g)[ width == 2 ]$label <- round(gl$Muc[i], 2)
+    E(g)$color <- "black"
+    E(g)[ width == 2 ]$color <- "#E495A5"
+    plot(g, layout=co)
+  }
+}
+
