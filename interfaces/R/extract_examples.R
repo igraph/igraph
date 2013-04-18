857dfda622b550abe027d5d5e94d1466994a39f7
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-04-18 12:11:13 -0400

    R: make manual example extracter ignore backup files.

--- interfaces/R/extract_examples.R
+++ interfaces/R/extract_examples.R
@@ -2,7 +2,7 @@
 
 library(tools)
 
-rdfiles <- list.files("igraph/man", full.names=TRUE)
+rdfiles <- list.files("igraph/man", pattern=".*\\.Rd$", full.names=TRUE)
 out <- file("igraph-Ex.R", open="w")
 cat("### Load the package\nlibrary(igraph)\n\n", file=out)
 sapply(rdfiles, Rd2ex, out=out)
