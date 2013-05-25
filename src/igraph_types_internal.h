ca61f81c5db10e9021e5f3ea6bcb3fe15e4a8560
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-05-25 16:21:16 -0400

    Reimplement push-relabel maximum flow with gap heuristics, only the value calculation so far.

--- src/igraph_types_internal.h
+++ src/igraph_types_internal.h
@@ -288,8 +288,8 @@ int igraph_hashtable_reset(igraph_hashtable_t *ht);
 /* Buckets, needed for the maximum flow algorithm */
 
 typedef struct igraph_buckets_t {
-  igraph_vector_t bptr;
-  igraph_vector_t buckets;
+  igraph_vector_long_t bptr;
+  igraph_vector_long_t buckets;
   igraph_integer_t max, no;
 } igraph_buckets_t;
 
@@ -297,9 +297,31 @@ int igraph_buckets_init(igraph_buckets_t *b, long int bsize, long int size);
 void igraph_buckets_destroy(igraph_buckets_t *b);
 void igraph_buckets_clear(igraph_buckets_t *b);
 long int igraph_buckets_popmax(igraph_buckets_t *b);
+long int igraph_buckets_pop(igraph_buckets_t *b, long int bucket);
 igraph_bool_t igraph_buckets_empty(const igraph_buckets_t *b);
+igraph_bool_t igraph_buckets_empty_bucket(const igraph_buckets_t *b, 
+					  long int bucket);
 void igraph_buckets_add(igraph_buckets_t *b, long int bucket,
-		       igraph_real_t elem);
+			long int elem);
+
+typedef struct igraph_dbuckets_t {
+  igraph_vector_long_t bptr;
+  igraph_vector_long_t next, prev;
+  igraph_integer_t max, no;
+} igraph_dbuckets_t;
+
+int igraph_dbuckets_init(igraph_dbuckets_t *b, long int bsize, long int size);
+void igraph_dbuckets_destroy(igraph_dbuckets_t *b);
+void igraph_dbuckets_clear(igraph_dbuckets_t *b);
+long int igraph_dbuckets_popmax(igraph_dbuckets_t *b);
+long int igraph_dbuckets_pop(igraph_dbuckets_t *b, long int bucket);
+igraph_bool_t igraph_dbuckets_empty(const igraph_dbuckets_t *b);
+igraph_bool_t igraph_dbuckets_empty_bucket(const igraph_dbuckets_t *b, 
+					   long int bucket);
+void igraph_dbuckets_add(igraph_dbuckets_t *b, long int bucket,
+			 long int elem);
+void igraph_dbuckets_delete(igraph_dbuckets_t *b, long int bucket,
+			    long int elem);
 
 /* Special maximum heap, needed for the minimum cut algorithm */
 
