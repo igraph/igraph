13cbaf76096fa44cf506b4bd10b422b9ca643b92
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-06-21 22:44:11 -0400

    R: move remaining test cases into the package.

---
+++ interfaces/R/igraph/inst/tests/test_graph.subisomorphic.lad.R
@@ -0,0 +1,42 @@
+
+context("graph.subisomorphic.lad")
+
+test_that("graph.subisomorphic.lad works", {
+
+  library(igraph)
+
+  pattern <- graph.formula(1:2:3:4:5,
+                           1 - 2:5, 2 - 1:5:3, 3 - 2:4, 4 - 3:5, 5 - 4:2:1)
+  target <- graph.formula(1:2:3:4:5:6:7:8:9,
+                          1 - 2:5:7, 2 - 1:5:3, 3 - 2:4, 4 - 3:5:6:8:9,
+                          5 - 1:2:4:6:7, 6 - 7:5:4:9, 7 - 1:5:6,
+                          8 - 4:9, 9 - 6:4:8)
+  domains <- list(`1` = c(1,3,9), `2` = c(5,6,7,8), `3` = c(2,4,6,7,8,9),
+                  `4` = c(1,3,9), `5` = c(2,4,8,9))
+  i1 <- graph.subisomorphic.lad(pattern, target, all.maps=TRUE)
+  i2 <- graph.subisomorphic.lad(pattern, target, induced=TRUE,
+                                all.maps=TRUE)
+  i3 <- graph.subisomorphic.lad(pattern, target, domains=domains,
+                                all.maps=TRUE)
+
+  expect_that(i1$iso, is_true())
+  expect_that(i2, equals(
+    structure(list(iso = TRUE, map = structure(c(1, 2, 3, 4, 5),
+                                 .Names = c("1", "2", "3", "4", "5")),
+                   maps = list(structure(c(1, 2, 3, 4, 5),
+                     .Names = c("1", "2", "3", "4", "5")),
+                     structure(c(6, 4, 3, 2, 5),
+                               .Names = c("6", "4", "3", "2", "5")),
+                     structure(c(6, 5, 2, 3, 4),
+                               .Names = c("6", "5", "2", "3", "4")),
+                     structure(c(1, 5, 4, 3, 2),
+                               .Names = c("1", "5", "4", "3", "2")))),
+              .Names = c("iso", "map", "maps")) ))
+  expect_that(i3, equals(
+    structure(list(iso = TRUE, map = structure(c(1, 5, 4, 3, 2),
+                                 .Names = c("1", "5", "4", "3", "2")),
+                   maps = list(structure(c(1, 5, 4, 3, 2),
+                     .Names = c("1", "5", "4", "3", "2")))),
+              .Names = c("iso", "map", "maps")) ))
+
+})
