cd9b76d222314a426fd2036fbea277a014dc6772
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-09-26 15:31:04 -0400

    Fix maximal cliques R test case (#513).

--- interfaces/R/igraph/inst/tests/test_graphlets.R
+++ interfaces/R/igraph/inst/tests/test_graphlets.R
@@ -9,7 +9,7 @@ test_that("Getting subcliques works", {
   ids <- 1:vcount(g)
 
   cl <- maximal.cliques(g)
-  cl <- lapply(cl, "-", 1)
+  cl <- lapply(cl, "-", 1)[c(9, 2, 3, 10, 5, 7, 6, 1, 4, 8)]
 
   res <- .Call("R_igraph_subclique_next", g, E(g)$weight, ids, cl,
                PACKAGE="igraph")
@@ -21,13 +21,13 @@ test_that("Getting subcliques works", {
   
   expect_that(res$thr, equals(c(7,2,5,3,1,7,3,2,4,7)))
   expect_that(res$next_thr, equals(c(Inf, 4, 8, 8, Inf, 9, 5, 3, 5, Inf)))
-  expect_that(res$weights, equals(list(numeric(), c(4,8), c(8,8), c(8,9),
+  expect_that(res$weights, equals(list(numeric(), c(4,8), c(8,8), c(9,8),
                                        numeric(), c(9,9), c(7,7,9,10,5),
-                                       c(7,4,5,3), c(7,5,5,9,10),
+                                       c(7,3,4,5), c(5,7,5,9,10),
                                        numeric())))
   expect_that(res$ids, equals(list(integer(), c(5,9,10), c(1,9,10),
-                                   c(1,9,8), integer(), c(3,7,6),
-                                   c(3,5,6,4), c(3,5,2,10), c(3,5,2,4),
+                                   c(1,8,9), integer(), c(3,7,6),
+                                   c(3,6,5,4), c(3,5,10,2), c(2,5,3,4),
                                    integer())))
   expect_that(sapply(res$graphs, vcount), equals(sapply(res$ids, length)))
   expect_that(sapply(res$graphs, ecount),
