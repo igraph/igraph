c5ebeeb0038571af2dddb2de0344a9d68ff093bc
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-10-09 12:41:05 -0400

    Nightly: better test data, it is actually meaningful (#514).

--- doc/nightly/test_data.sh
+++ doc/nightly/test_data.sh
@@ -2,18 +2,12 @@
 
 echo "
 INSERT INTO 'files'
-       VALUES ('c/igraph-0.7-pre+41.badcafe.tar.gz', 'c', '0.7-pre', 'develop',
-               'f5115b6', '2013-09-04', '1200000', 0);
+       VALUES ('c/igraph-0.6.6-pre+41.849c191.tar.gz', 'c', '0.6.6-pre',
+               'release-0.6', '849c191', '2013-10-09', 2886425, 0);
 INSERT INTO 'files'
-       VALUES ('c/igraph-0.6.5.tar.gz.', 'r', '0.6.5', 'master',
-               'badcafe', '2013-09-03', '1200000', 0);
+       VALUES ('r/igraph_0.6.5.999-41.tar.gz', 'r', '0.6.6-pre',
+               'release-0.6', '849c191', '2013-10-08', 2316191, 0);
 INSERT INTO 'files'
-       VALUES ('python/python-igraph-0.6.5.tar.gz', 'python', '0.6.5', 'master',
-               'f5115bb', '2013-08-05', '1200000', 0);
-INSERT INTO 'files'
-       VALUES ('c/igraph-0.7-pre+25.badcafe.tar.gz', 'c', '0.7', 'develop',
-               'badcafe', '2013-09-02', '1200000', 0);
-INSERT INTO 'files'
-       VALUES ('r/igraph-0.7-pre+14.badcafe.tar.gz', 'r', '0.7', 'develop',
-               'badcafe', '2013-09-01', '1200000', 0);
+       VALUES ('r/igraph_0.6.999-523.tar.gz', 'r', '0.7.0-pre',
+               'develop', '50c1ec0', '2013-10-08', 2367155, 0);
 " | sqlite3 nightly.db
