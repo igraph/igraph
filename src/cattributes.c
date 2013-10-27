384b3fd786ea8aa5e7e7507ecca811790011e060
Tamas Nepusz <ntamas@gmail.com>
2013-10-27 23:33:24 +0100

    moving variable declarations to the beginning of the function

--- src/cattributes.c
+++ src/cattributes.c
@@ -98,10 +98,11 @@ int igraph_i_cattributes_copy_attribute_record(igraph_attribute_record_t **newre
 int igraph_i_cattribute_init(igraph_t *graph, igraph_vector_ptr_t *attr) {
   igraph_attribute_record_t *attr_rec;
   long int i, n;
+  igraph_i_cattributes_t *nattr;
 
   n = attr ? igraph_vector_ptr_size(attr) : 0;
 
-  igraph_i_cattributes_t *nattr=igraph_Calloc(1, igraph_i_cattributes_t);
+  nattr=igraph_Calloc(1, igraph_i_cattributes_t);
   if (!nattr) {
     IGRAPH_ERROR("Can't init attributes", IGRAPH_ENOMEM);
   }
