a7cc251aa6580ae942ef40a5949be3e932bb6758
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-11-05 10:29:30 -0500

    WWW: rewrite links to old docs, refer back to sourceforge.

--- www/_plugins/biggerh1.rb
+++ www/_plugins/biggerh1.rb
@@ -1,10 +1,10 @@
 # Put the H1 into a div, for bootstrap
 module Jekyll
-  module PostFilter
+  module BiggerH1Filter
     def biggerh1(input)
       input.gsub(/(<h1[^>]*>.*<\/h1>)/, "<div class=\"page-header\">\\1</div>")
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::PostFilter)
+Liquid::Template.register_filter(Jekyll::BiggerH1Filter)
