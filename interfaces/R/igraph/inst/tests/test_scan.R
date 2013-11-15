761df45132db47cb648ac6b7b56bbdae9274485c
Gabor Csardi <csardi.gabor@gmail.com>
2013-11-15 10:30:57 -0500

    R: updated scan1() test to proper test_that() usage, preparing for #5.

--- interfaces/R/igraph/inst/tests/test_scan.R
+++ interfaces/R/igraph/inst/tests/test_scan.R
@@ -1,34 +1,27 @@
 
+context("Scan-1")
 
-library(igraph)
-set.seed(42)
-
-for (i in 1:100) {
-  cat(".")
-  g <- erdos.renyi.game(100, 10/100)
+test_that("scan-1 statistics works", {
   
-  s1 <- scan1(g)
-  
-  s1a <- scan1.approx(g, 20)$res
-  cor(s1, s1a)
+  library(igraph)
+  set.seed(42)
 
-  E <- graph.eigen(g, which=list(howmany=20, pos="LM"))
-  s1aa <- scan1.approx.eigen(g, E$values, E$vectors)
-  cor(s1, s1aa)
+  for (i in 1:100) {
 
-  E2 <- eigen(get.adjacency(g, sparse=FALSE))
-  s1aaa <- colSums(E2$values ^3 * t(E2$vectors)^2 / 2) + degree(g)
-  max(abs(s1aaa - s1))
+    g <- erdos.renyi.game(100, 10/100)
+    
+    s1 <- scan1(g)
+    s1a <- scan1.approx(g, 20)$res
+    expect_that(cor(s1, s1a) > 0.95, is_true())
 
-  std <- function(x) {
-    x <- zapsmall(x)
-    apply(x, 2, function(col) {
-      if (any(col < 0) && col[which(col != 0)[1]] < 0) { -col } else { col }
-    })
+    E <- graph.eigen(g, which=list(howmany=20, pos="LM"))
+    s1aa <- scan1.approx.eigen(g, E$values, E$vectors)
+    expect_that(cor(s1, s1aa) > 0.95, is_true())
+
+    E2 <- eigen(get.adjacency(g, sparse=FALSE))
+    s1aaa <- colSums(E2$values ^3 * t(E2$vectors)^2 / 2) + degree(g)
+    expect_that(s1aaa, equals(s1))
+
   }
 
-  ord <- order(abs(E2$values), decreasing=TRUE)
-  max(abs(E$values - E2$values[ord[1:20]]))
-  max(abs(std(E$vectors) - std(E2$vectors[,ord[1:20]])))
-
-}
+})
