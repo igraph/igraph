89ad4cdc75491dbe8b6570b0f8d7d3cf9e7c9bbc
Gabor Csardi <csardi.gabor@gmail.com>
2013-11-18 10:34:58 -0500

    Packer: provisioner to install Python versions on OSX and ubuntu.

--- tools/virtual/packer/scripts/igraphdeps.sh
+++ tools/virtual/packer/scripts/igraphdeps.sh
@@ -2,4 +2,5 @@
 
 apt-get -y install autoconf automake bison flex libtool \
     libxml2-dev libgmp-dev docbook2x source-highlight libxml2-utils \
-    mesa-common-dev libglu1-mesa-dev libpng-dev curl
+    mesa-common-dev libglu1-mesa-dev libpng-dev curl libreadline6-dev \
+    libsqlite3-dev make libssl-dev zlib1g-dev libbz2-dev
