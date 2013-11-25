596cb8af4e64ef11ed87e0ac7cf366c2e8e2d9bc
Gabor Csardi <csardi.gabor@gmail.com>
2013-11-24 23:35:31 -0500

    R: better test cases for dimSelect (#9).

--- interfaces/R/igraph/inst/tests/test_dimSelect.R
+++ interfaces/R/igraph/inst/tests/test_dimSelect.R
@@ -17,5 +17,17 @@ test_that("dimensionality selection works", {
   expect_that(dimSelect(runif(100)), equals(88))
   expect_that(dimSelect(runif(100)), equals(3))
   expect_that(dimSelect(runif(100)), equals(99))
-  
+
+  ## Some more meaningful tests
+  x <- c(rnorm(50, mean=0, sd=1), rnorm(50, mean=5, sd=1))
+  expect_that(dimSelect(x), equals(50))
+
+  x <- c(rnorm(10, mean=0, sd=1), rnorm(90, mean=2, sd=1))
+  expect_that(dimSelect(x), equals(10))
+
+  x <- c(10, rnorm(99, mean=0, sd=1))
+  expect_that(dimSelect(x), equals(1))
+
+  x <- c(rnorm(99, mean=0, sd=1), 10)
+  expect_that(dimSelect(x), equals(99))
 })
