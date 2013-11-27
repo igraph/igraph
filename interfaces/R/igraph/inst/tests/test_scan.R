54d6cdfca8c15d454542a13d14574f9e6eec867b
Gabor Csardi <csardi.gabor@gmail.com>
2013-11-27 15:12:33 -0500

    R: local.scan tests compare to digest, not baseline function (#5).
    Because baseline function is slow, and local.scan is expected
    to be much faster, soon.

--- interfaces/R/igraph/inst/tests/test_scan.R
+++ interfaces/R/igraph/inst/tests/test_scan.R
@@ -26,65 +26,11 @@ test_that("Local scan-1 statistics works", {
 
 })
 
-my.local.scan <- function(g, gp=NULL, k=1, mode="out", FUN=ecount,
-                          weighted=FALSE) {
-
-  wstat <- function(g,...) {
-    A <- get.adjacency(g, attr="weight")
-    if (k==0) { # weighted degree
-      indeg <- colSums(A)
-      outdeg <- rowSums(A)
-      if (mode=="in") {
-        out <- indeg
-      } else if (mode=="out") {
-        out <- outdeg
-      } else {
-        out <- indeg + outdeg
-      }
-    } else { # weighted ecount
-      out <- sum(A)
-      if (mode=="in" | mode=="out") out <- out/2
-    }
-    return(out)
-  }
-
-  if(k < 0) { stop("Error: k should be a non-negative integer!\n") }
-  if(k == 0) { FUN <- degree }
-  if(weighted) { FUN <- wstat }
-
-  require(igraph)
-
-  if (is.matrix(g) | is.matrix(gp)) {
-    gmode <- ifelse((mode=="out" | mode=="in"),"directed","undirected")
-    g <- simplify(graph.adjacency(g, mode=gmode))
-    if (!is.null(gp)) { gp <- simplify(graph.adjacency(gp, mode=gmode)) }
-  }
-
-  n <- vcount(g)
-  if (is.null(gp)) {
-    if (k==0) {
-      out <- FUN(g,mode=mode)
-    } else {
-      out <- sapply(graph.neighborhood(g, k, V(g), mode), FUN)
-    }
-  } else { # them
-    if (k==0) {
-      out <- unlist(sapply(1:n, function(x) {
-        vid <- unlist(neighborhood(g, k+1, x, mode));
-        x.rank <- which(sort(vid)==x);
-        FUN(induced.subgraph(gp, vid), mode=mode)[x.rank]}))
-    } else {
-      out <- sapply(V(g), function(x) {
-        FUN(induced.subgraph(gp,unlist(neighborhood(g, k, x, mode))))})
-    }
-  }
-  return(out)
-}
-
 test_that("General scan-stat works", {
 
   require(igraph)
   require(Matrix)
+  require(digest)
 
   set.seed(12345)
   n <- 10^3
@@ -96,62 +42,50 @@ test_that("General scan-stat works", {
 
   ## us: scan0, unweighted
   system.time(s1 <- local.scan(g, k=0))
-  system.time(s2 <- my.local.scan(g, k=0))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("659ffaaf303742f0806a79b8ff3d88b3"))
 
   ## us: scan0, weighted
   system.time(s1 <- local.scan(g, k=0, weighted=TRUE))
-  system.time(s2 <- my.local.scan(g, k=0, weighted=TRUE))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("0f8d7ac831389cea04e0bfc5e2510c73"))
 
   ## us: scan1, unweighted
   system.time(s1 <- local.scan(g))
-  system.time(s2 <- my.local.scan(g, k=1))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("df0fd77489f70cc47f682dc31d9f52f5"))
 
   ## us: scan1, weighted
   system.time(s1 <- local.scan(g, k=1, weighted=TRUE))
-  system.time(s2 <- my.local.scan(g, k=1, weighted=TRUE))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("af720916ae4b49881745d2dcdd614401"))
 
   ## us: scan2, unweighted
   system.time(s1 <- local.scan(g, k=2))
-  system.time(s2 <- my.local.scan(g, k=2))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("6f47f47abde25d00d615dd56826cca5a"))
 
   ## us: scan2, weighted
   system.time(s1 <- local.scan(g, k=2, weighted=TRUE))
-  system.time(s2 <- my.local.scan(g, k=2, weighted=TRUE))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("e02e9d58168ee5d53850497f6d4c76b0"))
 
   ## them: scan0, unweighted
   system.time(s1 <- local.scan(g, gp, k=0))
-  system.time(s2 <- my.local.scan(g, gp, k=0))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("f584f7d287f8f89f5f7882165ca41b8c"))
 
   ## them: scan0, weighted
   system.time(s1 <- local.scan(g, gp, k=0, weighted=TRUE))
-  system.time(s2 <- my.local.scan(g, gp, k=0, weighted=TRUE))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("213db8e7517d1e6406da3dbd55281ed1"))
 
   ## them: local.scan, unweighted
   system.time(s1 <- local.scan(g, gp, k=1))
-  system.time(s2 <- my.local.scan(g, gp, k=1))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("e9ca740ebba2fd1db4abe939954b2638"))
 
   ## them: local.scan, weighted
   system.time(s1 <- local.scan(g, gp, k=1, weighted=TRUE))
-  system.time(s2 <- my.local.scan(g, gp, k=1, weighted=TRUE))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("a98e9a03eda7feaae8524dc9348ad74b"))
 
   ## them: scan2, unweighted
   system.time(s1 <- local.scan(g, gp, k=2))
-  system.time(s2 <- my.local.scan(g, gp, k=2))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("a3237a9a55e9d86ab471c81a291eb03b"))
 
   ## them: scan2, weighted
   system.time(s1 <- local.scan(g, gp, k=2, weighted=TRUE))
-  system.time(s2 <- my.local.scan(g, gp, k=2, weighted=TRUE))
-  expect_that(s1, equals(s2))
+  expect_that(digest(s1), equals("995d0b6a952834ff6e534efc2cfb917b"))
 
 })
