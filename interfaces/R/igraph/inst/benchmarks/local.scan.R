9811fbf0f481ebefaca4ae8ff5f80bc551620d3d
Gabor Csardi <csardi.gabor@gmail.com>
2013-11-27 01:20:03 -0500

    Rewrote test local.scan benchmarks with timethat package.

--- interfaces/R/igraph/inst/benchmarks/local.scan.R
+++ interfaces/R/igraph/inst/benchmarks/local.scan.R
@@ -1,22 +1,13 @@
 
-benchmark_context(
-  id="local.scan",
-  version="1"
-  )
+time_group("local scan v1")
 
-benchmark_this("",
-  init = {
-    library(igraph)
-    set.seed(42)
-  },
-  init_each = {
-    g <- random.graph.game(1000, p=.2)    
-  },
-  replications = 10,
-  scan0 = {
-    ls0 <- local.scan(g, k=0)
-    error
-  },
-  scan1 = {
-    ls1 <- local.scan(g)
-  })
+init   <- expression({library(igraph); set.seed(42) })
+reinit <- expression({g <- random.graph.game(1000, p=.2)})
+
+time_that("scan0 on random graphs",
+          replications=10, init=init, reinit=reinit,
+          { ls0 <- local.scan(g, k=0) })
+
+time_that("scan1 on random graphs",
+          replications=10, init=init, reinit=reinit,
+          { ls1 <- local.scan(g) })
