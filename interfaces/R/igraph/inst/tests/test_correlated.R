d8fe7ae69cfcb80339821a59592044140e6ae1ad
Gabor Csardi <csardi.gabor@gmail.com>
2013-12-07 17:38:05 -0500

    R: added more correlated.game test cases (#8).

--- interfaces/R/igraph/inst/tests/test_correlated.R
+++ interfaces/R/igraph/inst/tests/test_correlated.R
@@ -28,3 +28,25 @@ test_that("correlated.pair.game works", {
   expect_that(abs(ecount(gp[[1]]) - ecount(gp[[2]])) < 3, is_true())
 
 })
+
+## Some corner cases
+
+test_that("correlated.game corner cases work", {
+
+  library(igraph)
+  set.seed(42)
+
+  is.full <- function(g) {
+    g2 <- graph.full(vcount(g), directed=is.directed(g))
+    graph.isomorphic(g, g2)
+  }
+
+  g <- erdos.renyi.game(10, .3)
+  g2 <- correlated.game(g, corr=0.000001, p=.99999999)
+  expect_that(is.full(g2), is_true())
+
+  g3 <- correlated.game(g, corr=0.000001, p=0.0000001)
+  expect_that(ecount(g3), equals(0))
+  expect_that(vcount(g3), equals(10))
+
+})
