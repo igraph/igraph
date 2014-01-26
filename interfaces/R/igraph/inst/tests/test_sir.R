a528394d3ac3838d0cd3e90e4ffa735a0ab8936e
Gabor Csardi <csardi.gabor@gmail.com>
2014-01-25 22:18:36 -0500

    R: fix a test case that is 32/64 bit dependent.

--- interfaces/R/igraph/inst/tests/test_sir.R
+++ interfaces/R/igraph/inst/tests/test_sir.R
@@ -9,6 +9,10 @@ test_that("SIR works", {
 
   g <- erdos.renyi.game(50, 50, type="gnm")
   res <- sir(g, beta=5, gamma=1, no.sim=10)
-  expect_that(digest(res), equals("bc42d0cbe0bb3321e83979c0432f9cea"))
+  if (.Machine$sizeof.pointer == 4) {
+    expect_that(digest(res), equals("b73a8ad03b832b3543f2f03d07330398"))
+  } else {
+    expect_that(digest(res), equals("bc42d0cbe0bb3321e83979c0432f9cea"))
+  }
 })
 
