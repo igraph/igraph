de1f976a161188b819ab931a8330a8dca6a61da3
Gabor Csardi <csardi.gabor@gmail.com>
2014-01-27 14:17:25 -0500

    Add gamma RNG.

--- include/igraph_random.h
+++ include/igraph_random.h
@@ -56,6 +56,8 @@ typedef struct igraph_rng_type_t {
   igraph_real_t (*get_geom)(void *state, igraph_real_t p);
   igraph_real_t (*get_binom)(void *state, long int n, igraph_real_t p);
   igraph_real_t (*get_exp)(void *state, igraph_real_t rate);
+  igraph_real_t (*get_gamma)(void *state, igraph_real_t shape,
+			     igraph_real_t scale);
 } igraph_rng_type_t;
 
 typedef struct igraph_rng_t {
@@ -87,6 +89,8 @@ igraph_real_t igraph_rng_get_binom(igraph_rng_t *rng, long int n,
 igraph_real_t igraph_rng_get_exp(igraph_rng_t *rng, igraph_real_t rate);
 unsigned long int igraph_rng_get_int31(igraph_rng_t *rng);
 igraph_real_t igraph_rng_get_exp(igraph_rng_t *rng, igraph_real_t rate);
+igraph_real_t igraph_rng_get_gamma(igraph_rng_t *rng, igraph_real_t shape,
+				   igraph_real_t scale);
 
 /* --------------------------------- */
 
