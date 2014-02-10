a7437d2b85a86d2703dbb993e10a9fd893acda8d
Gabor Csardi <csardi.gabor@gmail.com>
2014-02-10 16:02:09 -0500

    R: adjacency spectral embedding docs and test updates (#26).

--- interfaces/R/igraph/inst/tests/test_adjacency.spectral.embedding.R
+++ interfaces/R/igraph/inst/tests/test_adjacency.spectral.embedding.R
@@ -7,6 +7,8 @@ test_that("adjacency.spectral.embedding works", {
   no <- 3
   g <- graph.tree(10, 3, mode="out")
   asm <- adjacency.spectral.embedding(g, no=no, cvec=degree(g)/2)
+  asmu <- adjacency.spectral.embedding(g, no=no, cvec=degree(g)/2,
+                                       scaled=FALSE)
 
   A <- get.adjacency(g)
   A <- A + 1/2 * diag(degree(g))
@@ -18,9 +20,14 @@ test_that("adjacency.spectral.embedding works", {
       if (any(col < 0) && col[which(col != 0)[1]] < 0) { -col } else { col }
     })
   }
+
+  expect_that(std(asmu$X), equals(std(asm2$u[,1:no])))
+  expect_that(std(asmu$Y), equals(std(asm2$v[,1:no])))
   
-  expect_that(asm$D, equals(asm2$d[1:no]))
-  expect_that(std(asm$U), equals(std(asm2$u[,1:no])))
-  expect_that(std(asm$V), equals(std(asm2$v[,1:no])))
+  X <- asm2$u[,1:no] %*% diag(asm2$d[1:no])
+  Y <- asm2$v[,1:no] %*% diag(asm2$d[1:no])
   
+  expect_that(std(asm$X), equals(std(X)))
+  expect_that(std(asm$Y), equals(std(Y)))
+
 })
