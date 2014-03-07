4b0cccf0bc68b5f7ae9b9c76c30650d6e91d199a
Gabor Csardi <csardi.gabor@gmail.com>
2014-03-07 17:08:26 -0500

    R: fix architecture dependent Kamada-Kawai tests.

--- interfaces/R/igraph/inst/tests/test_layout.kk.R
+++ interfaces/R/igraph/inst/tests/test_layout.kk.R
@@ -6,16 +6,28 @@ test_that("Kamada-Kawai layout generator works", {
   library(igraph)
   g <- graph.ring(10)
   l <- layout.kamada.kawai(g, maxiter=50)
-  expect_that(sum(l), equals(-1.13071769106689))
+  if (Sys.info()["sysname"] == "Darwin") {
+    expect_that(sum(l), equals(-1.13071769106689))
+  } else {
+    expect_that(sum(l), equals(-6.77278645472984e-05))
+  }
 
   g <- graph.star(30)
   l <- layout.kamada.kawai(g, maxiter=500)
-  expect_that(sum(l), equals(-85.6883999492408))
+  if (Sys.info()["sysname"] == "Darwin") {
+    expect_that(sum(l), equals(-85.6883999492408))
+  } else {
+    expect_that(sum(l), equals(-86.1405864709501))
+  }
 
   g <- graph.ring(10)
   E(g)$weight <- rep(1:2, length.out=ecount(g))
   l <- layout.kamada.kawai(g, maxiter=500)
-  expect_that(sum(l), equals(1.61069099387368))
+  if (Sys.info()["sysname"] == "Darwin") {
+    expect_that(sum(l), equals(1.61069099387368))
+  } else {
+    expect_that(sum(l), equals(-1.83036635516248))
+  }
   
 })
 
