05a009e3d18bc12857b9390cd76e98e2899d7957
Gabor Csardi <csardi.gabor@gmail.com>
2014-05-19 15:40:42 -0400

    SGM: use sparse matrices, except for solve_LSAP #22
    This makes things 3-4 times faster.

--- interfaces/R/igraph/R/sgm.R
+++ interfaces/R/igraph/R/sgm.R
@@ -3,16 +3,16 @@ sgm <- function(A, B, m, start, iteration) {
   totv <- ncol(A)
   n <- totv - m
   if (m != 0) {
-    A12 <- rbind(A[1:m, (m+1):(m+n)])
-    A21 <- cbind(A[(m+1):(m+n), 1:m])
-    B12 <- rbind(B[1:m, (m+1):(m+n)])
-    B21 <- cbind(B[(m+1):(m+n), 1:m])
+    A12 <- A[1:m, (m+1):(m+n), drop=FALSE]
+    A21 <- A[(m+1):(m+n), 1:m, drop=FALSE]
+    B12 <- B[1:m, (m+1):(m+n), drop=FALSE]
+    B21 <- B[(m+1):(m+n), 1:m, drop=FALSE]
   }
   if ( m==0 ) {
-    A12 <- matrix(0, n, n)
-    A21 <- matrix(0, n, n)
-    B12 <- matrix(0, n, n)
-    B21 <- matrix(0, n, n)
+    A12 <- Matrix(0, n, n)
+    A21 <- Matrix(0, n, n)
+    B12 <- Matrix(0, n, n)
+    B21 <- Matrix(0, n, n)
   }
   A22 <- A[(m+1):(m+n), (m+1):(m+n)]
   B22 <- B[(m+1):(m+n), (m+1):(m+n)]
@@ -23,38 +23,41 @@ sgm <- function(A, B, m, start, iteration) {
   iter <- 0
   while (toggle == 1 & iter < patience)  {
     iter <- iter+1
-    x <-  A21 %*% t(B21)
-    y <-  t(A12) %*% B12
-    z <-  A22 %*% P %*% t(B22)
-    w <-  t(A22) %*% P %*% B22
+    x <-  A21 %*% Matrix::t(B21)
+    y <-  Matrix::t(A12) %*% B12
+    z <-  A22 %*% P %*% Matrix::t(B22)
+    w <-  Matrix::t(A22) %*% P %*% B22
     Grad <- x + y + z + w
-    ind <- matrix(solve_LSAP(Grad, maximum = TRUE))
-    T <- diag(n)
+    ind <- as.vector(solve_LSAP(as.matrix(Grad), maximum = TRUE))
+    T <- Matrix::Diagonal(n)
     T <- T[ind, ]
-    wt <- t(A22) %*% T %*% B22
-    c <- sum(diag(w %*% t(P)))
-    d <- sum(diag(wt %*% t(P)))+sum(diag(w %*% t(T)))
-    e <- sum(diag(wt %*% t(T)))
-    u <- sum(diag(t(P) %*% x+t(P) %*% y))
-    v <- sum(diag(t(T) %*% x+t(T) %*% y))
-    if( c-d+e == 0 && d-2*e+u-v == 0) {
+    wt <- Matrix::t(A22) %*% T %*% B22
+    c <- sum(Matrix::diag(w %*% Matrix::t(P)))
+    d <- sum(Matrix::diag(wt %*% Matrix::t(P))) +
+      sum(Matrix::diag(w %*% Matrix::t(T)))
+    e <- sum(Matrix::diag(wt %*% Matrix::t(T)))
+    u <- sum(Matrix::diag(Matrix::t(P) %*% x+Matrix::t(P) %*% y))
+    v <- sum(Matrix::diag(Matrix::t(T) %*% x+Matrix::t(T) %*% y))
+    if ( c-d+e == 0 && d-2*e+u-v == 0) {
       alpha <- 0
     } else {
       alpha <- -(d-2*e+u-v) / (2*(c-d+e))}
     f0 <- 0
     f1 <-  c-e+u-v
     falpha <- (c-d+e) * alpha^2 + (d-2*e+u-v) * alpha
-    if(alpha < tol && alpha > 0 && falpha > f0 && falpha > f1) {
+    if (alpha < tol && alpha > 0 && falpha > f0 && falpha > f1) {
       P <-  alpha*P + (1-alpha) * T
-    } else if(f0 > f1) {
+    } else if (f0 > f1) {
       P <- T
     } else {
-      toggle <- 0}
+      toggle <- 0
+    }
   }
   D <- P
-  corr <- matrix(solve_LSAP(P,  maximum = TRUE))
-  P = diag(n)
-  P = rbind(cbind(diag(m), matrix(0, m, n)), cbind(matrix(0, n, m), P[corr, ]))
+  corr <- matrix(solve_LSAP(as.matrix(P),  maximum = TRUE))
+  P = Matrix::diag(n)
+  P = rbind(cbind(Matrix::diag(m), matrix(0, m, n)),
+    cbind(matrix(0, n, m), P[corr, ]))
   corr <- cbind(matrix((m+1):totv,  n), matrix(m+corr, n))
-  return(list(corr=corr,  P=P,  D=D))
+  list(corr=corr,  P=P,  D=D)
 }
--- interfaces/R/igraph/inst/tests/test_sgm.R
+++ interfaces/R/igraph/inst/tests/test_sgm.R
@@ -11,8 +11,8 @@ test_that("SGM works", {
   g1 <- erdos.renyi.game(vc, .5)
   randperm <- c(1:nos, nos + sample(vc-nos))
   g2 <- correlated.game(g1, corr=.7, p=g1$p, perm=randperm)
-  P  <-sgm (g1[sparse=FALSE], g2[sparse=FALSE], m=nos,
-            start=matrix(1/(vc-nos), vc-nos, vc-nos), iteration=20)
+  P  <-sgm (g1[], g2[], m=nos, start=matrix(1/(vc-nos), vc-nos, vc-nos),
+            iteration=20)
 
   expect_that(c(1:nos, P$corr[,2]), equals(randperm))
   expect_that(apply(P$P != 0, 1, which), equals(randperm))
@@ -29,8 +29,8 @@ test_that("SGM works", {
   perm <- c(1:nos, sample(vc-nos)+nos)
   g2 <- correlated.game(g1, corr=1, p=g1$p, perm=perm)
 
-  P <- sgm(g1[sparse=FALSE], g2[sparse=FALSE], m=nos,
-         start=matrix(1/(vc-nos), vc-nos, vc-nos), iteration=20)
+  P <- sgm(g1[], g2[], m=nos, start=matrix(1/(vc-nos), vc-nos, vc-nos),
+           iteration=20)
 
   test_that(P$corr[,2], equals(perm[(nos+1):vc]))
   expect_that(apply(P$P != 0, 1, which), equals(perm))
