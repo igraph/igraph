a8ab36cce0325dde51b3f476fb1bdc472b498713
Gabor Csardi <csardi.gabor@gmail.com>
2014-07-23 15:14:39 -0400

    DYN: R: print now in summary header #657

--- interfaces/R/igraph/R/print.R
+++ interfaces/R/igraph/R/print.R
@@ -53,13 +53,15 @@
     stop("Not a graph object")
   }
 
+  temp <- if (! is.temporal(object)) "" else paste(" @", object$now)
+
   title <- paste(sep="", "IGRAPH ",
                  c("U","D")[is.directed(object)+1],
                  c("-","N")[is.named(object)+1],
                  c("-","W")[is.weighted(object)+1],
                  c("-","B")[is.bipartite(object)+1],
                  c("-","T")[is.temporal(object)+1], " ",
-                 vcount(object), " ", ecount(object), " -- ")
+                 vcount(object), " ", ecount(object), temp, " -- ")
   w <- getOption("width")
   if (nchar(title) < w && "name" %in% list.graph.attributes(object)) {
     title <- substring(paste(sep="", title,
