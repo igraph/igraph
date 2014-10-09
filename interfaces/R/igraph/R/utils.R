651ae6ce670335465adaec6627e39b8388faf874
Gabor Csardi <csardi.gabor@gmail.com>
2014-10-09 14:24:32 -0400

    R: utilities, do_call and add_class

---
+++ interfaces/R/igraph/R/utils.R
@@ -0,0 +1,42 @@
+
+## -----------------------------------------------------------------------
+##
+##   IGraph R package
+##   Copyright (C) 2014  Gabor Csardi <csardi.gabor@gmail.com>
+##   334 Harvard street, Cambridge, MA 02139 USA
+##
+##   This program is free software; you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation; either version 2 of the License, or
+##   (at your option) any later version.
+##
+##   This program is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##
+##   You should have received a copy of the GNU General Public License
+##   along with this program; if not, write to the Free Software
+##   Foundation, Inc.,  51 Franklin Street, Fifth Floor, Boston, MA
+##   02110-1301 USA
+##
+## -----------------------------------------------------------------------
+
+make_call <- function(f, ..., .args = list()) {
+  if (is.character(f)) f <- as.name(f)
+  as.call(c(f, ..., .args))
+}
+
+do_call <- function(f, ..., .args = list(), .env = parent.frame()) {
+  f <- substitute(f)
+
+  call <- make_call(f, ..., .args)
+  eval(call, .env)
+}
+
+add_class <- function(x, class) {
+  if (!is(x, class)) {
+    class(x) <- c(class, class(x))
+  }
+  x
+}
