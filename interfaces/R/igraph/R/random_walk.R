ed8c0d3d2aac4576e0e23fb4a9fc8dda69f7bba7
Gabor Csardi <csardi.gabor@gmail.com>
2015-01-02 11:05:00 -0500

    R: extend random_walk example in docs

--- interfaces/R/igraph/R/random_walk.R
+++ interfaces/R/igraph/R/random_walk.R
@@ -22,10 +22,14 @@
 #'   make_star(11, center = 11) + edge(11, 1)
 #'
 #' ec <- eigen_centrality(g, directed = TRUE)$vector
+#' pg <- page_rank(g, damping = 0.999)$vector
 #' w <- random_walk(g, start = 1, steps = 10000)
 #'
 #' ## These are similar, but not exactly the same
 #' cor(table(w), ec)
+#'
+#' ## But these are (almost) the same
+#' cor(table(w), pg)
 
 random_walk <- function(graph, start, steps, mode = c("out", "in", "all"),
                         stuck = c("return", "error")) {
