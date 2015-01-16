66a353915f6c419481054f2d0cfd731014464fa3
Gabor Csardi <csardi.gabor@gmail.com>
2015-01-16 11:02:52 -0500

    Remove R package from igraph/igraph repo
    It is in its own repo now, currently at
    https://github.com/igraph/rigraph

--- interfaces/R/igraph/inst/benchmarks/correlated.R
+++
@@ -1,8 +0,0 @@
-
-time_group("correlated E-R graphs, v1")
-
-time_that("sample_correlated_gnp is fast", replications=10,
-          init={ library(igraph) },
-          { sample_correlated_gnp_pair(100, corr=.8, p=5/100) })
-
-
--- interfaces/R/igraph/inst/benchmarks/time_sir.R
+++
@@ -1,7 +0,0 @@
-
-time_group("SIR epidemics models on networks")
-
-time_that("SIR is fast", replications=10,
-          init = { library(igraph); set.seed(42) },
-          reinit = { g <- sample_gnm(40, 40) },
-          { sir(g, beta=5, gamma=1, no.sim=100) })
