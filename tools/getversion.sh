2fa4a7a87f091678a3694cb07c4b2cbb5f4aa844
Gabor Csardi <csardi.gabor@gmail.com>
2015-01-29 14:31:04 -0500

    New kind of version numbers for dev versions
    It is now simply the next version, taken from a file,
    plus "-pre+badcafe", where "badcafe" is the hash of the current commit.

--- tools/getversion.sh
+++ tools/getversion.sh
@@ -1,3 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 
-git describe HEAD --tags | rev | sed 's/g-/./' | sed 's/-/+/' | rev
+thistag=$(git describe --exact-match --tags HEAD 2>/dev/null || true)
+
+if [ -z "${thistag}" ]; then
+    # taghash=$(git rev-list --tags --max-count=1)
+    # tag=$(git describe --tags "$taghash")
+    next_version=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cat NEXT_VERSION )
+    current=$(git rev-parse --short HEAD)
+    echo "${next_version}-pre+${current}"
+else
+    echo "${thistag}"
+fi
