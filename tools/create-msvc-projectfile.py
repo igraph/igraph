03d501fb16d5195012f190e4aa59c5ba1c78c3ad
Jan Schulz <jasc@gmx.net>
2016-07-08 16:23:58 +0200

    Fix py2/3 incompatibilities in msvc build

--- tools/create-msvc-projectfile.py
+++ tools/create-msvc-projectfile.py
@@ -43,6 +43,7 @@ def runmake(makefile, target):
     out = check_output("make -q -C " + os.path.dirname(makefile) + 
                        " -f " + os.path.basename(makefile) + 
                        " " + target, shell=True)
+    out = out.decode(sys.stdout.encoding)
     return out.replace("/", "\\")
 
 def rreplace(s, old, new, occurrence):
@@ -51,7 +52,7 @@ def rreplace(s, old, new, occurrence):
 
 def main():
     if len(sys.argv) != 4:
-        print "Error: need three arguments"
+        print( "Error: need three arguments")
         sys.exit(1)
     package = sys.argv[1]
     projectfile = sys.argv[2]
