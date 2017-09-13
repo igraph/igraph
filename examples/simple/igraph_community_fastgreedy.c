8232f6c825be013fa33ef3bcf9440568282826f2
Tamas Nepusz <ntamas@gmail.com>
2017-09-13 21:22:23 +0200

    fix igraph_community_fastgreedy() test case for real

--- examples/simple/igraph_community_fastgreedy.c
+++ examples/simple/igraph_community_fastgreedy.c
@@ -169,7 +169,6 @@ int main() {
            0,5, -1);
   igraph_community_fastgreedy(&g, 0, 0, &modularity, &membership);
   show_results(&g, &modularity, 0, &membership, stdout);
-  igraph_destroy(&g);
 
   /* Regression test -- asking for optimal membership vector but not
    * providing a merge matrix or a modularity vector */
