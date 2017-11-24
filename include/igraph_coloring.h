271ccda3b185289c21357a18dabfca02ba4f74c3
Szabolcs Horvát <szhorvat@gmail.com>
2017-11-24 15:26:55 +0100

    Address PR review:  - use heap  - add benchmark  - minor corrections

--- include/igraph_coloring.h
+++ include/igraph_coloring.h
@@ -9,11 +9,11 @@ __BEGIN_DECLS
  * \typedef igraph_coloring_greedy_t
  * Ordering heuristics for igraph_vertex_coloring_greedy
  *
- * \enumval IGRAPH_COLORING_GREEDY_CN Choose vertex with largest number of already coloured neighbours.
+ * \enumval IGRAPH_COLORING_GREEDY_COLORED_NEIGHBORS  Choose vertex with largest number of already colored neighbors.
  *
  */
 typedef enum {
-    IGRAPH_COLORING_GREEDY_CN = 0
+    IGRAPH_COLORING_GREEDY_COLORED_NEIGHBORS = 0
 } igraph_coloring_greedy_t;
 
 DECLDIR int igraph_vertex_coloring_greedy(const igraph_t *graph, igraph_vector_int_t *colors, igraph_coloring_greedy_t heuristic);
