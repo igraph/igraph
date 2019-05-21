2e5336f64fe19ef68c5edf2915e44cf9964a0483
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-05-21 15:44:03 -0700

    Check for #1056: Test for boolean graph attribute

---
+++ examples/simple/cattr_bool_bug2.c
@@ -0,0 +1,32 @@
+
+#include <igraph.h>
+#include <stdio.h>
+
+#define FILENAME "mybool.graphml.xml"
+
+int main() {
+  igraph_attribute_table_t* oldHandler = igraph_i_set_attribute_table(&igraph_cattribute_table);
+
+  FILE* graphFile = fopen("cattr_bool_bug2.graphml", "r");
+
+
+  if (!ifile) {
+    printf("Cannot open input file");
+    return 1;
+  }
+
+  igraph_read_graph_graphml(&graph, graphFile, 0);
+
+  fclose(graphFile);
+
+  if (igraph_cattribute_has_attr(&graph, IGRAPH_ATTRIBUTE_GRAPH, "mybool")) {
+      igraph_bool_t value = igraph_cattribute_GAB(&graph, "mybool");
+      printf("found boolean value %d\n", value);
+  } else {
+      return 2;
+  }
+
+  igraph_destroy(&graph);
+  
+  return 0;
+}
