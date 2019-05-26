efcb2803385e26a5ca283f0273e09a142ba52873
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-05-26 12:06:39 -0700

    Fix test for #1191

--- examples/tests/cattr_bool_bug2.c
+++ examples/tests/cattr_bool_bug2.c
@@ -5,25 +5,35 @@
 #define FILENAME "mybool.graphml.xml"
 
 int main() {
+
   igraph_t graph;
+  igraph_error_handler_t* oldhandler;
+  int result;
+  FILE* ifile = fopen("cattr_bool_bug2.graphml", "r");
 
-  FILE* graphFile = fopen("cattr_bool_bug2.graphml", "r");
-  igraph_i_set_attribute_table(&igraph_cattribute_table);
-
-  if (!graphFile) {
+  if (!ifile) {
     printf("Cannot open input file");
     return 1;
   }
+  
+  igraph_i_set_attribute_table(&igraph_cattribute_table);
 
-  igraph_read_graph_graphml(&graph, graphFile, 0);
+  oldhandler=igraph_set_error_handler(igraph_error_handler_ignore);
+  if ((result=igraph_read_graph_graphml(&graph, ifile, 0))) {
+    /* maybe it is simply disabled at compile-time */
+    if (result == IGRAPH_UNIMPLEMENTED) return 77;
+    return 1;
+  }
+  igraph_set_error_handler(oldhandler);
 
-  fclose(graphFile);
+  fclose(ifile);
 
-  if (igraph_cattribute_has_attr(&graph, IGRAPH_ATTRIBUTE_GRAPH, "mybool")) {
-      igraph_bool_t value = igraph_cattribute_GAB(&graph, "mybool");
-      printf("found boolean value %d\n", value);
-  } else {
+  if (!igraph_cattribute_has_attr(&graph, IGRAPH_ATTRIBUTE_GRAPH, "mybool")) {
+      printf("boolean value mybool not found\n");
       return 2;
+  //} else {
+  //    //igraph_bool_t value = igraph_cattribute_GAB(&graph, "mybool");
+  //    //printf("found boolean value %d\n", value);
   }
 
   igraph_destroy(&graph);
