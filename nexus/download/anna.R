fce24f81b90fa472589ffbcb63215e246c854d95
Szabolcs Horvát <szhorvat@gmail.com>
2020-02-01 11:23:10 +0100

    Remove nexus from C core repo

--- nexus/download/anna.R
+++
@@ -1,9 +0,0 @@
-
-source("sgb-coappearance.R")
-
-tmp <- tempdir()
-annakarenina <-
-  getnet("http://mirror.switch.ch/ftp/mirror/tex/support/graphbase/anna.dat")
-
-annakarenina$name <- "Anna Karenina coappearance network"
-save(annakarenina, file="/tmp/annakarenina.Rdata")
