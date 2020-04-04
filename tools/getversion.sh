15d478e95a65f94931af42e2d3eddaa5e7eb66a8
vtraag <vincent@traag.net>
2020-04-04 22:04:23 +0200

    VERSION: Changed versioning system.

--- tools/getversion.sh
+++ tools/getversion.sh
@@ -3,11 +3,18 @@
 thistag=$(git describe --exact-match --tags HEAD 2>/dev/null || true)
 
 if [ -z "${thistag}" ]; then
-    # taghash=$(git rev-list --tags --max-count=1)
-    # tag=$(git describe --tags "$taghash")
-    next_version=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cat NEXT_VERSION )
-    current=$(git rev-parse --short HEAD)
-    echo "${next_version}-pre+${current}"
+    script_dir=$( dirname "${BASH_SOURCE[0]}" )
+    if [ -f $script_dir/NEXT_VERSION ]; then
+        # taghash=$(git rev-list --tags --max-count=1)
+        # tag=$(git describe --tags "$taghash")
+        next_version=$( cat $script_dir/NEXT_VERSION )
+        current=$(git rev-parse --short HEAD)
+        echo "${next_version}-pre+${current}"
+    else
+        latest_version=$(git describe --abbrev=0 HEAD)
+        current=$(git rev-parse --short HEAD)
+        echo "${latest_version}-post+${current}"
+    fi
 else
     echo "${thistag}"
 fi
