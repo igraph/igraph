74cd2c8b9e4d2a5a7c48194c049666e5c5b32806
Tamas Nepusz <ntamas@gmail.com>
2020-06-15 11:48:31 +0200

    GLPK can also be vendored

--- etc/cmake/dependencies.cmake
+++ etc/cmake/dependencies.cmake
@@ -2,7 +2,7 @@ include(PadString)
 
 macro(find_dependencies)
   # Declare the list of dependencies that _may_ be vendored
-  set(VENDORABLE_DEPENDENCIES BLAS CXSparse LAPACK ARPACK)
+  set(VENDORABLE_DEPENDENCIES BLAS CXSparse GLPK LAPACK ARPACK)
 
   # Declare configuration options for dependencies
   option(IGRAPH_GLPK_SUPPORT "Compile igraph with GLPK support" YES)
@@ -10,6 +10,7 @@ macro(find_dependencies)
   option(IGRAPH_GRAPHML_SUPPORT "Compile igraph with GraphML support" YES)
   option(IGRAPH_USE_INTERNAL_BLAS "Compile igraph with internal BLAS" NO)
   option(IGRAPH_USE_INTERNAL_CXSparse "Compile igraph with internal CXSparse" NO)
+  option(IGRAPH_USE_INTERNAL_GLPK "Compile igraph with internal GLPK" NO)
   option(IGRAPH_USE_INTERNAL_LAPACK "Compile igraph with internal LAPACK" NO)
   option(IGRAPH_USE_INTERNAL_ARPACK "Compile igraph with internal ARPACK" NO)
 
@@ -29,8 +30,10 @@ macro(find_dependencies)
   endforeach()
 
   # Declare dependencies dependent on some configuration settings
-  if(IGRAPH_GLPK_SUPPORT)
-    list(APPEND REQUIRED_DEPENDENCIES GLPK)
+  if(NOT IGRAPH_GLPK_SUPPORT)
+    if(IGRAPH_USE_INTERNAL_GLPK)
+      list(REMOVE VENDORED_DEPENDENCIES GLPK)
+    endif()
   endif()
   if(IGRAPH_GMP_SUPPORT)
     list(APPEND REQUIRED_DEPENDENCIES GMP)
@@ -44,18 +47,19 @@ macro(find_dependencies)
     find_package(${DEPENDENCY})
   endforeach()
 
+    # Override libraries of vendored dependencies even if they were somehow
+    # detected above
+    foreach(DEPENDENCY ${VENDORABLE_DEPENDENCIES})
+      string(TOUPPER "${DEPENDENCY}" LIBNAME_UPPER)
+      string(TOLOWER "${DEPENDENCY}" LIBNAME_LOWER)
+      if(IGRAPH_USE_INTERNAL_${DEPENDENCY})
+        set(${LIBNAME_UPPER}_LIBRARIES ${LIBNAME_LOWER}_vendored)
+        set(${LIBNAME_UPPER}_FOUND 1)
+      endif()
+    endforeach()
+
   # Export some aliases that will be used in config.h
   set(HAVE_GLPK ${GLPK_FOUND})
   set(HAVE_GMP ${GMP_FOUND})
   set(HAVE_LIBXML ${LIBXML2_FOUND})
-
-  # Override libraries of vendored dependencies even if they were somehow
-  # detected above
-  foreach(DEPENDENCY ${VENDORABLE_DEPENDENCIES})
-    string(TOUPPER "${DEPENDENCY}" LIBNAME_UPPER)
-    string(TOLOWER "${DEPENDENCY}" LIBNAME_LOWER)
-    if(IGRAPH_USE_INTERNAL_${DEPENDENCY})
-      set(${LIBNAME_UPPER}_LIBRARIES ${LIBNAME_LOWER}_vendored)
-    endif()
-  endforeach()
 endmacro()
