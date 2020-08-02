eb17d76d4638693561b3dbe5a357485fc7289186
Szabolcs Horvát <szhorvat@gmail.com>
2020-08-02 09:59:08 +0200

    realize_degree_sequence: update copyright notice

--- src/degree_sequence.cpp
+++ src/degree_sequence.cpp
@@ -1,6 +1,7 @@
 /*
+  IGraph library.
   Constructing realizations of degree sequences and bi-degree sequences.
-  Copyright (C) 2018-2020 Szabolcs Horvat <szhorvat@gmail.com>
+  Copyright (C) 2018-2020  The igraph development team <igraph@igraph.org>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -13,9 +14,7 @@
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-  02110-1301 USA
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "igraph_constructors.h"
