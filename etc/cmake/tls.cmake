46663bf7a5fdd7dbb0095f5f8f4119dc4bbc50eb
Tamas Nepusz <ntamas@gmail.com>
2020-10-26 14:00:10 +0100

    fix: fix IGRAPH_THREAD_SAFE define in generated include/igraph_threading.h

--- etc/cmake/tls.cmake
+++ etc/cmake/tls.cmake
@@ -7,6 +7,11 @@ if(IGRAPH_ENABLE_TLS)
   if(NOT TLS_KEYWORD)
     message(FATAL_ERROR "Thread-local storage not supported on this compiler")
   endif()
+
+  # TODO: we should probably set this only if we are building igraph with
+  # internal-everything
+  set(IGRAPH_THREAD_SAFE YES)
 else()
   set(TLS_KEYWORD "")
+  set(IGRAPH_THREAD_SAFE NO)
 endif()
