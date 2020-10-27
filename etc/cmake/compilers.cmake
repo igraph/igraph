94fa748560530649abdb538ba8ad1f170bd4ef45
Szabolcs Horvát <szhorvat@gmail.com>
2020-10-27 22:58:58 +0100

    cmake: Condition warning options on C_COMPILER_ID (#1509)

--- etc/cmake/compilers.cmake
+++ etc/cmake/compilers.cmake
@@ -1,11 +1,5 @@
 if(MSVC)
   add_compile_options(/FS)
-else()
-  add_compile_options(
-    # Intel compiler:
-    # Do not print remarks:
-    $<$<C_COMPILER_ID:Intel>:-diag-disable=remark>
-  )
 endif()
 
 macro(use_all_warnings TARGET_NAME)
@@ -13,13 +7,18 @@ macro(use_all_warnings TARGET_NAME)
     target_compile_options(${TARGET_NAME} PRIVATE /W4)
   else()
     target_compile_options(${TARGET_NAME} PRIVATE 
-      -Wall -Wextra -pedantic -Werror -Wno-unused-parameter -Wno-sign-compare
-      $<$<NOT:$<C_COMPILER_ID:Intel>>:-Wno-varargs>
+      # GCC-style compilers:
+      $<$<C_COMPILER_ID:GCC,Clang,AppleClang>:
+        -Wall -Wextra -pedantic -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-varargs
+      >
       # Intel compiler:
-      # disable #279: controlling expression is constant; affecting assert(condition && "message")
-      # disable #188: enumerated type mixed with another type; affecting IGRAPH_CHECK
-      # disable #592: variable "var" is used before its value is set; affecting IGRAPH_UNUSED
-      $<$<C_COMPILER_ID:Intel>:-wd279 -wd188 -wd592>
+      $<$<C_COMPILER_ID:Intel>:
+        -Wall -Wextra -pedantic -Werror -Wno-unused-parameter -Wno-sign-compare
+        # disable #279: controlling expression is constant; affecting assert(condition && "message")
+        # disable #188: enumerated type mixed with another type; affecting IGRAPH_CHECK
+        # disable #592: variable "var" is used before its value is set; affecting IGRAPH_UNUSED
+        -wd279 -wd188 -wd592 -diag-disable=remark
+      >
     )
   endif()
 endmacro()
