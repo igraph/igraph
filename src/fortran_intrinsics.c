12d4e2cdee4977d99d24d04409bf2bb1a3c34786
Szabolcs Horvát <szhorvat@gmail.com>
2020-10-31 18:24:29 +0100

    fix prototypes of fortran intrinsics

--- src/fortran_intrinsics.c
+++ src/fortran_intrinsics.c
@@ -23,31 +23,31 @@
 
 #include <float.h>
 
-double digitsdbl_(double x) {
+double digitsdbl_(double *x) {
     return (double) DBL_MANT_DIG;
 }
 
-double epsilondbl_(double x) {
+double epsilondbl_(double *x) {
     return DBL_EPSILON;
 }
 
-double hugedbl_(double x) {
+double hugedbl_(double *x) {
     return DBL_MAX;
 }
 
-double tinydbl_(double x) {
+double tinydbl_(double *x) {
     return DBL_MIN;
 }
 
-int maxexponentdbl_(double x) {
+int maxexponentdbl_(double *x) {
     return DBL_MAX_EXP;
 }
 
-int minexponentdbl_(double x) {
+int minexponentdbl_(double *x) {
     return DBL_MIN_EXP;
 }
 
-double radixdbl_(double x) {
+double radixdbl_(double *x) {
     return (double) FLT_RADIX;
 }
 
