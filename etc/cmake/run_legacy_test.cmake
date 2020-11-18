44cb0205f31bfc0cedda8d80c45844755296c726
Tamas Nepusz <ntamas@gmail.com>
2020-11-18 21:12:50 +0100

    fix: attempt to provide a replacement for 'cmake -E cat'

--- etc/cmake/run_legacy_test.cmake
+++ etc/cmake/run_legacy_test.cmake
@@ -13,6 +13,18 @@
 # - IGRAPH_VERSION: version string of igraph that should be replaced in
 #   expected outputs
 
+function(print_file FILENAME)
+  # Replacement of "cmake -E cat" for older CMake versions. cat was added in
+  # CMake 3.18
+  if(UNIX OR APPLE)
+    # Most likely Linux or macOS
+    execute_process(COMMAND "/bin/sh" "-c" "cat ${FILENAME}")
+  elseif(WIN32)
+    # Most likely Windows
+    execute_process(COMMAND "cmd" "/c" "type" "${FILENAME}")
+  endif()
+endfunction()
+
 get_filename_component(WORK_DIR ${EXPECTED_OUTPUT_FILE} DIRECTORY)
 
 execute_process(
@@ -27,7 +39,7 @@ if(ERROR_CODE EQUAL 77)
 elseif(ERROR_CODE)
   set(MESSAGE "Test exited abnormally with error: ${ERROR_CODE}")
   file(WRITE ${OBSERVED_OUTPUT_FILE} "${MESSAGE}\n=========================================\n${OBSERVED_OUTPUT}")
-  execute_process(COMMAND "${CMAKE_COMMAND}" -E cat "${OBSERVED_OUTPUT_FILE}")
+  print_file("${OBSERVED_OUTPUT_FILE}")
   file(REMOVE ${DIFF_FILE})
   message(FATAL_ERROR "Exiting test.")
 else()
@@ -59,7 +71,7 @@ else()
     if(EXISTS ${DIFF_FILE})
       message(STATUS "See diff below:")
       message(STATUS "-------------------------------------------------------")
-      execute_process(COMMAND "${CMAKE_COMMAND}" -E cat "${DIFF_FILE}")
+      print_file("${DIFF_FILE}")
       message(STATUS "-------------------------------------------------------")
     else()
       message(STATUS "Diff omitted; no diff tool was installed.")
