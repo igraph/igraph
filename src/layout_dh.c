96075932554171b8e02195a73da9881f8acd29c5
Szabolcs Horvát <szhorvat@gmail.com>
2020-12-07 12:43:56 +0100

    layout_davidson_harel: make interruptible

--- src/layout_dh.c
+++ src/layout_dh.c
@@ -26,6 +26,7 @@
 #include "igraph_interface.h"
 #include "igraph_random.h"
 #include "igraph_math.h"
+#include "igraph_interrupt_internal.h"
 
 #include <math.h>
 
@@ -242,6 +243,8 @@ int igraph_layout_davidson_harel(const igraph_t *graph, igraph_matrix_t *res,
     for (round = 0; round < maxiter + fineiter; round++) {
         igraph_integer_t p;
         igraph_vector_int_shuffle(&perm);
+
+        IGRAPH_ALLOW_INTERRUPTION();
 
         fine_tuning = round >= maxiter;
         if (fine_tuning) {
