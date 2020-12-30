f1904610c4863e6af215757122719c3c78768642
AdamKorcz <adam@adalogics.com>
2020-12-30 16:41:57 +0000

    Added fuzzer and OSS-fuzz build script

---
+++ fuzzing/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+sed 's/use_all_warnings/\#use_all_warnings/g' -i $SRC/igraph/src/CMakeLists.txt
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+
+# Create seed corpus
+zip $OUT/read_lgl_fuzzer_seed_corpus.zip \
+        $SRC/igraph/examples/simple/karate.gml
+
+cd $SRC/igraph
+
+$CC $CFLAGS -I$SRC/igraph/build/include -I$SRC/igraph/include -o read_lgl_fuzzer.o -c ./fuzzing/read_lgl_fuzzer.c
+$CC $CFLAGS $LIB_FUZZING_ENGINE read_lgl_fuzzer.o \
+        -o $OUT/read_lgl_fuzzer ./build/src/libigraph.a
