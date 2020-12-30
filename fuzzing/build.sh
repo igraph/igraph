4801d0fc6ea0dd82160a097ff8ce527faae25b91
AdamKorcz <adam@adalogics.com>
2020-12-30 16:17:19 +0000

    Updated build script

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -eu
+!/bin/bash -eu
 
-./bootstrap.sh
-./configure
+sed 's/use_all_warnings/\#use_all_warnings/g' -i $SRC/igraph/src/CMakeLists.txt
+mkdir build && cd build
+cmake ..
 make -j$(nproc)
 
 # Create seed corpus
 zip $OUT/read_lgl_fuzzer_seed_corpus.zip \
-	$SRC/igraph/examples/simple/karate.gml
+        $SRC/igraph/examples/simple/karate.gml
 
+cd $SRC/igraph
 
-mv $SRC/igraph/fuzzing/read_lgl_fuzzer.c .
-$CC $CFLAGS -I./include -o read_lgl_fuzzer.o -c read_lgl_fuzzer.c
+$CC $CFLAGS -I$SRC/igraph/build/include -I$SRC/igraph/include -o read_lgl_fuzzer.o -c ./fuzzing/read_lgl_fuzzer.c
 $CC $CFLAGS $LIB_FUZZING_ENGINE read_lgl_fuzzer.o \
-	-o $OUT/read_lgl_fuzzer /src/igraph/src/.libs/libigraph.a
+        -o $OUT/read_lgl_fuzzer ./build/src/libigraph.a
