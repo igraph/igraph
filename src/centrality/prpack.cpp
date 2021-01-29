94a4ebe4acd1cc73f14945792c3ce21465be4aa5
Szabolcs Horvát <szhorvat@gmail.com>
2021-01-29 19:56:21 +0100

    pagerank: protect against potential exceptions thrown from PRPACK

--- src/centrality/prpack.cpp
+++ src/centrality/prpack.cpp
@@ -26,6 +26,7 @@
 #include "centrality/prpack_internal.h"
 #include "centrality/prpack/prpack_igraph_graph.h"
 #include "centrality/prpack/prpack_solver.h"
+#include "core/exceptions.h"
 
 using namespace prpack;
 using namespace std;
@@ -46,40 +47,42 @@ int igraph_i_personalized_pagerank_prpack(const igraph_t *graph, igraph_vector_t
     double *v = nullptr;
     const prpack_result *res;
 
-    if (reset) {
-        if (igraph_vector_size(reset) != no_of_nodes) {
-            IGRAPH_ERROR("Invalid length of reset vector when calculating "
-                         "personalized PageRank scores.", IGRAPH_EINVAL);
+    IGRAPH_HANDLE_EXCEPTIONS(
+        if (reset) {
+            if (igraph_vector_size(reset) != no_of_nodes) {
+                IGRAPH_ERROR("Invalid length of reset vector when calculating "
+                             "personalized PageRank scores.", IGRAPH_EINVAL);
+            }
+
+            /* Normalize reset vector so the sum is 1 */
+            double reset_min = igraph_vector_min(reset);
+            if (reset_min < 0) {
+                IGRAPH_ERROR("The reset vector must not contain negative elements.", IGRAPH_EINVAL);
+            }
+            if (igraph_is_nan(reset_min)) {
+                IGRAPH_ERROR("The reset vector must not contain NaN values.", IGRAPH_EINVAL);
+            }
+
+            double reset_sum = igraph_vector_sum(reset);
+            if (reset_sum == 0) {
+                IGRAPH_ERROR("The sum of the elements in the reset vector must not be zero.", IGRAPH_EINVAL);
+            }
+
+            // Construct the personalization vector
+            v = new double[no_of_nodes];
+            for (i = 0; i < no_of_nodes; i++) {
+                v[i] = VECTOR(*reset)[i] / reset_sum;
+            }
         }
 
-        /* Normalize reset vector so the sum is 1 */
-        double reset_min = igraph_vector_min(reset);
-        if (reset_min < 0) {
-            IGRAPH_ERROR("The reset vector must not contain negative elements.", IGRAPH_EINVAL);
-        }
-        if (igraph_is_nan(reset_min)) {
-            IGRAPH_ERROR("The reset vector must not contain NaN values.", IGRAPH_EINVAL);
-        }
+        // Construct and run the solver
+        prpack_igraph_graph prpack_graph(graph, weights, directed);
+        prpack_solver solver(&prpack_graph, false);
+        res = solver.solve(damping, 1e-10, u, v, "");
 
-        double reset_sum = igraph_vector_sum(reset);
-        if (reset_sum == 0) {
-            IGRAPH_ERROR("The sum of the elements in the reset vector must not be zero.", IGRAPH_EINVAL);
-        }
-
-        // Construct the personalization vector
-        v = new double[no_of_nodes];
-        for (i = 0; i < no_of_nodes; i++) {
-            v[i] = VECTOR(*reset)[i] / reset_sum;
-        }
-    }
-
-    // Construct and run the solver
-    prpack_igraph_graph prpack_graph(graph, weights, directed);
-    prpack_solver solver(&prpack_graph, false);
-    res = solver.solve(damping, 1e-10, u, v, "");
-
-    // Delete the personalization vector
-    delete [] v;
+        // Delete the personalization vector
+        delete [] v;
+    );
 
     // Check whether the solver converged
     // TODO: this is commented out because some of the solvers do not implement it yet
