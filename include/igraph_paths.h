ff2c24c32f8f10816a16d3108118157fd09ecb18
Guy Rozenberg <guyroznb@gmail.com>
2021-02-02 21:30:37 +0200

    resolve conflicts

--- include/igraph_paths.h
+++ include/igraph_paths.h
@@ -3,22 +3,18 @@
    IGraph library.
    Copyright (C) 2009-2012  Gabor Csardi <csardi.gabor@gmail.com>
    334 Harvard street, Cambridge, MA 02139 USA
-
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc.,  51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301 USA
-
 */
 
 #ifndef IGRAPH_PATHS_H
@@ -34,7 +30,7 @@
 
 __BEGIN_DECLS
 
-DECLDIR int igraph_diameter(const igraph_t *graph, igraph_real_t *res,
+DECLDIR int igraph_diameter(const igraph_t *graph, igraph_integer_t *res,
                             igraph_integer_t *from, igraph_integer_t *to,
                             igraph_vector_t *path,
                             igraph_bool_t directed, igraph_bool_t unconn);
@@ -90,15 +86,12 @@ DECLDIR int igraph_get_shortest_paths_dijkstra(const igraph_t *graph,
         igraph_neimode_t mode,
         igraph_vector_long_t *predecessors,
         igraph_vector_long_t *inbound_edges);
-DECLDIR int igraph_get_shortest_paths_bellman_ford(const igraph_t *graph,
-         igraph_vector_ptr_t *vertices,
-         igraph_vector_ptr_t *edges,
-         const igraph_integer_t from,
-         igraph_vs_t to,
-         const igraph_vector_t *weights,
-         igraph_neimode_t mode,
-         igraph_vector_long_t *predecessors,
-         igraph_vector_long_t *inbound_edges);
+DECLDIR int igraph_shortest_paths_bellman_ford(const igraph_t *graph,
+        igraph_matrix_t *res,
+        const igraph_vs_t from,
+        const igraph_vs_t to,
+        const igraph_vector_t *weights,
+        igraph_neimode_t mode);
 DECLDIR int igraph_get_shortest_path_dijkstra(const igraph_t *graph,
         igraph_vector_t *vertices,
         igraph_vector_t *edges,
@@ -118,26 +111,10 @@ DECLDIR int igraph_shortest_paths_johnson(const igraph_t *graph,
         const igraph_vs_t to,
         const igraph_vector_t *weights);
 
-DECLDIR int igraph_average_path_length(const igraph_t *graph,
-                                       igraph_real_t *res, igraph_real_t *unconn_pairs,
+DECLDIR int igraph_average_path_length(const igraph_t *graph, igraph_real_t *res,
                                        igraph_bool_t directed, igraph_bool_t unconn);
-DECLDIR int igraph_average_path_length_dijkstra(const igraph_t *graph,
-                                                igraph_real_t *res, igraph_real_t *unconn_pairs,
-                                                const igraph_vector_t *weights,
-                                                igraph_bool_t directed, igraph_bool_t unconn);
 DECLDIR int igraph_path_length_hist(const igraph_t *graph, igraph_vector_t *res,
                                     igraph_real_t *unconnected, igraph_bool_t directed);
-
-DECLDIR int igraph_global_efficiency(const igraph_t *graph, igraph_real_t *res,
-                                     const igraph_vector_t *weights,
-                                     igraph_bool_t directed);
-DECLDIR int igraph_local_efficiency(const igraph_t *graph, igraph_vector_t *res,
-                                    const igraph_vs_t vids,
-                                    const igraph_vector_t *weights,
-                                    igraph_bool_t directed, igraph_neimode_t mode);
-DECLDIR int igraph_average_local_efficiency(const igraph_t *graph, igraph_real_t *res,
-                                    const igraph_vector_t *weights,
-                                    igraph_bool_t directed, igraph_neimode_t mode);
 
 DECLDIR int igraph_eccentricity(const igraph_t *graph,
                                 igraph_vector_t *res,
