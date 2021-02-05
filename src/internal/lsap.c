4e861b4c623fddbc12fb650cc469925de0673621
Szabolcs Horvát <szhorvat@gmail.com>
2021-02-05 20:43:13 +0100

    lsap: replace igraph_warningf by IGRAPH_WARNINGF

--- src/internal/lsap.c
+++ src/internal/lsap.c
@@ -203,8 +203,7 @@ AP *ap_read_problem(char *file) {
         /*
           fprintf(stderr,"ap_read_problem: problem not quadratic\nrows =%d, cols = %d\n",nrow,ncol);
         */
-        igraph_warningf("ap_read_problem: problem not quadratic\nrows = %d, cols = %d\n",
-                        IGRAPH_FILE_BASENAME, __LINE__, -1, nrow, ncol);
+        IGRAPH_WARNINGF("ap_read_problem: problem not quadratic; rows = %d, cols = %d.", nrow, ncol);
         return NULL;
     }
 
