7a829fff48d9bf2c957bca19b402c1742e1aca75
Ramy Saied <ramy.m.saied@gmail.com>
2021-02-12 15:06:32 +0200

    Add heap_min functions tests

--- tests/unit/heap.c
+++ tests/unit/heap.c
@@ -27,60 +27,106 @@
 
 int main() {
 
-    igraph_heap_t h;
+    igraph_heap_t h_max;
+    igraph_heap_min_t h_min;
     igraph_integer_t i;
-    igraph_real_t *arr =(igraph_real_t*) calloc(5, sizeof(igraph_real_t));
+    igraph_real_t random_list[13]={-9.999,0,6,235,-2,-1000,-1,4,2000,0.5,1,-9,10};
+    const igraph_integer_t li_size=13;
 
-    /* igraph_heap_init, igraph_heap_destroy  */
-    printf("Test igraph_heap_init, igraph_heap_destroy\n");
-    igraph_heap_init(&h, 0);
-    igraph_heap_destroy(&h);
-    igraph_heap_init(&h, 10);
-    igraph_heap_destroy(&h);
+    /* max heap init & destroy*/
+    printf("Create empty max heap & destroy\n");
+    igraph_heap_init(&h_max, 0);
+    IGRAPH_ASSERT(igraph_heap_empty(&h_max));
+    igraph_heap_destroy(&h_max);
+    printf("Create empty max heap but allocate size for some elements\n");
+    igraph_heap_init(&h_max, 10);
+    IGRAPH_ASSERT(igraph_heap_empty(&h_max));
+    igraph_heap_destroy(&h_max);
 
-    /* igraph_heap_reserve */
-    printf("Test igraph_heap_reserve\n");
-    igraph_heap_init(&h, 0);
-    igraph_heap_reserve(&h, 10);
-    igraph_heap_reserve(&h, 5);
-    igraph_heap_destroy(&h);
+    /* min heap init & destroy*/
+    printf("Create empty min heap & destroy\n");
+    igraph_heap_min_init(&h_min, 0);
+    IGRAPH_ASSERT(igraph_heap_min_empty(&h_min));
+    igraph_heap_min_destroy(&h_min);
+    printf("Create empty min heap but allocate size for some elements\n");
+    igraph_heap_min_init(&h_min, 10);
+    IGRAPH_ASSERT(igraph_heap_min_empty(&h_min));
+    igraph_heap_min_destroy(&h_min);
 
-    /* igraph_heap_init_array*/
-    printf("Test igraph_heap_init_array\n");
-    igraph_heap_init_array(&h,arr,5);
-
-    /* igraph_heap_empty igraph_heap_size */
-    printf("Test igraph_heap_size, igraph_heap_empty\n");
-    IGRAPH_ASSERT((igraph_heap_size(&h)==5) && (!igraph_heap_empty(&h)));
-    igraph_heap_push(&h,100);
-    IGRAPH_ASSERT((igraph_heap_size(&h)==6) && (!igraph_heap_empty(&h)));
-    igraph_heap_delete_top(&h);
-    IGRAPH_ASSERT((igraph_heap_size(&h)==5) && (!igraph_heap_empty(&h)));
-    igraph_heap_destroy(&h);
-    igraph_heap_init(&h, 0);
-    IGRAPH_ASSERT((igraph_heap_size(&h)==0) && (igraph_heap_empty(&h)));
-
-    /* igraph_heap_push igraph_heap_top  */
-    printf("Test igraph_heap_push, igraph_heap_top\n");
-    igraph_real_t random_list[13]={-9.999,0,6,235,-2,-1000,-1,4,2000,0.1,1,-9,10};
-    igraph_vector_t top_sequence;
-    igraph_vector_init(&top_sequence,13);
-    for (i=0;i<13;i++)
-    {
-        igraph_heap_push(&h,random_list[i]);
-        VECTOR(top_sequence)[i]=igraph_heap_top(&h);
+    /*  max heap_reserve, heap_size and heap_empty*/
+    printf("Test max heap_reserve, heap_size and heap_empty\n");
+    igraph_heap_init(&h_max, 5);
+    IGRAPH_ASSERT(igraph_heap_empty(&h_max));
+    IGRAPH_ASSERT(igraph_heap_size(&h_max)==0);
+    igraph_heap_reserve(&h_max, 10);
+    IGRAPH_ASSERT(igraph_heap_empty(&h_max));
+    IGRAPH_ASSERT(igraph_heap_size(&h_max)==0);
+    for (i=0;i<15;i++){
+        igraph_heap_push(&h_max,i);
     }
-    print_vector_format(&top_sequence,stdout,"%g");
+    IGRAPH_ASSERT(igraph_heap_size(&h_max)==15);
+    IGRAPH_ASSERT(!igraph_heap_empty(&h_max));
+    igraph_heap_reserve(&h_max, 5);
+    IGRAPH_ASSERT(igraph_heap_size(&h_max)==15);
+    IGRAPH_ASSERT(!igraph_heap_empty(&h_max));
+    igraph_heap_destroy(&h_max);
 
-    /* igraph_heap_delete_top  */
-    printf("Test igraph_heap_delete_top\n");
-    for (i=0;i<13;i++)  VECTOR(top_sequence)[i]=(&top_sequence,igraph_heap_delete_top(&h));
-    print_vector_format(&top_sequence,stdout,"%g");
-    IGRAPH_ASSERT(igraph_heap_empty(&h));
+    /*  min heap reserve, heap_size and heap_empty*/
+    printf("Test min heap_reserve, heap_size and heap_empty\n");
+    igraph_heap_min_init(&h_min, 5);
+    IGRAPH_ASSERT(igraph_heap_min_empty(&h_min));
+    IGRAPH_ASSERT(igraph_heap_min_size(&h_min)==0);
+    igraph_heap_min_reserve(&h_min, 10);
+    IGRAPH_ASSERT(igraph_heap_min_empty(&h_min));
+    IGRAPH_ASSERT(igraph_heap_min_size(&h_min)==0);
+    for (i=0;i<15;i++){
+        igraph_heap_min_push(&h_min,i);
+    }
+    IGRAPH_ASSERT(igraph_heap_min_size(&h_min)==15);
+    IGRAPH_ASSERT(!igraph_heap_min_empty(&h_min));
+    igraph_heap_min_reserve(&h_min, 5);
+    IGRAPH_ASSERT(igraph_heap_min_size(&h_min)==15);
+    IGRAPH_ASSERT(!igraph_heap_min_empty(&h_min));
+    igraph_heap_min_destroy(&h_min);
 
-    free(arr);
-    igraph_heap_destroy(&h);
-    igraph_vector_destroy(&top_sequence);
+    /* max heap init_array and delete_top */
+    printf("Test max heap_init array and delete_top\n");
+    igraph_heap_init_array(&h_max,random_list,li_size);
+    while (igraph_heap_size(&h_max)>0){
+        printf("%g ",igraph_heap_delete_top(&h_max));
+    }
+    printf("\n");
+    igraph_heap_destroy(&h_max);
+
+    /* min heap init_array and delete_top */
+    printf("Test min heap init_array and delete_top\n");
+    igraph_heap_min_init_array(&h_min,random_list,li_size);
+    while (igraph_heap_min_size(&h_min)>0){
+        printf("%g ",igraph_heap_min_delete_top(&h_min));
+    }
+    printf("\n");
+    igraph_heap_min_destroy(&h_min);
+
+    /* max heap top and push */
+    printf("Test max heap top and push\n");
+    igraph_heap_init(&h_max, 0);
+    for (i=0;i<li_size;i++){
+        igraph_heap_push(&h_max,random_list[i]);
+        printf("%g ",igraph_heap_top(&h_max));
+    }
+    printf("\n");
+
+    /* min heap top and push */
+    printf("Test min heap top and push\n");
+    igraph_heap_min_init(&h_min, 0);
+    for (i=0;i<li_size;i++){
+        igraph_heap_min_push(&h_min,random_list[i]);
+        printf("%g ",igraph_heap_min_top(&h_min));
+    }
+    printf("\n");
+
+    igraph_heap_destroy(&h_max);
+    igraph_heap_min_destroy(&h_min);
 
     VERIFY_FINALLY_STACK();
 
