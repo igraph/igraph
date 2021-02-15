16ddf28c00d2ee43c0f117227032f62e3d39aa05
Szabolcs Horvát <szhorvat@gmail.com>
2021-02-15 18:40:46 +0100

    asymmetric_preference_game: match declaration to definition

--- include/igraph_games.h
+++ include/igraph_games.h
@@ -114,12 +114,12 @@ IGRAPH_EXPORT int igraph_preference_game(igraph_t *graph, igraph_integer_t nodes
                                          igraph_vector_t *node_type_vec,
                                          igraph_bool_t directed, igraph_bool_t loops);
 IGRAPH_EXPORT int igraph_asymmetric_preference_game(igraph_t *graph, igraph_integer_t nodes,
-                                                    igraph_integer_t in_types,
                                                     igraph_integer_t out_types,
+                                                    igraph_integer_t in_types,
                                                     const igraph_matrix_t *type_dist_matrix,
                                                     const igraph_matrix_t *pref_matrix,
-                                                    igraph_vector_t *node_type_in_vec,
                                                     igraph_vector_t *node_type_out_vec,
+                                                    igraph_vector_t *node_type_in_vec,                                                    
                                                     igraph_bool_t loops);
 
 IGRAPH_EXPORT int igraph_rewire_edges(igraph_t *graph, igraph_real_t prob,
