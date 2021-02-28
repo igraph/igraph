78e1c6686ce51c72c7d42866dc2035495aefc8d4
Szabolcs Horvát <szhorvat@gmail.com>
2021-02-28 10:48:10 +0100

    neighborhood_size: update docs and messages

--- src/properties/neighborhood.c
+++ src/properties/neighborhood.c
@@ -38,8 +38,10 @@
  * neighbors, order 2 is order 1 plus the immediate neighbors of the
  * vertices in order 1, etc.
  *
- * </para><para> This function calculates the size of the neighborhood
+ * </para><para> 
+ * This function calculates the size of the neighborhood
  * of the given order for the given vertices.
+ *
  * \param graph The input graph.
  * \param res Pointer to an initialized vector, the result will be
  *    stored here. It will be resized as needed.
@@ -53,9 +55,9 @@
  *   \c order steps are counted. \c IGRAPH_ALL ignores the direction
  *   of the edges. This argument is ignored for undirected graphs.
  * \param mindist The minimum distance to include a vertex in the counting.
+ *   Vertices reachable with a path shorter than this value are excluded.
  *   If this is one, then the starting vertex is not counted. If this is
- *   two, then its neighbors are not counted, either, etc. Vertices which
- *   can also be reached by longer paths are still excluded.
+ *   two, then its neighbors are not counted either, etc.
  * \return Error code.
  *
  * \sa \ref igraph_neighborhood() for calculating the actual neighborhood,
@@ -83,8 +85,8 @@ int igraph_neighborhood_size(const igraph_t *graph, igraph_vector_t *res,
     }
 
     if (mindist < 0 || mindist > order) {
-        IGRAPH_ERRORF("Minimum distance should be between zero and order, got %" IGRAPH_PRId ".",
-                      IGRAPH_EINVAL, mindist);
+        IGRAPH_ERRORF("Minimum distance should be between 0 and the neighborhood order (%" IGRAPH_PRId "), got %" IGRAPH_PRId ".",
+                      IGRAPH_EINVAL, order, mindist);
     }
 
     added = igraph_Calloc(no_of_nodes, long int);
