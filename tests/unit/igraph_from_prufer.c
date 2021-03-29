4b54602e383d070ade24367476190544f1d5ac1e
Tamas Nepusz <ntamas@gmail.com>
2021-03-29 21:21:35 +0200

    fix: fix a few type incompatibilities between int vs igraph_integer_t and int vs igraph_bool_t

--- tests/unit/igraph_from_prufer.c
+++ tests/unit/igraph_from_prufer.c
@@ -7,19 +7,19 @@
 
 int main() {
     igraph_t graph;
-    igraph_integer_t prufer1[] = {2, 3, 2, 3};
-    igraph_integer_t prufer2[] = {0, 2, 4, 1, 1, 0};
+    int prufer1[] = {2, 3, 2, 3};
+    int prufer2[] = {0, 2, 4, 1, 1, 0};
     igraph_vector_int_t prufer;
     igraph_bool_t tree;
 
-    igraph_vector_int_view(&prufer, prufer1, sizeof(prufer1) / sizeof(igraph_integer_t));
+    igraph_vector_int_view(&prufer, prufer1, sizeof(prufer1) / sizeof(int));
     igraph_from_prufer(&graph, &prufer);
     igraph_is_tree(&graph, &tree, NULL, IGRAPH_ALL);
     IGRAPH_ASSERT(tree);
     print_graph(&graph);
     igraph_destroy(&graph);
 
-    igraph_vector_int_view(&prufer, prufer2, sizeof(prufer2) / sizeof(igraph_integer_t));
+    igraph_vector_int_view(&prufer, prufer2, sizeof(prufer2) / sizeof(int));
     igraph_from_prufer(&graph, &prufer);
     igraph_is_tree(&graph, &tree, NULL, IGRAPH_ALL);
     IGRAPH_ASSERT(tree);
