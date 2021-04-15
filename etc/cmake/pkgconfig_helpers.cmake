5f0b0e6b8dae28ec1b585cb35af92f69fd7479b5
Tamas Nepusz <ntamas@gmail.com>
2021-04-15 14:55:33 +0200

    fix: convert full OpenMP library filename to short library flag in igraph.pc

--- etc/cmake/pkgconfig_helpers.cmake
+++ etc/cmake/pkgconfig_helpers.cmake
@@ -1,6 +1,24 @@
 # Helper functions for generating a nicely formatted igraph.pc file from
 # igraph.pc.in
 
+include(JoinPaths)
+
+# Converts the name of a library file (or framework on macOS) into an
+# appropriate linker flag if the library file resides in a standard
+# library path. Returns the input intact otherwise.
+function(convert_library_file_to_flags output_variable input)
+  get_filename_component(input_filename ${input} NAME_WE)
+  get_filename_component(input_extension ${input} LAST_EXT)
+  if(input_extension STREQUAL ${CMAKE_SHARED_LIBRARY_SUFFIX} OR input_extension STREQUAL ${CMAKE_STATIC_LIBRARY_SUFFIX})
+    string(REGEX REPLACE "^${CMAKE_SHARED_LIBRARY_PREFIX}" "" input_stripped ${input_filename})
+    set("${output_variable}" "-l${input_stripped}" PARENT_SCOPE)
+  elseif(APPLE AND input_extension STREQUAL ".framework")
+    set("${output_variable}" "-framework ${input_filename}" PARENT_SCOPE)
+  else()
+    set("${output_variable}" "${input}" PARENT_SCOPE)
+  endif()
+endfunction()
+
 # The library names being used here are Linux-specific, but pkgconfig files
 # are mostly used on Linux anyway. Nevertheless, we take care not to include
 # -lm on Windows because the Python interface of igraph uses the pkg-config
@@ -11,6 +29,7 @@ if(WIN32)
 else()
   set(PKGCONFIG_LIBS_PRIVATE "-lm")
 endif()
+
 if(APPLE)
   # All recent macOS distributions use libc++
   set(PKGCONFIG_LIBS_PRIVATE "${PKGCONFIG_LIBS_PRIVATE} -lc++")
@@ -42,14 +61,15 @@ if(NOT IGRAPH_USE_INTERNAL_ARPACK)
 endif()
 if(IGRAPH_OPENMP_SUPPORT AND OpenMP_FOUND)
   foreach(CURRENT_LIB ${OpenMP_C_LIB_NAMES})
-    set(PKGCONFIG_LIBS_PRIVATE "${PKGCONFIG_LIBS_PRIVATE} ${OpenMP_${CURRENT_LIB}_LIBRARY}")
+    convert_library_file_to_flags(CURRENT_LIB "${OpenMP_${CURRENT_LIB}_LIBRARY}")
+    set(PKGCONFIG_LIBS_PRIVATE "${PKGCONFIG_LIBS_PRIVATE} ${CURRENT_LIB}")
   endforeach()
 endif()
 
-include(JoinPaths)
-
-function(generate_pkgconfig_file SOURCE TARGET)
-  join_paths(PKGCONFIG_LIBDIR "\${exec_prefix}" "${CMAKE_INSTALL_LIBDIR}")
-  join_paths(PKGCONFIG_INCLUDEDIR "\${prefix}" "${CMAKE_INSTALL_INCLUDEDIR}")
-  configure_file(${SOURCE} ${TARGET})
-endfunction()
+join_paths(PKGCONFIG_LIBDIR "\${exec_prefix}" "${CMAKE_INSTALL_LIBDIR}")
+join_paths(PKGCONFIG_INCLUDEDIR "\${prefix}" "${CMAKE_INSTALL_INCLUDEDIR}")
+configure_file(
+  ${PROJECT_SOURCE_DIR}/igraph.pc.in
+  ${PROJECT_BINARY_DIR}/igraph.pc
+  @ONLY
+)
