983676712cd40005024d69b19f90dd4e4c414dd3
Szabolcs Horvát <szhorvat@gmail.com>
2021-04-26 14:27:09 +0200

    tests: exercise all splitting heursitics in bliss

--- tests/unit/igraph_isomorphic_bliss.c
+++ tests/unit/igraph_isomorphic_bliss.c
@@ -35,96 +35,93 @@ int main() {
     igraph_vector_t perm;
     igraph_bool_t iso;
 
-    igraph_rng_seed(igraph_rng_default(), 54321);
+    igraph_bliss_sh_t sh_list[] = {
+        IGRAPH_BLISS_F, IGRAPH_BLISS_FL,
+        IGRAPH_BLISS_FS, IGRAPH_BLISS_FM,
+        IGRAPH_BLISS_FLM, IGRAPH_BLISS_FSM
+    };
 
-    igraph_ring(&ring1, 100, /*directed=*/ 0, /*mutual=*/ 0, /*circular=*/1);
-    igraph_vector_init_seq(&perm, 0, igraph_vcount(&ring1) - 1);
-    igraph_vector_shuffle(&perm);
-    igraph_permute_vertices(&ring1, &ring2, &perm);
+    size_t i;
 
-    /* Without colors */
-    igraph_isomorphic_bliss(&ring1, &ring2, 0, 0, &iso, 0, 0, IGRAPH_BLISS_F, 0, 0);
-    if (!iso) {
-        fprintf(stderr, "Without color failed.\n");
-        return 1;
+    for (i=0; i < sizeof(sh_list) / sizeof(igraph_bliss_sh_t); ++i) {
+
+        igraph_bliss_sh_t sh = sh_list[i];
+
+        igraph_ring(&ring1, 100, /*directed=*/ 0, /*mutual=*/ 0, /*circular=*/1);
+        igraph_vector_init_seq(&perm, 0, igraph_vcount(&ring1) - 1);
+        igraph_vector_shuffle(&perm);
+        igraph_permute_vertices(&ring1, &ring2, &perm);
+
+        /* Without colors */
+        igraph_isomorphic_bliss(&ring1, &ring2, 0, 0, &iso, 0, 0, sh, 0, 0);
+        IGRAPH_ASSERT(iso);
+
+        /* Everything has the same colors */
+        igraph_vector_int_init(&color1, igraph_vcount(&ring1));
+        igraph_vector_int_init(&color2, igraph_vcount(&ring2));
+
+        igraph_isomorphic_bliss(&ring1, &ring2, &color1, &color2, &iso, 0, 0, sh, 0, 0);
+        IGRAPH_ASSERT(iso);
+
+        /* Try a negative result */
+        igraph_vector_int_fill(&color1, 0);
+        igraph_vector_int_fill(&color2, 0);
+        VECTOR(color1)[0] = 1;
+        igraph_isomorphic_bliss(&ring1, &ring2, &color1, &color2, &iso, 0, 0, sh, 0, 0);
+        IGRAPH_ASSERT(! iso);
+
+        /* Another negative, same color distribution, different topology */
+        igraph_vector_int_fill(&color1, 0);
+        igraph_vector_int_fill(&color2, 0);
+        VECTOR(color1)[0] = 1;
+        VECTOR(color1)[1] = 1;
+        VECTOR(color2)[0] = 1;
+        VECTOR(color2)[2] = 1;
+        igraph_isomorphic_bliss(&ring1, &ring2, &color1, &color2, &iso, 0, 0, sh, 0, 0);
+        IGRAPH_ASSERT(! iso);
+
+
+        /* More complicated test with colors */
+        igraph_vector_int_destroy(&color1);
+        igraph_vector_int_destroy(&color2);
+
+        igraph_vector_destroy(&perm);
+        igraph_destroy(&ring2);
+        igraph_destroy(&ring1);
+
+        igraph_small(&g1, 8, IGRAPH_DIRECTED,
+                     0, 4, 0, 5, 0, 6, 1, 4, 1, 5, 1, 7, 2, 4, 2, 6, 2, 7, 3, 5, 3, 6, 3, 7, -1
+                    );
+        igraph_small(&g2, 8, IGRAPH_DIRECTED,
+                     0, 1, 0, 3, 0, 4, 2, 3, 2, 1, 2, 6, 5, 1, 5, 4, 5, 6, 7, 3, 7, 6, 7, 4, -1
+                    );
+
+        igraph_vector_int_init(&color1, 8);
+        igraph_vector_int_init(&color2, 8);
+
+        VECTOR(color1)[1] = 1;
+        VECTOR(color1)[3] = 1;
+        VECTOR(color1)[5] = 1;
+        VECTOR(color1)[7] = 1;
+
+        VECTOR(color2)[2] = 1;
+        VECTOR(color2)[3] = 1;
+        VECTOR(color2)[6] = 1;
+        VECTOR(color2)[7] = 1;
+
+        iso = 0;
+        igraph_isomorphic_bliss(&g1, &g2, &color1, &color2, &iso, 0, 0, sh, 0, 0);
+        IGRAPH_ASSERT(iso);
+
+        igraph_vector_int_destroy(&color1);
+        igraph_vector_int_destroy(&color2);
+
+        igraph_destroy(&g2);
+        igraph_destroy(&g1);
+
+        VERIFY_FINALLY_STACK();
+
     }
-
-    /* Everything has the same colors */
-    igraph_vector_int_init(&color1, igraph_vcount(&ring1));
-    igraph_vector_int_init(&color2, igraph_vcount(&ring2));
-
-    igraph_isomorphic_bliss(&ring1, &ring2, &color1, &color2, &iso, 0, 0, IGRAPH_BLISS_F, 0, 0);
-    if (!iso) {
-        fprintf(stderr, "Single color failed.\n");
-        return 2;
-    }
-
-    /* Try a negative result */
-    igraph_vector_int_fill(&color1, 0);
-    igraph_vector_int_fill(&color2, 0);
-    VECTOR(color1)[0] = 1;
-    igraph_isomorphic_bliss(&ring1, &ring2, &color1, &color2, &iso, 0, 0, IGRAPH_BLISS_F, 0, 0);
-    if (iso) {
-        fprintf(stderr, "Negative test failed.\n");
-        return 3;
-    }
-
-    /* Another negative, same color distribution, different topology */
-    igraph_vector_int_fill(&color1, 0);
-    igraph_vector_int_fill(&color2, 0);
-    VECTOR(color1)[0] = 1;
-    VECTOR(color1)[1] = 1;
-    VECTOR(color2)[0] = 1;
-    VECTOR(color2)[2] = 1;
-    igraph_isomorphic_bliss(&ring1, &ring2, &color1, &color2, &iso, 0, 0, IGRAPH_BLISS_F, 0, 0);
-    if (iso) {
-        fprintf(stderr, "Second negative test failed.\n");
-        return 4;
-    }
-
-
-    /* More complicated test with colors */
-    igraph_vector_int_destroy(&color1);
-    igraph_vector_int_destroy(&color2);
-
-    igraph_vector_destroy(&perm);
-    igraph_destroy(&ring2);
-    igraph_destroy(&ring1);
-
-    igraph_small(&g1, 8, IGRAPH_DIRECTED,
-                 0, 4, 0, 5, 0, 6, 1, 4, 1, 5, 1, 7, 2, 4, 2, 6, 2, 7, 3, 5, 3, 6, 3, 7, -1
-                );
-    igraph_small(&g2, 8, IGRAPH_DIRECTED,
-                 0, 1, 0, 3, 0, 4, 2, 3, 2, 1, 2, 6, 5, 1, 5, 4, 5, 6, 7, 3, 7, 6, 7, 4, -1
-                );
-
-    igraph_vector_int_init(&color1, 8);
-    igraph_vector_int_init(&color2, 8);
-
-    VECTOR(color1)[1] = 1;
-    VECTOR(color1)[3] = 1;
-    VECTOR(color1)[5] = 1;
-    VECTOR(color1)[7] = 1;
-
-    VECTOR(color2)[2] = 1;
-    VECTOR(color2)[3] = 1;
-    VECTOR(color2)[6] = 1;
-    VECTOR(color2)[7] = 1;
-
-    iso = 0;
-    igraph_isomorphic_bliss(&g1, &g2, &color1, &color2, &iso, 0, 0, IGRAPH_BLISS_F, 0, 0);
-    if (!iso) {
-        fprintf(stderr, "BLISS failed to identify colored graphs as isomorphic.\n");
-        return 5;
-    }
-
-    igraph_vector_int_destroy(&color1);
-    igraph_vector_int_destroy(&color2);
-
-    igraph_destroy(&g2);
-    igraph_destroy(&g1);
-
-    VERIFY_FINALLY_STACK();
 
     return 0;
 }
