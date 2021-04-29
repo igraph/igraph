0bd90d6998e3fe3f957c9d6daba9c966d49bc8e5
Szabolcs Horvát <szhorvat@gmail.com>
2021-04-29 11:23:47 +0200

    chore: explain the purpose of IGRAPH_HANDLE_EXCEPTIONS

--- src/core/exceptions.h
+++ src/core/exceptions.h
@@ -4,6 +4,11 @@
 #include <exception>
 #include <new>
 
+/* igraph functions which may be called from C code must not throw C++ exceptions.
+ * This includes all public functions. This macro is meant to handle exceptions thrown
+ * by C++ libraries used by igraph (such as bliss). Wrap the entire body
+ * of public functions implemented in C++ in IGRAPH_HANDLE_EXCEPTIONS().
+ */
 #define IGRAPH_HANDLE_EXCEPTIONS(code) \
     try { code; } \
     catch (const std::bad_alloc &e) { IGRAPH_ERROR(e.what(), IGRAPH_ENOMEM); } \
