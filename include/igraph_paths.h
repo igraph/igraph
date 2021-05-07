d047b2f37c29304ec59392c4ae64e6b49013b23c
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-05-07 09:38:44 +0200

    Add pseudo_diameter_dijkstra (#1736)

--- include/igraph_paths.h
+++ include/igraph_paths.h
@@ -158,7 +158,17 @@ IGRAPH_EXPORT igraph_error_t igraph_pseudo_diameter(const igraph_t *graph,
                                          igraph_integer_t vid_start,
                                          igraph_integer_t *from,
                                          igraph_integer_t *to,
+                                         igraph_bool_t directed,
                                          igraph_bool_t unconn);
+
+IGRAPH_EXPORT igraph_error_t igraph_pseudo_diameter_dijkstra(const igraph_t *graph,
+                                                  const igraph_vector_t *weights,
+                                                  igraph_real_t *diameter,
+                                                  igraph_integer_t vid_start,
+                                                  igraph_integer_t *from,
+                                                  igraph_integer_t *to,
+                                                  igraph_bool_t directed,
+                                                  igraph_bool_t unconn);
 
 IGRAPH_EXPORT igraph_error_t igraph_get_all_simple_paths(const igraph_t *graph,
                                               igraph_vector_int_t *res,
