1ca191a24ac009b87b6db41459a9ebe36d55299b
Tamas Nepusz <ntamas@gmail.com>
2021-05-20 10:05:33 +0200

    fix: just use the Zachary karate club graph from igraph_famous() in tests

--- tests/unit/igraph_edge_betweenness_subset.c
+++ tests/unit/igraph_edge_betweenness_subset.c
@@ -178,52 +178,30 @@ int main() {
     igraph_vector_destroy(&bet2);
     igraph_destroy(&g);
 
-    {
-        /* We use igraph_create() instead of igraph_small() as some MSVC versions
-           will choke on an overlong argument list with "internal error C1001". */
-        igraph_real_t edge_array[] = {
-            0,  1,  0,  2,  0,  3,  0,  4,  0,  5,
-            0,  6,  0,  7,  0,  8,  0, 10,  0, 11,
-            0, 12,  0, 13,  0, 17,  0, 19,  0, 21,
-            0, 31,  1,  2,  1,  3,  1,  7,  1, 13,
-            1, 17,  1, 19,  1, 21,  1, 30,  2,  3,
-            2,  7,  2,  8,  2,  9,  2, 13,  2, 27,
-            2, 28,  2, 32,  3,  7,  3, 12,  3, 13,
-            4,  6,  4, 10,  5,  6,  5, 10,  5, 16,
-            6, 16,  8, 30,  8, 32,  8, 33,  9, 33,
-            13, 33, 14, 32, 14, 33, 15, 32, 15, 33,
-            18, 32, 18, 33, 19, 33, 20, 32, 20, 33,
-            22, 32, 22, 33, 23, 25, 23, 27, 23, 29,
-            23, 32, 23, 33, 24, 25, 24, 27, 24, 31,
-            25, 31, 26, 29, 26, 33, 27, 33, 28, 31,
-            28, 33, 29, 32, 29, 33, 30, 32, 30, 33,
-            31, 32, 31, 33, 32, 33
-        };
-        printf("\nLarge unweighted graph, edge betweenness\n");
-        printf("==========================================================\n");
-        igraph_create(&g, igraph_vector_view(&edges, edge_array, sizeof(edge_array) / sizeof(igraph_real_t)), 0, IGRAPH_UNDIRECTED);
-        igraph_vector_init_seq(&source_vec, 0, 32);
-        igraph_vs_vector(&vs_source, &source_vec);
-        igraph_vector_init_seq(&target_vec, 1, 33);
-        igraph_vs_vector(&vs_target, &target_vec);
-        igraph_vector_init(&eb, 0);
+    printf("\nZachary karate club, unweighted graph, edge betweenness\n");
+    printf("==========================================================\n");
+    igraph_famous(&g, "zachary");
+    igraph_vector_init_seq(&source_vec, 0, 32);
+    igraph_vs_vector(&vs_source, &source_vec);
+    igraph_vector_init_seq(&target_vec, 1, 33);
+    igraph_vs_vector(&vs_target, &target_vec);
+    igraph_vector_init(&eb, 0);
 
-        igraph_edge_betweenness_subset (/* graph=     */ &g,
-        /* res=       */ &eb,
-        /* eids=      */ igraph_ess_all(IGRAPH_EDGEORDER_ID),
-        /* directed = */ IGRAPH_UNDIRECTED,
-        /* sources = */ vs_source,
-        /* target = */ vs_target,
-        /* weights=   */ NULL);
+    igraph_edge_betweenness_subset (/* graph=     */ &g,
+    /* res=       */ &eb,
+    /* eids=      */ igraph_ess_all(IGRAPH_EDGEORDER_ID),
+    /* directed = */ IGRAPH_UNDIRECTED,
+    /* sources = */ vs_source,
+    /* target = */ vs_target,
+    /* weights=   */ NULL);
 
-        print_vector(&eb);
-        igraph_vector_destroy(&eb);
-        igraph_destroy(&g);
-        igraph_vs_destroy(&vs_source);
-        igraph_vector_destroy(&source_vec);
-        igraph_vs_destroy(&vs_target);
-        igraph_vector_destroy(&target_vec);
-    }
+    print_vector(&eb);
+    igraph_vector_destroy(&eb);
+    igraph_destroy(&g);
+    igraph_vs_destroy(&vs_source);
+    igraph_vector_destroy(&source_vec);
+    igraph_vs_destroy(&vs_target);
+    igraph_vector_destroy(&target_vec);
         
     printf("\nSmall unweighted graph, edge betweenness\n");
     printf("==========================================================\n");
