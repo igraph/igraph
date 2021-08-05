3bd062234f57b298ea44d635e8cdae65638feb97
Nachiket18 <nachideo.em@gmail.com>
2021-08-04 20:16:38 -0400

    Added code for calculating the distance for subsets of size greater than 1

--- src/paths/steiner.c
+++ src/paths/steiner.c
@@ -12,7 +12,7 @@
 
 
 
-igraph_vector_t generateSubsets(igraph_vector_t* C, igraph_integer_t n,igraph_integer_t graphsize)
+igraph_vector_t* generateSubsets(igraph_vector_t* C, igraph_integer_t n,igraph_integer_t graphsize)
 {
     igraph_integer_t count = pow(2, n);
 
@@ -70,16 +70,34 @@ igraph_vector_t generateSubsets(igraph_vector_t* C, igraph_integer_t n,igraph_in
 }
 
 
+int fetchIndexofMapofSets(igraph_vector_t subset)
+{
+    int key;
+    for(map< igraph_vector_t , int >::const_iterator it = subsetMap.begin(); it != subsetMap.end(); ++it)
+    {
 
-int igraph_get_all_shortest_paths(const igraph_t *graph,
+        if (it -> first == subset)
+        {
+            key = it ->second;
+        }
+    }
+
+ return key;
+}
+
+
+
+int igraph_steiner_dreyfus_wagner(const igraph_t *graph,
                                   igraph_vector_t *steiner_terminals,
                                   igraph_neimode_t mode) {
         
 
 igraph_integer_t no_of_vertices = (igraph_integer_t) igraph_vcount(&graph);
+
 igraph_vector_t steiner_vertices;
 igraph_matrix_t dp_cache; // dynamic programming table
-
+igraph_integer_t q;
+igraph_vector_t *allSubsets;
 
 igraph_vector_sort(&steiner_terminals);
 
@@ -96,6 +114,16 @@ for (int i = 0,j = 0;i < igraph_vector_size(&steiner_terminals); i++,j++)
 igraph_matrix_init(&dp_cache,pow(2,igraph_vector_size(&steiner_terminals)),igraph_vector_size(&steiner_vertices));
 igraph_matrix_fill(&dp_cache,INT_MAX);
 
+
+q = VECTOR[steiner_terminals][0];
+
+igraph_vector_remove(&steiner_terminals,0);
+
+allSubsets = generateSubsets(&steiner_terminals,igraph_vector_size(&steiner_terminals),no_of_vertices);
+
+
+
+
 // Singleton subset rows may be filled in trivially
 
 for (int i =0; i < igraph_vector_size(&steiner_terminals); i++)
@@ -106,6 +134,79 @@ for (int i =0; i < igraph_vector_size(&steiner_terminals); i++)
 	
 	}	
 }
+
+
+for (int i = igraph_vector_size(&steiner_terminals); i < igraph_matrix_capacity(&dp_cache); i++ )
+{
+	igraph_vector_t D = VECTOR[allsubsets][i];
+	igraph_integer_t indexOfSubsetD
+	
+	if(D.size() == 1)
+        {
+        	indexOfSubsetD = VECTOR[D][0];
+        }
+        else
+        {
+                indexOfSubsetD = fetchIndexofMapofSets(&D);
+        }
+	
+	
+	for (int u = 0; u < igraph_vector_size(&steiner_vertices); u++ )
+	{
+		
+		for (int v = 0; v < igraph_vector_size(&steiner_vertices); v++ )
+		{
+			
+			for (int subset_iterator = 0; subset_iterator < igraph_vector_size(&D); subset_iterator++ )	
+			{ 	
+				igraph_integer_t indexOfSubsetDPrime
+				
+				if(VECTOR[D][subset_iterator].size() == 1)
+             		        {
+                            		indexOfSubsetDPrime = VECTOR[D][subset_iterator];
+                        	}
+                        	else
+                        	{
+                             		indexOfSubsetDPrime = fetchIndexofMapofSets(&VECTOR[D][subset_iterator]);
+                        	}	
+				
+			
+				igraph_integer_t DprimetoU = MATRIX[dp_cache][indexOfSubsetDPrime][u];
+				
+				igraph_vector_t DminusDprime = D;
+				igraph_vector_remove(&VECTOR[D][subset_iterator],E);	
+				
+				igraph_integer_t DminusDprime = VECTOR[DminusE][u]
+				igraph_integer_t indexOfSubsetDminusDprime
+				
+				if(DminusDprime.size() == 1)
+             		        {
+                            		indexOfSubsetDminusDprime = VECTOR[DminusDprime][0];
+                        	}
+                        	else
+                        	{
+                             		indexOfSubsetDminusDprime = fetchIndexofMapofSets(&VECTOR[D][subset_iterator]);
+                        	}	
+				
+	
+				MATRIX[dp_cache][indexOfSubsetDminusDprime][u]
+				igraph_integer_t distance_u_v = MATRIX[distance][u][v]
+				
+				if (MATRIX[dp_cache][indexOfSubsetD][v] > DprimetoU + DminusDprime + distance_u_v )
+				{
+					MATRIX[dp_cache][indexOfSubsetD][v] = DprimetoU + DminusDprime + distance_u_v 
+				}			
+				
+				
+			}
+				
+		}
+		
+	}
+		
+				
+}
+
 
 
 
