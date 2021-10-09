85ed790871198f1837c88634130338ca984eb274
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-10-09 16:39:04 +0200

    Start testing igraph_random_walk

---
+++ tests/unit/igraph_random_walk.c
@@ -0,0 +1,43 @@
+/*
+   IGraph library.
+   Copyright (C) 2021  The igraph development team <igraph@igraph.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <igraph.h>
+#include "test_utilities.inc"
+
+void random_walk(igraph_t *graph, igraph_integer_t start, igraph_neimode_t mode,
+		igraph_integer_t steps, igraph_random_walk_stuck_t stuck) {
+	igraph_vector_int_t walk;
+
+	igraph_vector_int_init(&walk, 0);
+	igraph_random_walk(graph, &walk, start, mode, steps, stuck);
+	print_vector_int(&walk);
+	igraph_vector_int_destroy(&walk);
+}
+
+int main() {
+    igraph_t g_1;
+
+	igraph_small(&g_1, 1, IGRAPH_UNDIRECTED, -1);
+
+	random_walk(&g_1, 0, IGRAPH_OUT, 0, IGRAPH_RANDOM_WALK_STUCK_RETURN);
+
+    igraph_destroy(&g_1);
+
+    VERIFY_FINALLY_STACK();
+    return 0;
+}
