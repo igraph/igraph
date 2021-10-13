7136389e07ff859c31cbf31437181efea6cd1fb7
Tamas Nepusz <ntamas@gmail.com>
2021-10-13 21:16:10 +0200

    fix: igraph_reindex_membership() should not allow negative membership indices

--- src/community/community_misc.c
+++ src/community/community_misc.c
@@ -270,6 +270,11 @@ igraph_error_t igraph_reindex_membership(igraph_vector_int_t *membership,
     for (i = 0; i < n; i++) {
         igraph_integer_t c = VECTOR(*membership)[i];
 
+        if (c < 0) {
+            IGRAPH_ERRORF("Membership indices should non-negative. "
+            "Found member of cluster %ld.", IGRAPH_EINVAL, c);
+        }
+
         if (c >= n) {
             IGRAPH_ERRORF("Membership indices should be less than total number of vertices. "
             "Found member of cluster %ld, but only %ld vertices.", IGRAPH_EINVAL, c, n);
