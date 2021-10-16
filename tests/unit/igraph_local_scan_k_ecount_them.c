cc452fe1fa2f7915276c1a0aa66e8a7ed08d1610
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-10-16 12:44:48 +0200

    Add more local_scan_k_ecount_them unit tests
    Add some tests where the us and them graphs are not equal.

--- tests/unit/igraph_local_scan_k_ecount_them.c
+++ tests/unit/igraph_local_scan_k_ecount_them.c
@@ -28,17 +28,21 @@ void call_and_print(igraph_t *us, igraph_t *them, int k, igraph_vector_t *weight
 
 
 int main() {
-    igraph_t g_0, g_1, g_lmu, g_lm, g_lm_nl;
+    igraph_t g_0, g_1, g_lmu, g_lm, g_lm_nl, g_6, g_6_1, g_6_full;
     igraph_vector_t weights, result;
 
     igraph_vector_init_real(&weights, 8, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6);
     igraph_vector_init(&result, 0);
     igraph_small(&g_0, 0, 0, -1);
     igraph_small(&g_1, 1, 0, -1);
+    igraph_small(&g_6, 6, 1, -1);
+    igraph_small(&g_6_1, 6, 1, 0,1, -1);
+    igraph_full(&g_6_full, 6, 1, 0);
     igraph_small(&g_lm, 6, 1, 0,1, 0,2, 1,1, 1,3, 2,0, 2,3, 3,4, 3,4, -1);
     igraph_small(&g_lmu, 6, 0, 0,1, 0,2, 1,1, 1,3, 2,0, 2,3, 3,4, 3,4, -1); //undirected
     igraph_small(&g_lm_nl, 6, 1, 0,1, 0,2, 1,3, 2,0, 2,3, 3,4, 3,4, -1); // no loop
 
+    printf("First some tests where us and them are equal:\n");
     printf("No vertices:\n");
     call_and_print(&g_0, &g_0, 2, NULL, IGRAPH_ALL);
 
@@ -66,6 +70,32 @@ int main() {
     printf("Same graph, weighted:\n");
     call_and_print(&g_lmu, &g_lmu, 2, &weights, IGRAPH_IN);
 
+    printf("Now some tests where us and them are not equal:\n");
+    printf("Us = same graph, them = edgless, directed, k=1, "
+           "should show 0, because there are no edges:\n");
+    call_and_print(&g_lm, &g_6, 1, NULL, IGRAPH_IN);
+
+    printf("Switched us and them, "
+           "should show only 1 at the loop:\n");
+    call_and_print(&g_6, &g_lm, 1, NULL, IGRAPH_IN);
+
+    printf("Us = same graph, them = only edge from 0 to 1, "
+           "directed, k=1, IGRAPH_IN, "
+           "should show edge from 0 to 1:\n");
+    call_and_print(&g_lm, &g_6_1, 1, NULL, IGRAPH_IN);
+
+    printf("Switched us and them, "
+           "should show edge from 0 to 1 and loop:\n");
+    call_and_print(&g_6_1, &g_lm, 1, NULL, IGRAPH_IN);
+
+    printf("Us = same graph, them = full graph, "
+           "directed, k=3, IGRAPH_ALL, "
+           "should show 4*5=20 edges for the connected part:\n");
+    call_and_print(&g_lm, &g_6_full, 3, NULL, IGRAPH_ALL);
+
+    printf("Switched us and them, "
+           "should show 8 edges for the whole graph:\n");
+    call_and_print(&g_6_full, &g_lm, 3, NULL, IGRAPH_ALL);
     VERIFY_FINALLY_STACK();
 
     printf("Check error handling:\n");
@@ -76,11 +106,20 @@ int main() {
     printf("Negative k.\n");
     CHECK_ERROR(igraph_local_scan_k_ecount_them(&g_lmu, &g_lmu, -3, &result, NULL, IGRAPH_ALL), IGRAPH_EINVAL);
 
+    printf("Number of vertices in us and them not equal.\n");
+    CHECK_ERROR(igraph_local_scan_k_ecount_them(&g_lmu, &g_1, 3, &result, NULL, IGRAPH_ALL), IGRAPH_EINVAL);
+
+    printf("Directedness in us and them not equal.\n");
+    CHECK_ERROR(igraph_local_scan_k_ecount_them(&g_lmu, &g_lm, 3, &result, NULL, IGRAPH_ALL), IGRAPH_EINVAL);
+
     igraph_destroy(&g_0);
     igraph_destroy(&g_1);
     igraph_destroy(&g_lmu);
     igraph_destroy(&g_lm);
     igraph_destroy(&g_lm_nl);
+    igraph_destroy(&g_6);
+    igraph_destroy(&g_6_1);
+    igraph_destroy(&g_6_full);
     igraph_vector_destroy(&weights);
     igraph_vector_destroy(&result);
 
