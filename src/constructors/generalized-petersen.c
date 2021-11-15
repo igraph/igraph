503e54a3c639989662ebe3250f135e5318fea012
alexsyou <54590871+alexsyou@users.noreply.github.com>
2021-11-15 10:17:20 -0500

    Create Generalized Petersen Graph
    Initialize edges and vertices and use igraph_create to create graph.

--- src/constructors/generalized-petersen.c
+++ src/constructors/generalized-petersen.c
@@ -21,23 +21,34 @@
 #include "igraph_interface.h"
 
 
-int igraph_generalized_petersen(igraph_t *igraph, igraph_integer_t n, igraph_integer_t k) {
+igraph_error_t igraph_generalized_petersen(igraph_t *graph, igraph_integer_t n, igraph_integer_t k) {
     /* This is a generalized Petersen graph constructor*/
     igraph_vector_int_t edges;
-
-    IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
+    igraph_integer_t v = 2*n;
+    igraph_integer_t i;
 
     if (n < 3) {
         IGRAPH_ERROR("n must be greater than or equal to 3", IGRAPH_EINVAL);
     }
 
-    if (k < 1 || k > (n-1)/2) {
+    if (k < 1 || k > ((n-1)/2)) {
         IGRAPH_ERROR("k must be positive and less than (n-1)/2", IGRAPH_EINVAL);
     }
 
+    IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
+    IGRAPH_CHECK(igraph_vector_int_reserve(&edges, 3*n));
 
+    for (i = 0; i < n; i++) {
+        igraph_vector_int_push_back(&edges, i);
+        igraph_vector_int_push_back(&edges, (i + 1) % n);
+        igraph_vector_int_push_back(&edges, i);
+        igraph_vector_int_push_back(&edges, i + n);
+        igraph_vector_int_push_back(&edges, i + n);
+        igraph_vector_int_push_back(&edges, ((i + k) % n) + n);
+    }
 
-
+    IGRAPH_CHECK(igraph_create(graph, &edges, v, IGRAPH_UNDIRECTED));
+    
     igraph_vector_int_destroy(&edges);
     IGRAPH_FINALLY_CLEAN(1);
     /* Return no error */
