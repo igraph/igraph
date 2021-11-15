5a76b3d3829d73cc16b83871f91974fc2a8a2d38
alexsyou <54590871+alexsyou@users.noreply.github.com>
2021-11-15 17:12:40 -0500

    generalized-petersen: Include Generalized-Petersen In igraph_constructors.h

--- tests/unit/igraph_generalized_petersen.c
+++ tests/unit/igraph_generalized_petersen.c
@@ -24,6 +24,7 @@ int main() {
     igraph_t graph, graph_test;
     igraph_bool_t iso;
 
+    /* Compares G(5,2) with Petersen Graph */
     IGRAPH_ASSERT(igraph_generalized_petersen(&graph, /* n */ 5, /* k */ 2) == IGRAPH_SUCCESS);
     igraph_small(&graph_test, 10, IGRAPH_UNDIRECTED, 0, 2, 0, 3, 0, 5, 1, 3, 1, 4, 1, 6, 2, 4, 2, 7, 3, 8, 4, 9, 5, 6, 5, 9, 6, 7, 7, 8, 8, 9, -1);
     IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
