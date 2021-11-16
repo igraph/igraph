1340c64fb150a7cb3e96cb447778d976b95c76eb
alexsyou <54590871+alexsyou@users.noreply.github.com>
2021-11-16 18:24:49 -0500

    generalized-petersen: Add Test For Small And Dodecahedron

--- tests/unit/igraph_generalized_petersen.c
+++ tests/unit/igraph_generalized_petersen.c
@@ -24,17 +24,25 @@ int main() {
     igraph_t graph, graph_test;
     igraph_bool_t iso;
 
-    /* Compares G(5,2) with Petersen Graph */
+    /* Compares G(5,2) with Petersen Graph in igraph_famous */
     IGRAPH_ASSERT(igraph_generalized_petersen(&graph, /* n */ 5, /* k */ 2) == IGRAPH_SUCCESS);
-    igraph_small(&graph_test, 10, IGRAPH_UNDIRECTED, 0, 2, 0, 3, 0, 5, 1, 3, 1, 4, 1, 6, 2, 4, 2, 7, 3, 8, 4, 9, 5, 6, 5, 9, 6, 7, 7, 8, 8, 9, -1);
+    igraph_famous(&graph_test, "petersen");
     IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
     IGRAPH_ASSERT(iso);
     igraph_destroy(&graph);
     igraph_destroy(&graph_test);
 
-    /* Compares G(5,2) with Petersen Graph in igraph_famous */
-    IGRAPH_ASSERT(igraph_generalized_petersen(&graph, /* n */ 5, /* k */ 2) == IGRAPH_SUCCESS);
-    igraph_famous(&graph_test, "petersen");
+    /* Compares G(4,2) with expected small graph */
+    IGRAPH_ASSERT(igraph_generalized_petersen(&graph, /* n */ 4, /* k */ 2) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 8, IGRAPH_UNDIRECTED, 0, 2, 0, 4, 1, 3, 1, 5, 2, 6, 3, 7, 4, 5, 4, 7, 5, 6, 6, 7, -1);
+    IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+
+    /* Compares G(10,2) with Dodecahedral Graph in igraph_famous */
+    IGRAPH_ASSERT(igraph_generalized_petersen(&graph, /* n */ 10, /* k */ 2) == IGRAPH_SUCCESS);
+    igraph_famous(&graph_test, "dodecahedron");
     IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
     IGRAPH_ASSERT(iso);
     igraph_destroy(&graph);
