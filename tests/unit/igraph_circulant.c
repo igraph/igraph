122469ede86882debf1b7008a2b71ed29af575ca
Gomango999 <kv.zhu999@gmail.com>
2021-11-25 18:36:01 +1100

    Add all tests

--- tests/unit/igraph_circulant.c
+++ tests/unit/igraph_circulant.c
@@ -27,8 +27,43 @@ int main() {
     igraph_bool_t iso;
     igraph_integer_t i;
 
+    /* Testing n = 1 case */
+    /* n = 1, l = [1], undirected */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, 1);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 1, &l, IGRAPH_UNDIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 1, IGRAPH_UNDIRECTED, -1);
+    IGRAPH_ASSERT(igraph_is_same_graph(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* Testing n = 2 case */
+    /* n = 2, l = [1], undirected */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, 1);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 2, &l, IGRAPH_UNDIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 2, IGRAPH_UNDIRECTED, 0, 1, -1);
+    IGRAPH_ASSERT(igraph_is_same_graph(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* Testing empty list case */
+    /* n = 5, l = [], undirected */
+    igraph_vector_int_init(&l, 0);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 5, &l, IGRAPH_UNDIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 5, IGRAPH_UNDIRECTED, -1);
+    IGRAPH_ASSERT(igraph_is_same_graph(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
     /* Testing typical use case */
-    /* Test n = 5, undirected, l = [1, 2] */
+    /* Test n = 5, l = [1, 2], undirected */
     igraph_vector_int_init(&l, 0);
     igraph_vector_int_push_back(&l, 1);
     igraph_vector_int_push_back(&l, 2);
@@ -43,7 +78,7 @@ int main() {
     igraph_vector_int_destroy(&l);
 
     /* Testing simplification */
-    /* n = 6, undirected, l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] */
+    /* n = 6, l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], undirected */
     igraph_vector_int_init(&l, 0);
     for (i = 1; i <= 15; i++) {
         igraph_vector_int_push_back(&l, i);
@@ -56,7 +91,85 @@ int main() {
     igraph_destroy(&graph_test);
     igraph_vector_int_destroy(&l);
 
-    // todo: add more tests
+    /* Testing simplification with negative values */
+    /* n = 7, l = [-15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5,
+            -4, -3, -2, -1, 0, 1, 2, 3, 4], undirected */
+    igraph_vector_int_init(&l, 0);
+    for (i = -15; i <= 4; i++) {
+        igraph_vector_int_push_back(&l, i);
+    }
+    IGRAPH_ASSERT(igraph_circulant(&graph, 7, &l, IGRAPH_UNDIRECTED) == IGRAPH_SUCCESS);
+    igraph_full(&graph_test, 7, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+    IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* Testing simplification when n is even, the offset = n/2, and the graph is undirected */
+    /* n = 6, l = [3], undirected */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, 3);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 6, &l, IGRAPH_UNDIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 6, IGRAPH_UNDIRECTED, 0, 3, 1, 4, 2, 5, -1);
+    IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* Testing directed graph */
+    /* n = 7, l = [1, -2], directed */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, 1);
+    igraph_vector_int_push_back(&l, -2);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 7, &l, IGRAPH_DIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 7, IGRAPH_DIRECTED, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 0,
+            0, 5, 1, 6, 2, 0, 3, 1, 4, 2, 5, 3, 6, 4, -1);
+    IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* Testing simplification works for directed too */
+    /* n = 5, l = [-4, 1, 6], directed */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, -4);
+    igraph_vector_int_push_back(&l, 1);
+    igraph_vector_int_push_back(&l, 6);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 5, &l, IGRAPH_DIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 5, IGRAPH_DIRECTED, 0, 1, 1, 2, 2, 3, 3, 4, 4, 0, -1);
+    IGRAPH_ASSERT(igraph_isomorphic(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* Testing that things that are normally simplified for undirected are not simplified for directed */
+    /* n = 5, l = [1, -1], directed */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, 1);
+    igraph_vector_int_push_back(&l, -1);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 5, &l, IGRAPH_DIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 5, IGRAPH_DIRECTED, 0, 1, 1, 2, 2, 3, 3, 4, 4, 0,
+            1, 0, 2, 1, 3, 2, 4, 3, 0, 4, -1);
+    IGRAPH_ASSERT(igraph_is_same_graph(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
+
+    /* n = 6, l = [3], directed */
+    igraph_vector_int_init(&l, 0);
+    igraph_vector_int_push_back(&l, 3);
+    IGRAPH_ASSERT(igraph_circulant(&graph, 6, &l, IGRAPH_DIRECTED) == IGRAPH_SUCCESS);
+    igraph_small(&graph_test, 6, IGRAPH_DIRECTED, 0, 3, 1, 4, 2, 5, 3, 0, 4, 1, 5, 2, -1);
+    IGRAPH_ASSERT(igraph_is_same_graph(&graph, &graph_test, &iso) == IGRAPH_SUCCESS);
+    IGRAPH_ASSERT(iso);
+    igraph_destroy(&graph);
+    igraph_destroy(&graph_test);
+    igraph_vector_int_destroy(&l);
 
     VERIFY_FINALLY_STACK();
     return 0;
