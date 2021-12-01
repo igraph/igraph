8e0e1dc08b5e929af006d5806366e250f236bf5d
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-01 11:30:45 +0100

    Add a basic gradient function

--- src/misc/umap.c
+++ src/misc/umap.c
@@ -15,7 +15,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+   */
 
 #include "igraph_matrix.h"
 #include "igraph_interface.h"
@@ -27,29 +27,29 @@ static igraph_error_t igraph_get_k_smallest(igraph_vector_t *values,
         igraph_vector_int_t *smallest, igraph_integer_t k, igraph_integer_t starting_index)
 {
     igraph_vector_t smallest_values;
-	igraph_real_t value;
-	igraph_integer_t max_index;
+    igraph_real_t value;
+    igraph_integer_t max_index;
 
     IGRAPH_VECTOR_INIT_FINALLY(&smallest_values, 0);
-	igraph_vector_int_resize(smallest, 0);
-	for (igraph_integer_t i = starting_index; i < igraph_vector_size(values); i++) {
-		value = VECTOR(*values)[i];
-		if (igraph_vector_int_size(smallest) < k) {
-			igraph_vector_int_push_back(smallest, i);
-			igraph_vector_push_back(&smallest_values, i);
-		} else {
-			max_index = 0;
-			for (igraph_integer_t j = 1; j < igraph_vector_int_size(smallest); j++) {
-				if (VECTOR(smallest_values)[j] > VECTOR(smallest_values)[max_index]) {
-					max_index = j;
-				}
-			}
-			if (value > VECTOR(smallest_values)[max_index]) {
-				VECTOR(smallest_values)[max_index] = value;
-				VECTOR(*smallest)[max_index] = i;
-			}
-		}
-	}
+    igraph_vector_int_resize(smallest, 0);
+    for (igraph_integer_t i = starting_index; i < igraph_vector_size(values); i++) {
+        value = VECTOR(*values)[i];
+        if (igraph_vector_int_size(smallest) < k) {
+            igraph_vector_int_push_back(smallest, i);
+            igraph_vector_push_back(&smallest_values, i);
+        } else {
+            max_index = 0;
+            for (igraph_integer_t j = 1; j < igraph_vector_int_size(smallest); j++) {
+                if (VECTOR(smallest_values)[j] > VECTOR(smallest_values)[max_index]) {
+                    max_index = j;
+                }
+            }
+            if (value > VECTOR(smallest_values)[max_index]) {
+                VECTOR(smallest_values)[max_index] = value;
+                VECTOR(*smallest)[max_index] = i;
+            }
+        }
+    }
     igraph_vector_destroy(&smallest_values);
     IGRAPH_FINALLY_CLEAN(1);
     return (IGRAPH_SUCCESS);
@@ -109,25 +109,25 @@ static igraph_error_t igraph_umap_find_open_sets(igraph_t *knn_graph,
         igraph_vector_t *open_set_decays) {
     igraph_integer_t no_of_nodes = igraph_vcount(knn_graph);
     igraph_vector_int_t eids;
-	igraph_real_t largest;
+    igraph_real_t largest;
 
-	/*this picks the shortest distance*/
+    /*this picks the shortest distance*/
     IGRAPH_VECTOR_INT_INIT_FINALLY(&eids, 0);
     for (igraph_integer_t i = 0; i < no_of_nodes; i++) {
-		igraph_incident(knn_graph, &eids, i, IGRAPH_ALL);
-		VECTOR(*open_set_sizes)[i] = VECTOR(*distances)[VECTOR(eids)[0]];
-		largest = -1;
-		for (igraph_integer_t j = 1; j < igraph_vector_int_size(&eids); j++) {
-			if (VECTOR(*distances)[VECTOR(eids)[j]] < VECTOR(*open_set_sizes)[i]) {
-				VECTOR(*open_set_sizes)[i] = VECTOR(*distances)[VECTOR(eids)[j]];
-			}
-			if (VECTOR(*distances)[VECTOR(eids)[j]] > largest) {
-				largest = VECTOR(*distances)[VECTOR(eids)[j]];
-			}
-		}
-		VECTOR(*open_set_decays)[i] = (largest - VECTOR(*open_set_sizes)[i]);
+        igraph_incident(knn_graph, &eids, i, IGRAPH_ALL);
+        VECTOR(*open_set_sizes)[i] = VECTOR(*distances)[VECTOR(eids)[0]];
+        largest = -1;
+        for (igraph_integer_t j = 1; j < igraph_vector_int_size(&eids); j++) {
+            if (VECTOR(*distances)[VECTOR(eids)[j]] < VECTOR(*open_set_sizes)[i]) {
+                VECTOR(*open_set_sizes)[i] = VECTOR(*distances)[VECTOR(eids)[j]];
+            }
+            if (VECTOR(*distances)[VECTOR(eids)[j]] > largest) {
+                largest = VECTOR(*distances)[VECTOR(eids)[j]];
+            }
+        }
+        VECTOR(*open_set_decays)[i] = (largest - VECTOR(*open_set_sizes)[i]);
 
-	}
+    }
     igraph_vector_int_destroy(&eids);
     IGRAPH_FINALLY_CLEAN(1);
     return (IGRAPH_SUCCESS);
@@ -136,7 +136,7 @@ static igraph_error_t igraph_umap_find_open_sets(igraph_t *knn_graph,
 
 igraph_error_t igraph_umap_decay(igraph_real_t *weight, igraph_real_t distance, igraph_real_t open_set_size, igraph_real_t open_set_decay)
 {
-	return (IGRAPH_SUCCESS);
+    return (IGRAPH_SUCCESS);
 }
 
 
@@ -148,16 +148,16 @@ static igraph_error_t igraph_umap_edge_weights(igraph_matrix_t *data,
         igraph_t *umap_graph, igraph_vector_t *umap_weights, igraph_vector_t *open_set_sizes,
         igraph_vector_t *open_set_decays) {
 
-	/* we go over all the nodes, and then over all the nodes, and then add an edge with the weight dependent on the open set and decay*/
-	/* and no edge if this weight would be 0 or lower */
+    /* we go over all the nodes, and then over all the nodes, and then add an edge with the weight dependent on the open set and decay*/
+    /* and no edge if this weight would be 0 or lower */
     igraph_integer_t no_of_nodes = igraph_matrix_nrow(data);
     igraph_vector_int_t edges;
     igraph_vector_t data_row_i;
     igraph_vector_t data_row_j;
-	igraph_real_t distance;
-	igraph_real_t weight_a;
-	igraph_real_t weight_b;
-	igraph_real_t weight;
+    igraph_real_t distance;
+    igraph_real_t weight_a;
+    igraph_real_t weight_b;
+    igraph_real_t weight;
 
     IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
     for (igraph_integer_t i = 0; i < no_of_nodes; i++) {
@@ -166,16 +166,16 @@ static igraph_error_t igraph_umap_edge_weights(igraph_matrix_t *data,
             igraph_matrix_get_row(data, &data_row_j, j);
             igraph_vector_sub(&data_row_j, &data_row_i);
             distance = igraph_vector_sumsq(&data_row_j);
-			IGRAPH_CHECK(igraph_umap_decay(&weight_a,  distance, VECTOR(*open_set_sizes)[i], VECTOR(*open_set_decays)[i]));
-			IGRAPH_CHECK(igraph_umap_decay(&weight_b, distance, VECTOR(*open_set_sizes)[j], VECTOR(*open_set_decays)[j]));
-			if (weight_a <= 0 || weight_b <= 0)
-				continue;
-			weight = weight_a + weight_b - weight_a * weight_b;
+            IGRAPH_CHECK(igraph_umap_decay(&weight_a,  distance, VECTOR(*open_set_sizes)[i], VECTOR(*open_set_decays)[i]));
+            IGRAPH_CHECK(igraph_umap_decay(&weight_b, distance, VECTOR(*open_set_sizes)[j], VECTOR(*open_set_decays)[j]));
+            if (weight_a <= 0 || weight_b <= 0)
+                continue;
+            weight = weight_a + weight_b - weight_a * weight_b;
             IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
             IGRAPH_CHECK(igraph_vector_int_push_back(&edges, j));
             IGRAPH_CHECK(igraph_vector_push_back(umap_weights, weight));
-		}
-	}
+        }
+    }
 
     IGRAPH_CHECK(igraph_create(umap_graph, &edges, no_of_nodes, IGRAPH_UNDIRECTED));
     igraph_vector_int_destroy(&edges);
@@ -183,19 +183,48 @@ static igraph_error_t igraph_umap_edge_weights(igraph_matrix_t *data,
     return IGRAPH_SUCCESS;
 }
 
+static igraph_error_t igraph_get_gradient(igraph_matrix_t *gradient, igraph_matrix_t *layout, igraph_t *umap_graph, igraph_vector_t *umap_weights)
+{
+    igraph_integer_t no_of_nodes = igraph_matrix_nrow(layout);
+    igraph_vector_int_t eids;
+    IGRAPH_VECTOR_INT_INIT_FINALLY(&eids, 0);
+    for (igraph_integer_t i = 0; i < no_of_nodes; i++) {
+        igraph_incident(umap_graph, &eids, i, IGRAPH_ALL);
+        MATRIX(*gradient, i, 0) = 0;
+        MATRIX(*gradient, i, 1) = 0;
+        for (igraph_integer_t j = 1; j < igraph_vector_int_size(&eids); j++) {
+            igraph_integer_t eid = VECTOR(eids)[j];
+            igraph_real_t x = MATRIX(*layout, i, 0);
+            igraph_real_t y = MATRIX(*layout, i, 1);
+            igraph_integer_t other = IGRAPH_OTHER(umap_graph, eid, i);
+            igraph_real_t other_x = MATRIX(*layout, other, 0) ;
+            igraph_real_t other_y = MATRIX(*layout, other, 1) ;
+            igraph_real_t x_diff = (x - other_x);
+            igraph_real_t y_diff = (y - other_y);
+            igraph_real_t distance = (x_diff * x_diff + y_diff * y_diff);
+            igraph_real_t weight_diff = -VECTOR(*umap_weights)[j] - 1 / distance;
+            igraph_real_t gradient_x = weight_diff * x_diff / distance;
+            igraph_real_t gradient_y = weight_diff * y_diff / distance;
+            MATRIX(*gradient, i, 0) += gradient_x;
+            MATRIX(*gradient, i, 1) += gradient_y;
+        }
+    }
+    return IGRAPH_SUCCESS;
+}
+
 static igraph_error_t igraph_umap_layout(igraph_t *umap_graph, igraph_vector_t *umap_weights,
         igraph_matrix_t *layout) {
-	igraph_integer_t epochs = 100;
-	igraph_real_t learning_rate = 0.01;
-	igraph_matrix_t gradient;
-	igraph_matrix_init(&gradient, igraph_matrix_nrow(layout), igraph_matrix_ncol(layout));
+    igraph_integer_t epochs = 100;
+    igraph_real_t learning_rate = 0.1;
+    igraph_matrix_t gradient;
+    igraph_matrix_init(&gradient, igraph_matrix_nrow(layout), igraph_matrix_ncol(layout));
 
-	igraph_layout_random(umap_graph, layout);
-	for (igraph_integer_t e = 0; e < epochs; e++) {
-		igraph_get_gradient(&gradient, &weights_2d, umap_graph, umap_weights);
-		igraph_matrix_scale(&gradient, learning_rate);
-		igraph_matrix_sub(layout, &gradient);
-	}
+    igraph_layout_random(umap_graph, layout);
+    for (igraph_integer_t e = 0; e < epochs; e++) {
+        igraph_get_gradient(&gradient, layout, umap_graph, umap_weights);
+        igraph_matrix_scale(&gradient, learning_rate);
+        igraph_matrix_sub(layout, &gradient);
+    }
 
     IGRAPH_FINALLY_CLEAN(1);
 
