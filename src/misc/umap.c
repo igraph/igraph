f86e21e9145ac5c7b80d0ed7d9aec5a08b6e1381
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-04 10:48:14 +0100

    start from knn in umap, instead of data

--- src/misc/umap.c
+++ src/misc/umap.c
@@ -23,79 +23,6 @@
 #include "igraph_constructors.h"
 #include "igraph_layout.h"
 
-static igraph_error_t igraph_get_k_smallest(igraph_vector_t *values,
-        igraph_vector_int_t *smallest, igraph_integer_t k, igraph_integer_t starting_index)
-{
-    igraph_vector_t smallest_values;
-    igraph_real_t value;
-    igraph_integer_t max_index;
-
-    IGRAPH_VECTOR_INIT_FINALLY(&smallest_values, 0);
-    igraph_vector_int_resize(smallest, 0);
-    for (igraph_integer_t i = starting_index; i < igraph_vector_size(values); i++) {
-        value = VECTOR(*values)[i];
-        if (igraph_vector_int_size(smallest) < k) {
-            igraph_vector_int_push_back(smallest, i);
-            igraph_vector_push_back(&smallest_values, i);
-        } else {
-            max_index = 0;
-            for (igraph_integer_t j = 1; j < igraph_vector_int_size(smallest); j++) {
-                if (VECTOR(smallest_values)[j] > VECTOR(smallest_values)[max_index]) {
-                    max_index = j;
-                }
-            }
-            if (value > VECTOR(smallest_values)[max_index]) {
-                VECTOR(smallest_values)[max_index] = value;
-                VECTOR(*smallest)[max_index] = i;
-            }
-        }
-    }
-    igraph_vector_destroy(&smallest_values);
-    IGRAPH_FINALLY_CLEAN(1);
-    return (IGRAPH_SUCCESS);
-}
-
-/*This creates a k-nearest neighbors graph with their distances.*/
-/*first trying everything with simple slow algorithms */
-static igraph_error_t igraph_knng_from_euclid(igraph_matrix_t *data,
-        igraph_t *knn_graph, igraph_vector_t *knn_distances) {
-    igraph_integer_t k = 10;
-    igraph_integer_t no_of_nodes = igraph_matrix_nrow(data);
-    igraph_vector_t node_distances;
-    igraph_vector_t data_row_i;
-    igraph_vector_t data_row_j;
-    igraph_vector_int_t closest;
-    igraph_vector_int_t edges;
-
-    IGRAPH_VECTOR_INIT_FINALLY(&node_distances, no_of_nodes);
-    IGRAPH_VECTOR_INIT_FINALLY(&data_row_i, 0);
-    IGRAPH_VECTOR_INIT_FINALLY(&data_row_j, 0);
-    IGRAPH_VECTOR_INT_INIT_FINALLY(&closest, k);
-    IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
-    for (igraph_integer_t i = 0; i < no_of_nodes; i++) {
-        igraph_matrix_get_row(data, &data_row_i, i);
-        for (igraph_integer_t j = i; j < no_of_nodes; j++) {
-            igraph_matrix_get_row(data, &data_row_j, j);
-            igraph_vector_sub(&data_row_j, &data_row_i);
-            VECTOR(node_distances)[j] = igraph_vector_sumsq(&data_row_j);
-        }
-        igraph_get_k_smallest(&node_distances, &closest, k, i); //uses i as a starting point. Very ugly interface
-        for (igraph_integer_t j = 0; j < igraph_vector_int_size(&closest); j++) { //size is usually k, but not for last few nodes
-            IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
-            IGRAPH_CHECK(igraph_vector_int_push_back(&edges, VECTOR(closest)[j]));
-            IGRAPH_CHECK(igraph_vector_push_back(knn_distances, VECTOR(node_distances)[VECTOR(closest)[j]]));
-        }
-    }
-    IGRAPH_CHECK(igraph_create(knn_graph, &edges, no_of_nodes, IGRAPH_UNDIRECTED));
-    igraph_vector_destroy(&node_distances);
-    igraph_vector_destroy(&data_row_i);
-    igraph_vector_destroy(&data_row_j);
-    igraph_vector_int_destroy(&closest);
-    igraph_vector_int_destroy(&edges);
-    IGRAPH_FINALLY_CLEAN(5);
-    return (IGRAPH_SUCCESS);
-}
-
 /*open set size is just the size of the distance to the closest neighbor*/
 /*the decay depends on the rest of the neighbors */
 
@@ -231,14 +158,12 @@ static igraph_error_t igraph_umap_layout(igraph_t *umap_graph, igraph_vector_t *
     return (IGRAPH_SUCCESS);
 }
 
-igraph_error_t igraph_umap(igraph_matrix_t *data, igraph_matrix_t *layout) {
-    igraph_t knn_graph;
-    igraph_t umap_graph;
+igraph_error_t igraph_layout_umap(igraph_t *graph, igraph_vector_t *weights, igraph_matrix_t *layout) {
     igraph_vector_t open_set_sizes;
     igraph_vector_t open_set_decays;
-    igraph_vector_t umap_weights;
-    igraph_integer_t no_of_nodes = igraph_matrix_nrow(data);
+    igraph_integer_t no_of_nodes = igraph_vcount(graph);
     igraph_vector_t knn_distances;
+    igraph_vector_t umap_weights;
 
     IGRAPH_VECTOR_INIT_FINALLY(&open_set_sizes, no_of_nodes);
     IGRAPH_VECTOR_INIT_FINALLY(&open_set_decays, no_of_nodes);
@@ -246,8 +171,7 @@ igraph_error_t igraph_umap(igraph_matrix_t *data, igraph_matrix_t *layout) {
     IGRAPH_VECTOR_INIT_FINALLY(&umap_weights, 0);
     IGRAPH_CHECK(igraph_matrix_resize(layout, no_of_nodes, 2));
 
-    IGRAPH_CHECK(igraph_knng_from_euclid(data, &knn_graph, &knn_distances));
-    IGRAPH_CHECK(igraph_umap_find_open_sets(&knn_graph, &knn_distances, &open_set_sizes,
+    IGRAPH_CHECK(igraph_umap_find_open_sets(graph, weights, &open_set_sizes,
                 &open_set_decays));
     IGRAPH_CHECK(igraph_umap_edge_weights(data, &umap_graph, &umap_weights, &open_set_sizes,
                 &open_set_decays));
