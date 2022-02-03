0c193508618ee64c5ff284138ed911535db2a96b
valdaarhun <icegambit91@gmail.com>
2022-02-03 23:05:05 +0530

    Demonstrate change in assortativity after rewiring BA network

--- examples/simple/igraph_assortativity.c
+++ examples/simple/igraph_assortativity.c
@@ -1,74 +1,35 @@
 #include <igraph.h>
 #include <stdio.h>
 
-void print_vector(igraph_vector_t *vec){
-    igraph_integer_t length = igraph_vector_size(vec);
-    for(int i = 0; i < length; i++){
-        printf(" %g", igraph_vector_e(vec, i));
-    }
-    printf("\n");
-}
-
 int main(){
     igraph_t g;
-    igraph_vector_t degree, out_degree, in_degree;
-    igraph_real_t assortativity;
-    igraph_integer_t edge_count;
-
-    /* Set seed for deterministic results */
-    igraph_rng_seed(igraph_rng_default(), 42);
-
-    /* Create graph */
-    igraph_small(&g, 10, IGRAPH_DIRECTED, 
-                 0, 1, 0, 2, 0, 3, 0, 5,
-                 3, 6,
-                 4, 5,
-                 6, 4, 6, 7,
-                 7, 8, 7, 9,
-                 8, 9,
-                 9, 0,
-                 -1);
-    
-    edge_count = igraph_ecount(&g);
-
-    /* Undirected graph */
+    igraph_integer_t nodes = 100000, A = 0, power = 1, m = 1;
+    igraph_vector_t degree;
     igraph_vector_init(&degree, 0);
-    igraph_degree(&g, &degree, igraph_vss_all(), IGRAPH_ALL, /* consider self-loops */ IGRAPH_LOOPS);
-    igraph_vector_sort(&degree);
-    printf("Undirected graph degree sequence: ");
-    print_vector(&degree);
+    
+    igraph_rng_seed(igraph_rng_default(), 42);
+    printf("Barabasi-Albert network with 100000 nodes\n\n");
 
-    /* Assortativities for 5 different undirected graphs with identical degree sequences */
-    for(int i = 0 ; i < 5 ; i++){
-        igraph_rewire(&g, 4 * edge_count, IGRAPH_REWIRING_SIMPLE_LOOPS);
+    for (int i = 0; i < 5; i++) {
+        /* Generate undirected graph with 100000 nodes */
+        igraph_barabasi_game(&g, nodes, power, m, NULL, IGRAPH_UNDIRECTED, A, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE, /* start from */ NULL);
+
+        igraph_integer_t edge_count = igraph_ecount(&g);
+
+        /* Get degree sequence */
+        igraph_degree(&g, &degree, igraph_vss_all(), IGRAPH_ALL, /* consider self-loops */ IGRAPH_LOOPS);
+        
+        igraph_real_t assortativity;
         igraph_assortativity(&g, &degree, NULL, &assortativity, /* ignore edge directions */ IGRAPH_UNDIRECTED);
-        printf("Assortativity of undirected graph = %g\n", assortativity);
+        printf("Assortativity before rewiring = %g\n", assortativity);
+        
+        /* Rewire graph */
+        igraph_rewire(&g, 10 * edge_count, IGRAPH_REWIRING_SIMPLE);
+        
+        igraph_assortativity(&g, &degree, NULL, &assortativity, /* ignore edge directions */ IGRAPH_UNDIRECTED);
+        printf("Assortativity after rewiring = %g\n\n", assortativity);
     }
-
+        
     igraph_vector_destroy(&degree);
-    printf("\n");
-
-    /* Directed graph */
-    igraph_vector_init(&out_degree, 0);
-    igraph_degree(&g, &out_degree, igraph_vss_all(), IGRAPH_OUT, /* consider self-loops */ IGRAPH_LOOPS);
-    igraph_vector_sort(&out_degree);
-    printf("Directed graph degree sequence: ");
-    print_vector(&out_degree);
-
-    igraph_vector_init(&in_degree, 0);
-    igraph_degree(&g, &in_degree, igraph_vss_all(), IGRAPH_IN, /* consider self-loops */ IGRAPH_LOOPS);
-    igraph_vector_sort(&in_degree);
-    printf("Directed graph degree sequence: ");
-    print_vector(&in_degree);
-
-    /* Assortativities for 5 different directed graphs with identical degree sequences */
-    for(int i = 0 ; i < 5 ; i++){
-        igraph_rewire(&g, 4 * edge_count, IGRAPH_REWIRING_SIMPLE_LOOPS);
-        igraph_assortativity(&g, &out_degree, &in_degree, &assortativity, /* consider edge directions */ IGRAPH_DIRECTED);
-        printf("Assortativity of undirected graph = %g\n", assortativity);
-    }
-
-    igraph_vector_destroy(&out_degree);
-    igraph_vector_destroy(&in_degree);
     igraph_destroy(&g);
 }
