b166d85503913c5b7ae45e22fc9986ad5e26f3fa
valdaarhun <icegambit91@gmail.com>
2022-02-06 01:23:16 +0530

    Resolve warnings in igraph_assortativity.c

--- examples/simple/igraph_assortativity.c
+++ examples/simple/igraph_assortativity.c
@@ -4,7 +4,7 @@
 int main(){
     igraph_t g;
     igraph_integer_t nodes = 100000, A = 0, power = 1, m = 1;
-    igraph_vector_t degree;
+    igraph_vector_t degree, weight;
     igraph_vector_init(&degree, 0);
     
     igraph_rng_seed(igraph_rng_default(), 42);
@@ -15,10 +15,12 @@ int main(){
         igraph_barabasi_game(&g, nodes, power, m, NULL, IGRAPH_UNDIRECTED, A, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE, /* start from */ NULL);
 
         igraph_integer_t edge_count = igraph_ecount(&g);
+        igraph_vector_init(&weight, edge_count);
+        igraph_vector_fill(&weight, /* weight of each edge = 1 */ 1);
 
         /* Get degree sequence */
-        igraph_degree(&g, &degree, igraph_vss_all(), IGRAPH_ALL, /* consider self-loops */ IGRAPH_LOOPS);
-        
+        igraph_strength(&g, &degree, igraph_vss_all(), IGRAPH_ALL, /* consider self-loops */ IGRAPH_LOOPS, &weight);
+       
         igraph_real_t assortativity;
         igraph_assortativity(&g, &degree, NULL, &assortativity, /* ignore edge directions */ IGRAPH_UNDIRECTED);
         printf("Assortativity before rewiring = %g\n", assortativity);
@@ -29,7 +31,8 @@ int main(){
         igraph_assortativity(&g, &degree, NULL, &assortativity, /* ignore edge directions */ IGRAPH_UNDIRECTED);
         printf("Assortativity after rewiring = %g\n\n", assortativity);
     }
-        
+
+    igraph_vector_destroy(&weight);
     igraph_vector_destroy(&degree);
     igraph_destroy(&g);
 }
