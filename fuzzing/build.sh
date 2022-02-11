db6e641fa3bd7e49a94ed592a8831033d0061ed1
Szabolcs Horvát <szhorvat@gmail.com>
2022-02-11 11:19:37 +0100

    fuzzer: add GraphML fuzzer

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -34,6 +34,12 @@ zip $OUT/read_ncol_fuzzer_seed_corpus.zip \
         $SRC/igraph/tests/unit/*.ncol \
         $SRC/igraph/fuzzing/test_inputs/*.ncol
 
+zip $OUT/read_graphml_fuzzer_seed_corpus.zip \
+        $SRC/igraph/examples/simple/*.graphml \
+        $SRC/igraph/tests/unit/*.graphml \
+        $SRC/igraph/tests/regression/*.graphml \
+        $SRC/igraph/fuzzing/test_inputs/*.graphml
+
 cd $SRC/igraph
 
 for TARGET in read_gml_fuzzer read_pajek_fuzzer read_dl_fuzzer read_lgl_fuzzer read_ncol_fuzzer bliss_fuzzer vertex_connectivity_fuzzer edge_connectivity_fuzzer vertex_separators_fuzzer
