9c779e099cfcee9201012e7acf9c577015e6d4ac
Vincent Traag <vincent@traag.net>
2022-02-17 12:38:24 +0100

    fix: corrected vector print function in example

--- examples/simple/igraph_community_label_propagation.c
+++ examples/simple/igraph_community_label_propagation.c
@@ -51,7 +51,7 @@ int main() {
     igraph_vector_int_print(&membership);
 
     printf("Communities membership: ");
-    igraph_vector_print(&membership);
+    igraph_vector_int_print(&membership);
 
     /* Destroy data structures at the end. */
     igraph_vector_int_destroy(&membership);
