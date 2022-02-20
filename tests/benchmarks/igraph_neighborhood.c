e6f1530b243c35a6515276574abbd73350dd8714
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-02-20 12:45:18 +0100

    add non-lazy adjlist to benchmarks

--- tests/benchmarks/igraph_neighborhood.c
+++ tests/benchmarks/igraph_neighborhood.c
@@ -93,7 +93,7 @@ igraph_vector_int_t *igraph_i_lazy_adjlist2_get_real(igraph_lazy_adjlist2_t *al,
     return &al->adjs[no];
 }
 
-igraph_error_t igraph_neighborhood_adj2(const igraph_t *graph, igraph_vector_int_list_t *res,
+igraph_error_t igraph_neighborhood_adjl2(const igraph_t *graph, igraph_vector_int_list_t *res,
                         igraph_vs_t vids, igraph_integer_t order,
                         igraph_neimode_t mode, igraph_integer_t mindist) {
 
@@ -194,7 +194,7 @@ igraph_error_t igraph_neighborhood_adj2(const igraph_t *graph, igraph_vector_int
     return IGRAPH_SUCCESS;
 }
 
-igraph_error_t igraph_neighborhood_adj(const igraph_t *graph, igraph_vector_int_list_t *res,
+igraph_error_t igraph_neighborhood_adjl(const igraph_t *graph, igraph_vector_int_list_t *res,
                         igraph_vs_t vids, igraph_integer_t order,
                         igraph_neimode_t mode, igraph_integer_t mindist) {
 
@@ -295,6 +295,107 @@ igraph_error_t igraph_neighborhood_adj(const igraph_t *graph, igraph_vector_int_
     return IGRAPH_SUCCESS;
 }
 
+igraph_error_t igraph_neighborhood_adj(const igraph_t *graph, igraph_vector_int_list_t *res,
+                        igraph_vs_t vids, igraph_integer_t order,
+                        igraph_neimode_t mode, igraph_integer_t mindist) {
+
+    igraph_integer_t no_of_nodes = igraph_vcount(graph);
+    igraph_dqueue_int_t q;
+    igraph_vit_t vit;
+    igraph_integer_t i, j;
+    igraph_integer_t *added;
+    igraph_vector_int_t *neis;
+    igraph_vector_int_t tmp;
+    igraph_adjlist_t adjlist;
+
+    if (order < 0) {
+        IGRAPH_ERROR("Negative order in neighborhood size", IGRAPH_EINVAL);
+    }
+
+    if (mindist < 0 || mindist > order) {
+        IGRAPH_ERROR("Minimum distance should be between zero and order",
+                     IGRAPH_EINVAL);
+    }
+
+    added = IGRAPH_CALLOC(no_of_nodes, igraph_integer_t);
+    if (added == 0) {
+        IGRAPH_ERROR("Cannot calculate neighborhood size", IGRAPH_ENOMEM);
+    }
+    IGRAPH_FINALLY(igraph_free, added);
+    IGRAPH_DQUEUE_INT_INIT_FINALLY(&q, 100);
+    IGRAPH_CHECK(igraph_vit_create(graph, vids, &vit));
+    IGRAPH_FINALLY(igraph_vit_destroy, &vit);
+    IGRAPH_VECTOR_INT_INIT_FINALLY(&tmp, 0);
+    IGRAPH_CHECK(igraph_vector_int_list_reserve(res, IGRAPH_VIT_SIZE(vit)));
+    igraph_vector_int_list_clear(res);
+
+    if (!igraph_is_directed(graph) || mode == IGRAPH_ALL) {
+        IGRAPH_CHECK(igraph_adjlist_init(graph, &adjlist, mode, IGRAPH_LOOPS_TWICE, IGRAPH_MULTIPLE));
+    } else {
+        IGRAPH_CHECK(igraph_adjlist_init(graph, &adjlist, mode, IGRAPH_LOOPS_ONCE, IGRAPH_MULTIPLE));
+    }
+    IGRAPH_FINALLY(igraph_adjlist_destroy, &adjlist);
+    for (i = 0; !IGRAPH_VIT_END(vit); IGRAPH_VIT_NEXT(vit), i++) {
+        igraph_integer_t node = IGRAPH_VIT_GET(vit);
+        added[node] = i + 1;
+        igraph_vector_int_clear(&tmp);
+        if (mindist == 0) {
+            IGRAPH_CHECK(igraph_vector_int_push_back(&tmp, node));
+        }
+        if (order > 0) {
+            IGRAPH_CHECK(igraph_dqueue_int_push(&q, node));
+            IGRAPH_CHECK(igraph_dqueue_int_push(&q, 0));
+        }
+
+        while (!igraph_dqueue_int_empty(&q)) {
+            igraph_integer_t actnode = igraph_dqueue_int_pop(&q);
+            igraph_integer_t actdist = igraph_dqueue_int_pop(&q);
+            igraph_integer_t n;
+            neis = igraph_adjlist_get(&adjlist, actnode);
+            //IGRAPH_CHECK(igraph_neighbors(graph, &neis, actnode, mode));
+            n = igraph_vector_int_size(neis);
+
+            if (actdist < order - 1) {
+                /* we add them to the q */
+                for (j = 0; j < n; j++) {
+                    igraph_integer_t nei = VECTOR(*neis)[j];
+                    if (added[nei] != i + 1) {
+                        added[nei] = i + 1;
+                        IGRAPH_CHECK(igraph_dqueue_int_push(&q, nei));
+                        IGRAPH_CHECK(igraph_dqueue_int_push(&q, actdist + 1));
+                        if (actdist + 1 >= mindist) {
+                            IGRAPH_CHECK(igraph_vector_int_push_back(&tmp, nei));
+                        }
+                    }
+                }
+            } else {
+                /* we just count them but don't add them to q */
+                for (j = 0; j < n; j++) {
+                    igraph_integer_t nei = VECTOR(*neis)[j];
+                    if (added[nei] != i + 1) {
+                        added[nei] = i + 1;
+                        if (actdist + 1 >= mindist) {
+                            IGRAPH_CHECK(igraph_vector_int_push_back(&tmp, nei));
+                        }
+                    }
+                }
+            }
+
+        } /* while q not empty */
+
+        IGRAPH_CHECK(igraph_vector_int_list_push_back_copy(res, &tmp));
+    }
+
+    igraph_adjlist_destroy(&adjlist);
+    igraph_vector_int_destroy(&tmp);
+    igraph_vit_destroy(&vit);
+    igraph_dqueue_int_destroy(&q);
+    IGRAPH_FREE(added);
+    IGRAPH_FINALLY_CLEAN(5);
+
+    return IGRAPH_SUCCESS;
+}
+
 void    do_benchmark(igraph_t *g, igraph_vs_t vids, igraph_integer_t repeat)
 {
     igraph_vector_int_list_t result_orig;
@@ -311,12 +412,16 @@ void    do_benchmark(igraph_t *g, igraph_vs_t vids, igraph_integer_t repeat)
                 REPEAT(igraph_neighborhood(g, &result_orig, vids, order,
                         /*mode*/ IGRAPH_ALL, /*mindist*/ 0), repeat));
 
-        BENCH("Using adjlist:",
-                REPEAT(igraph_neighborhood_adj(g, &result_adj, vids, order,
+        BENCH("Using a lazy adjlist:",
+                REPEAT(igraph_neighborhood_adjl(g, &result_adj, vids, order,
                         /*mode*/ IGRAPH_ALL, /*mindist*/ 0), repeat));
 
-        BENCH("Using adjlist 2:",
-                REPEAT(igraph_neighborhood_adj2(g, &result_adj2, vids, order,
+        BENCH("Using lazy adjlist 2:",
+                REPEAT(igraph_neighborhood_adjl2(g, &result_adj2, vids, order,
+                        /*mode*/ IGRAPH_ALL, /*mindist*/ 0), repeat));
+
+        BENCH("Using adjlist:",
+                REPEAT(igraph_neighborhood_adj(g, &result_adj2, vids, order,
                         /*mode*/ IGRAPH_ALL, /*mindist*/ 0), repeat));
         for (igraph_integer_t i = 0; i <= order; i++) {
             IGRAPH_ASSERT(!igraph_vector_int_lex_cmp(&VECTOR(result_orig)[i], &VECTOR(result_adj)[i]));
