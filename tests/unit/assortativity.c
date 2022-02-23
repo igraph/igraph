6377bf95f9ef7589d01a44e45abb16c249a3d8d0
Szabolcs Horvát <szhorvat@gmail.com>
2022-02-23 23:38:53 +0100

    tests: assortativity test cases for null and singleton graphs

--- tests/unit/assortativity.c
+++ tests/unit/assortativity.c
@@ -58,9 +58,71 @@ int main() {
 
     /* Assortativity based on vertex categories */
 
+    igraph_vector_int_init(&types, 0);
+
+    igraph_vector_init(&values, 0);
+
+    igraph_empty(&g, 0, IGRAPH_UNDIRECTED);
+    igraph_assortativity_nominal(&g, &types, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
+    printf("Null graph nominal assortativity: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity(&g, &values, NULL, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
+    printf("Null graph value assortativity: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_destroy(&g);
+
+    igraph_empty(&g, 1, IGRAPH_UNDIRECTED);
+
+    igraph_vector_int_resize(&types, 1);
+    VECTOR(types)[0] = 0;
+
+    igraph_vector_resize(&values, 1);
+    VECTOR(values)[0] = 0;
+
+    igraph_assortativity_nominal(&g, &types, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
+    printf("Singleton graph nominal assortativity: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity_nominal(&g, &types, &assort, IGRAPH_UNDIRECTED, /* normalized */ 0);
+    printf("Singleton graph nominal assortativity, unnormalized: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity(&g, &values, NULL, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
+    printf("Singleton graph value assortativity: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity(&g, &values, NULL, &assort, IGRAPH_UNDIRECTED, /* normalized */ 0);
+    printf("Singleton graph value assortativity, unnormalized: ");
+    igraph_real_printf(assort);
+    printf("\n");
+
+    igraph_add_edge(&g, 0, 0);
+    igraph_assortativity_nominal(&g, &types, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
+    printf("Singleton graph with loop, nominal assortativity: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity_nominal(&g, &types, &assort, IGRAPH_UNDIRECTED, /* normalized */ 0);
+    printf("Singleton graph with loop, nominal assortativity, unnormalized: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity(&g, &values, NULL, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
+    printf("Singleton graph with loop, value assortativity: ");
+    igraph_real_printf(assort);
+    printf("\n");
+    igraph_assortativity(&g, &values, NULL, &assort, IGRAPH_UNDIRECTED, /* normalized */ 0);
+    printf("Singleton graph with loop, value assortativity, unnormalized: ");
+    igraph_real_printf(assort);
+    printf("\n");
+
+    igraph_destroy(&g);
+    igraph_vector_destroy(&values);
+
+    printf("\n");
+
     igraph_famous(&g, "zachary");
 
-    igraph_vector_int_init(&types, 0);
     igraph_degree(&g, &types, igraph_vss_all(), IGRAPH_ALL, IGRAPH_LOOPS);
 
     igraph_assortativity_nominal(&g, &types, &assort, IGRAPH_UNDIRECTED, /* normalized */ 1);
