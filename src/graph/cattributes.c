94ee1d6430c800cf8c0b0422e324d9037d68792a
Szabolcs Horvát <szhorvat@gmail.com>
2022-03-13 14:34:15 +0100

    docs: improved readability for C attribute handler

--- src/graph/cattributes.c
+++ src/graph/cattributes.c
@@ -2664,7 +2664,7 @@ const igraph_attribute_table_t igraph_cattribute_table = {
  * <para>It is not currently possible to have attribute handlers on a
  * per-graph basis. All graphs in an application must be managed with
  * the same attribute handler. (Including the default case when there
- * is no attribute handler at all.</para>
+ * is no attribute handler at all.)</para>
  *
  * <para>The C attribute handler supports attaching real numbers and
  * character strings as attributes. No vectors are allowed, i.e. every
