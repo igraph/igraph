aaba598610b2329dae1b37316832584d0b80b3de
Szabolcs Horvát <szhorvat@gmail.com>
2022-03-19 21:44:45 +0100

    refactor: update cycle basiss functions to use vector_int_list

--- include/igraph_cycles.h
+++ include/igraph_cycles.h
@@ -5,19 +5,19 @@
 #include "igraph_decls.h"
 #include "igraph_interface.h"
 #include "igraph_types.h"
-#include "igraph_vector_ptr.h"
+#include "igraph_vector_list.h"
 
 __BEGIN_DECLS
 
 IGRAPH_EXPORT igraph_error_t igraph_fundamental_cycles(const igraph_t *graph,
                                             igraph_integer_t start_vid,
                                             igraph_integer_t cutoff,
-                                            igraph_vector_ptr_t *result);
+                                            igraph_vector_int_list_t *result);
 
 IGRAPH_EXPORT igraph_error_t igraph_minimum_cycle_basis(const igraph_t *graph,
                                              igraph_integer_t cutoff,
                                              igraph_bool_t complete,
-                                             igraph_vector_ptr_t *result);
+                                             igraph_vector_int_list_t *result);
 
 __END_DECLS
 
--- tests/unit/cycle_bases.c
+++ tests/unit/cycle_bases.c
@@ -1,15 +1,15 @@
 
 #include <igraph.h>
 
-#include "test_utilities.inc"
+#include "test_utilities.h"
 
-void print_check_destroy(igraph_t *graph, igraph_vector_ptr_t *result) {
-    igraph_integer_t i, rank = igraph_vector_ptr_size(result);
+void print_check_destroy(igraph_t *graph, igraph_vector_int_list_t *result) {
+    igraph_integer_t i, rank = igraph_vector_int_list_size(result);
     igraph_integer_t ecount = igraph_ecount(graph), vcount = igraph_vcount(graph);
     igraph_integer_t ccount;
 
     for (i=0; i < rank; ++i) {
-        print_vector_int((igraph_vector_int_t *) VECTOR(*result)[i] );
+        print_vector_int(igraph_vector_int_list_get_ptr(result, i));
     }
     igraph_connected_components(graph, NULL, NULL, &ccount, IGRAPH_WEAK);
     IGRAPH_ASSERT(rank == ecount - vcount + ccount);
@@ -18,12 +18,10 @@ void print_check_destroy(igraph_t *graph, igraph_vector_ptr_t *result) {
 
 int main() {
     igraph_t graph;
-    igraph_vector_int_t dimvec;
-    igraph_vector_ptr_t result;
+    igraph_vector_int_list_t result;
     igraph_integer_t rank;
 
-    igraph_vector_ptr_init(&result, 0);
-    IGRAPH_VECTOR_PTR_SET_ITEM_DESTRUCTOR(&result, igraph_vector_int_destroy);
+    igraph_vector_int_list_init(&result, 0);
 
     printf("Null graph\n");
     igraph_empty(&graph, 0, IGRAPH_UNDIRECTED);
@@ -67,25 +65,36 @@ int main() {
 
     printf("\nPeriodic (5,6)-grid\n");
 
-    igraph_vector_int_init_int_end(&dimvec, -1,
-                                   1, 5, 6, -1);
-    igraph_lattice(&graph, &dimvec, 1, IGRAPH_ADJ_UNDIRECTED, 0, 1);
+
+    {
+        igraph_vector_int_t dimvec;
+        igraph_vector_bool_t periodic;
+
+        igraph_vector_int_init_int_end(&dimvec, -1,
+                                       1, 5, 6, -1);
+
+        igraph_vector_bool_init(&periodic, igraph_vector_int_size(&dimvec));
+        igraph_vector_bool_fill(&periodic, 1);
+
+        igraph_square_lattice(&graph, &dimvec, 1, IGRAPH_ADJ_UNDIRECTED, 0, &periodic);
+
+        igraph_vector_bool_destroy(&periodic);
+        igraph_vector_int_destroy(&dimvec);
+    }
 
     igraph_minimum_cycle_basis(&graph, /* cutoff */ -1, /* complete */ 1, &result);
 
-    rank = igraph_vector_ptr_size(&result);
+    rank = igraph_vector_int_list_size(&result);
 
     /* In a periodic grid graph, all elements in the minimum basis have size 4,
      * except two, which have size equal to the grid dimensions. */
 
-    IGRAPH_ASSERT(igraph_vector_int_size(VECTOR(result)[0]) == 4);
-    IGRAPH_ASSERT(igraph_vector_int_size(VECTOR(result)[rank-1]) == 6);
-    IGRAPH_ASSERT(igraph_vector_int_size(VECTOR(result)[rank-2]) == 5);
-    IGRAPH_ASSERT(igraph_vector_int_size(VECTOR(result)[rank-3]) == 4);
+    IGRAPH_ASSERT(igraph_vector_int_size(&VECTOR(result)[0]) == 4);
+    IGRAPH_ASSERT(igraph_vector_int_size(&VECTOR(result)[rank-1]) == 6);
+    IGRAPH_ASSERT(igraph_vector_int_size(&VECTOR(result)[rank-2]) == 5);
+    IGRAPH_ASSERT(igraph_vector_int_size(&VECTOR(result)[rank-3]) == 4);
 
     print_check_destroy(&graph, &result);
-
-    igraph_vector_int_destroy(&dimvec);
 
 #if 0
     /* This is for benchmarking */
@@ -95,7 +104,7 @@ int main() {
     print_check_destroy(&graph, &result);
 #endif
 
-    igraph_vector_ptr_destroy_all(&result);
+    igraph_vector_int_list_destroy(&result);
 
     VERIFY_FINALLY_STACK();
 
