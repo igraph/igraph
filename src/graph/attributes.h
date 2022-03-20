7ff0ddcdb1c80b71247b7e6c0c0226475e0e2dbf
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-20 17:43:20 +0100

    fix: private export igraph_i_attribute_permute_vertices

--- src/graph/attributes.h
+++ src/graph/attributes.h
@@ -46,7 +46,7 @@ void igraph_i_attribute_destroy(igraph_t *graph);
 igraph_error_t igraph_i_attribute_copy(igraph_t *to, const igraph_t *from,
                             igraph_bool_t ga, igraph_bool_t va, igraph_bool_t ea);
 igraph_error_t igraph_i_attribute_add_vertices(igraph_t *graph, igraph_integer_t nv, void *attr);
-igraph_error_t igraph_i_attribute_permute_vertices(const igraph_t *graph,
+IGRAPH_PRIVATE_EXPORT igraph_error_t igraph_i_attribute_permute_vertices(const igraph_t *graph,
                                         igraph_t *newgraph,
                                         const igraph_vector_int_t *idx);
 igraph_error_t igraph_i_attribute_combine_vertices(const igraph_t *graph,
