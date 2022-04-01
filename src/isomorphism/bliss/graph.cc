a7f6ad54880c537155d347546b1dcb057db82a83
Szabolcs Horvát <szhorvat@gmail.com>
2022-04-01 12:31:05 +0200

    fix: UBSan warning in Bliss about loading invalid value into bool

--- src/isomorphism/bliss/graph.cc
+++ src/isomorphism/bliss/graph.cc
@@ -794,6 +794,7 @@ AbstractGraph::search(const bool canonical,
     root.fp_extendable = TreeNode::MAYBE;
     root.in_best_path = false;
     root.cmp_to_best_path = 0;
+    root.needs_long_prune = false; /* patch in igraph only, to silence UBSan */
     root.long_prune_begin = 0;
 
     root.failure_recording_ival = 0;
