6b2fa438a652a5a3a31f8a17a82243549883c641
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-04-17 12:54:34 +0200

    tests: add more st_vertex_connectivity tests

--- tests/unit/igraph_st_vertex_connectivity.c
+++ tests/unit/igraph_st_vertex_connectivity.c
@@ -45,6 +45,19 @@ int main() {
     igraph_small(&g, 2, IGRAPH_UNDIRECTED, 0,1, -1);
     print_and_destroy(&g, 0, 1, IGRAPH_VCONN_NEI_IGNORE);
 
+    printf("line graph with 6 vertices: ");
+    igraph_small(&g, 6, IGRAPH_UNDIRECTED, 0,1, 1,2, 2,3, 3,4, 4,5, -1);
+    print_and_destroy(&g, 0, 5, IGRAPH_VCONN_NEI_ERROR);
+    VERIFY_FINALLY_STACK();
+
+    printf("full graph with 6 vertices: IGRAPH_VCONN_NEI_IGNORE: ");
+    igraph_full(&g, 6, IGRAPH_UNDIRECTED, 0);
+    print_and_destroy(&g, 0, 1, IGRAPH_VCONN_NEI_IGNORE);
+
+    printf("full graph with 6 vertices: IGRAPH_VCONN_NEI_IGNORE, directed: ");
+    igraph_full(&g, 6, IGRAPH_DIRECTED, 0);
+    print_and_destroy(&g, 0, 1, IGRAPH_VCONN_NEI_IGNORE);
+
     VERIFY_FINALLY_STACK();
 
     printf("check error on graph with two connected vertices.\n");
