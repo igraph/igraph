302c522da5fd7f8db7cbe8b7b9243f6a463c5bcb
valdaarhun <icegambit91@gmail.com>
2022-04-27 23:10:14 +0530

    Change msg in igraph_assortativity_nominal

--- examples/simple/igraph_assortativity_nominal.c
+++ examples/simple/igraph_assortativity_nominal.c
@@ -8,7 +8,7 @@ int main() {
     igraph_matrix_init(&pref_matrix, types, types);
 
     igraph_rng_seed(igraph_rng_default(), 42);
-    printf("Randomly generated graph with 1000 nodes and 50 vertex types\n\n");
+    printf("Randomly generated graph with 250 nodes and 5 vertex types\n\n");
 
     /* Generate preference matrix giving connection probabilities for different vertex types */
     for (igraph_integer_t i = 0; i < types; i++) {
