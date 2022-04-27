d22820145bb235a31729790f54284d5e78157f47
Szabolcs Horvát <szhorvat@gmail.com>
2022-04-27 17:16:11 +0200

    refactor: backport separated exception handling macros from develop

--- src/core/exceptions.h
+++ src/core/exceptions.h
@@ -8,12 +8,23 @@
  * This includes all public functions. This macro is meant to handle exceptions thrown
  * by C++ libraries used by igraph (such as bliss). Wrap the entire body
  * of public functions implemented in C++ in IGRAPH_HANDLE_EXCEPTIONS().
+ *
+ * In some cases IGRAPH_HANDLE_EXCEPTIONS() won't work because the
+ * C preprocessor gets confused by the code block. In that case one can use
+ * IGRAPH_HANDLE_EXCEPTIONS_BEGIN; and IGRAPH_HANDLE_EXCEPTIONS_END at the
+ * beginning and end of the code block.
  */
-#define IGRAPH_HANDLE_EXCEPTIONS(code) \
-    try { code; } \
-    catch (const std::bad_alloc &e) { IGRAPH_ERROR(e.what(), IGRAPH_ENOMEM); } \
+#define IGRAPH_HANDLE_EXCEPTIONS_BEGIN \
+    try {
+#define IGRAPH_HANDLE_EXCEPTIONS_END \
+    } \
+    catch (const std::bad_alloc &e) { IGRAPH_ERROR(e.what(), IGRAPH_ENOMEM); /* LCOV_EXCL_LINE */ } \
     catch (const std::exception &e) { IGRAPH_ERROR(e.what(), IGRAPH_FAILURE); } \
     catch (...) { IGRAPH_ERROR("Unknown exception caught.", IGRAPH_FAILURE); }
 
+#define IGRAPH_HANDLE_EXCEPTIONS(code) \
+    IGRAPH_HANDLE_EXCEPTIONS_BEGIN; \
+    code; \
+    IGRAPH_HANDLE_EXCEPTIONS_END;
 
 #endif // IGRAPH_HANDLE_EXCEPTIONS_H
