549940af2e7c8c6a36b9d8094c04992edcf1c255
Szabolcs Horvát <szhorvat@gmail.com>
2022-04-28 11:57:07 +0200

    tests: speed up assortativity_degree example

--- examples/simple/igraph_assortativity_degree.c
+++ examples/simple/igraph_assortativity_degree.c
@@ -3,23 +3,29 @@
 
 int main(){
     igraph_t g;
-    igraph_integer_t nodes = 100000, A = 0, power = 1, m = 1;
+    igraph_integer_t vcount = 1000;
+    igraph_real_t pf = 0.2;
     
+    /* Seed random number generator to ensure reproducibility. */
     igraph_rng_seed(igraph_rng_default(), 42);
-    printf("Barabasi-Albert network with 100000 nodes\n\n");
+
+    printf("Forest fire model network with %" IGRAPH_PRId " vertices and %g forward burning probability.\n\n",
+           vcount, pf);
 
     for (int i = 0; i < 5; i++) {
         igraph_real_t assortativity;
 
-        /* Generate undirected graph with 100000 nodes */
-        igraph_barabasi_game(&g, nodes, power, m, NULL, /* outpref */ 0, A, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE, /* start from */ NULL);
+        /* Generate graph from the forest fire model. */
+        igraph_forest_fire_game(&g, vcount, pf, 1.0, 1, IGRAPH_UNDIRECTED);
 
+        /* Compute assortativity. */
         igraph_assortativity_degree(&g, &assortativity, /* ignore edge directions */ IGRAPH_UNDIRECTED);
         printf("Assortativity before rewiring = %g\n", assortativity);
         
-        /* Rewire graph */
-        igraph_rewire(&g, 10 * igraph_ecount(&g), IGRAPH_REWIRING_SIMPLE);
+        /* Randomize the graph while preserving the degrees. */
+        igraph_rewire(&g, 20 * igraph_ecount(&g), IGRAPH_REWIRING_SIMPLE);
         
+        /* Re-compute assortativity. Did it change? */
         igraph_assortativity_degree(&g, &assortativity, /* ignore edge directions */ IGRAPH_UNDIRECTED);
         printf("Assortativity after rewiring = %g\n\n", assortativity);
 
