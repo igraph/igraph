f5d831aa198eccce497d0c6936ad96a6795fd5e1
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-05-08 09:42:52 +0200

    tests: laplacian, all normalizations and sparsemat

--- tests/unit/igraph_get_laplacian.c
+++ tests/unit/igraph_get_laplacian.c
@@ -21,36 +21,45 @@
 #include "test_utilities.h"
 
 void test_laplacian(igraph_t *g, const igraph_vector_t *w, igraph_bool_t dir, igraph_laplacian_normalization_t normalization) {
-    igraph_matrix_t m;
+    igraph_matrix_t m, m_converted_sparse;
+    igraph_sparsemat_t m_sparse;
 
     igraph_matrix_init(&m, 0, 0);
+    igraph_matrix_init(&m_converted_sparse, 0, 0);
+    igraph_sparsemat_init(&m_sparse, 0, 0, 0);
 
     igraph_get_laplacian(g, &m, IGRAPH_OUT, normalization, w);
+    igraph_get_laplacian_sparse(g, &m_sparse, IGRAPH_OUT, normalization, w);
     igraph_matrix_print(&m);
+    igraph_sparsemat_as_matrix(&m_converted_sparse, &m_sparse);
+    IGRAPH_ASSERT(igraph_matrix_all_e(&m, &m_converted_sparse));
 
     igraph_matrix_destroy(&m);
+    igraph_matrix_destroy(&m_converted_sparse);
+    igraph_sparsemat_destroy(&m_sparse);
 }
 
 int main() {
     igraph_t g_un, g_dir;
     igraph_vector_t weights;
+    char *n[]  = {"none", "symmetric", "left", "right"};
 
     igraph_vector_init_int(&weights, 9, 1, 2, 3, 4, 5, 2, 2, 3, 3);
     igraph_small(&g_un, 6, IGRAPH_UNDIRECTED, 0,1, 1,2, 2,3, 3,4, 4,0, 1,1, 2,2, 1,2, 3,4, -1);
     igraph_small(&g_dir, 6, IGRAPH_DIRECTED, 0,1, 1,2, 2,3, 3,4, 4,0, 1,1, 2,2, 1,2, 3,4, -1);
 
-    for (int i = 0; i < 8; i++) {
-        igraph_bool_t is_normalized = i / 4;
-        igraph_vector_t* v = ((i & 2) / 2 ? &weights : 0);
-        igraph_bool_t dir = (i % 2 ? IGRAPH_DIRECTED : IGRAPH_UNDIRECTED);
+    for (int normalization = 0; normalization < 4; normalization++) {
+        for (int weighted = 0; weighted < 2; weighted++) {
+            for (int directed = 0; directed < 2; directed++) {
+                printf("=== normalization: %s, %sweighted, %sdirected\n",
+                        n[normalization],
+                        (weighted ? "" : "un"),
+                        (directed ? "" : "un")
+                      );
 
-        printf("=== %sormalized, %sweighted, %sdirected\n",
-               (is_normalized ? "N" : "Unn"),
-               (v != 0 ? "" : "un"),
-               (dir == IGRAPH_DIRECTED ? "" : "un")
-              );
-
-        test_laplacian(dir ? &g_dir : &g_un, v, dir, is_normalized ? IGRAPH_LAPLACIAN_SYMMETRIC : IGRAPH_LAPLACIAN_UNNORMALIZED);
+                test_laplacian(directed ? &g_dir : &g_un, weighted ? &weights : NULL, directed, normalization);
+            }
+        }
     }
 
     igraph_vector_destroy(&weights);
