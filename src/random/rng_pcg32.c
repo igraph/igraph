41f214f56f31624ee4144566ed464069fc1c9ef6
Tamas Nepusz <ntamas@gmail.com>
2022-05-30 13:27:15 +0200

    fix: replaced old, minimal PCG implementation with the dual-licensed variant

--- src/random/rng_pcg32.c
+++ src/random/rng_pcg32.c
@@ -21,79 +21,15 @@
 #include "igraph_memory.h"
 #include "igraph_types.h"
 
+#include "random/pcg/pcg_variants.h"
+
 /* The original implementation of the 32-bit PCG random number generator in this
- * file was downloaded from https://www.pcg-random.org/download.html
+ * file was obtained from https://github.com/imneme/pcg-c
  *
- * PCG is licensed under the Apache License. The original license notice follows
- * below.
- *
- * Some modifications were made to the generator to make it fit igraph's RNG
- * interface. Functions related to the global PCG RNG and bounded RNG were
- * removed as we don't need a global RNG.
+ * PCG is dual-licensed under Apache-2.0 and MIT Licenses. MIT is compatible
+ * with igraph's GPLv2 license. License notices for PCG are to be found in the
+ * pcg_variants.h header
  */
-
-/*
- * PCG Random Number Generation for C.
- *
- * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * For additional information about the PCG random number generation scheme,
- * including its license and other licensing options, visit
- *
- *       http://www.pcg-random.org
- */
-
-struct pcg_state_setseq_64 {    // Internals are *Private*.
-    uint64_t state;             // RNG state.  All values are possible.
-    uint64_t inc;               // Controls which RNG sequence (stream) is
-                                // selected. Must *always* be odd.
-};
-typedef struct pcg_state_setseq_64 pcg32_random_t;
-
-// If you *must* statically initialize it, here's one.
-
-#define PCG32_INITIALIZER   { 0x853c49e6748fea9bULL, 0xda3e39cb94b95bdbULL }
-
-static uint32_t pcg32_random_r(pcg32_random_t* rng);
-
-// pcg32_srandom_r(rng, initstate, initseq):
-//     Seed the rng.  Specified in two parts, state initializer and a
-//     sequence selection constant (a.k.a. stream id)
-
-static void pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate, uint64_t initseq)
-{
-    rng->state = 0U;
-    rng->inc = (initseq << 1u) | 1u;
-    pcg32_random_r(rng);
-    rng->state += initstate;
-    pcg32_random_r(rng);
-}
-
-// pcg32_random_r(rng)
-//     Generate a uniformly distributed 32-bit random number
-
-static uint32_t pcg32_random_r(pcg32_random_t* rng)
-{
-    uint64_t oldstate = rng->state;
-    rng->state = oldstate * 6364136223846793005ULL + rng->inc;
-    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
-    uint32_t rot = oldstate >> 59u;
-    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
-}
-
-/* igraph-specific code follows below */
 
 static const pcg32_random_t pcg32_initializer = PCG32_INITIALIZER;
 
