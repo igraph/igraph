a0205ff5f2f0de887de6e2bba5933bbe81b9d774
Szabolcs Horvát <szhorvat@gmail.com>
2022-06-04 17:40:19 +0200

    refactor: use simple inverse transform sampling for exponential variate generation

--- src/random/random.c
+++ src/random/random.c
@@ -1475,96 +1475,19 @@ static double igraph_i_norm_rand(igraph_rng_t *rng) {
 }
 
 /*
- *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2002 the R Development Core Team
+ * The following function is igraph code (not R / Mathlib).
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
- *
- *  SYNOPSIS
- *
- *    #include <Rmath.h>
- *    double exp_rand(void);
- *
- *  DESCRIPTION
- *
- *    Random variates from the standard exponential distribution.
- *
- *  REFERENCE
- *
- *    Ahrens, J.H. and Dieter, U. (1972).
- *    Computer methods for sampling from the exponential and
- *    normal distributions.
- *    Comm. ACM, 15, 873-882.
+ * We use simple inverse transform sampling, with the assumption that the
+ * quality/resolution of uniform variates is high (52 bits in the default
+ * implementation). The quantile function is -log(1 - r) but given that
+ * r is sampled uniformly form the unit interval, -log(r) is equivalent.
+ * r = 0 is disallowed as it would yield infinity.
  */
 
 static double igraph_i_exp_rand(igraph_rng_t *rng) {
-    /* q[k-1] = sum(log(2)^k / k!)  k=1,..,n, */
-    /* The highest n (here 8) is determined by q[n-1] = 1.0 */
-    /* within standard precision */
-    const double q[] = {
-        0.6931471805599453,
-        0.9333736875190459,
-        0.9888777961838675,
-        0.9984959252914960,
-        0.9998292811061389,
-        0.9999833164100727,
-        0.9999985691438767,
-        0.9999998906925558,
-        0.9999999924734159,
-        0.9999999995283275,
-        0.9999999999728814,
-        0.9999999999985598,
-        0.9999999999999289,
-        0.9999999999999968,
-        0.9999999999999999,
-        1.0000000000000000
-    };
-    double a, u, ustar, umin;
-    int i;
-
-    a = 0.;
-    /* precaution if u = 0 is ever returned */
-    u = igraph_rng_get_unif01(rng);
-    while (u <= 0.0 || u >= 1.0) {
-        u = igraph_rng_get_unif01(rng);
-    }
-    for (;;) {
-        u += u;
-        if (u > 1.0) {
-            break;
-        }
-        a += q[0];
-    }
-    u -= 1.;
-
-    if (u <= q[0]) {
-        return a + u;
-    }
-
-    i = 0;
-    ustar = igraph_rng_get_unif01(rng);
-    umin = ustar;
-    do {
-        ustar = igraph_rng_get_unif01(rng);
-        if (ustar < umin) {
-            umin = ustar;
-        }
-        i++;
-    } while (u > q[i]);
-    return a + umin * q[0];
+    igraph_real_t r = igraph_rng_get_unif01(rng);
+    if (r == 0.0) r = 1.0; /* sample from (0, 1] instead of [0, 1) */
+    return -log(r);
 }
 
 /*
