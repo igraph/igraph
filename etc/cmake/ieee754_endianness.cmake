8023f1c48f91767c7195803970820edaba6a26f2
Tamas Nepusz <ntamas@gmail.com>
2022-07-22 01:42:56 +0200

    chore: add warning about IEEE754 endianness check when cross-compiling, closes #2112

--- etc/cmake/ieee754_endianness.cmake
+++ etc/cmake/ieee754_endianness.cmake
@@ -36,6 +36,12 @@ else()
     # If we are cross-compiling, let's just assume that IEEE754 doubles use the
     # same endianness as uint64_t
     set(IEEE754_DOUBLE_ENDIANNESS_MATCHES YES)
+    message(WARNING "\
+igraph is being cross-compiled, therefore we cannot validate whether the \
+endianness of IEEE754 doubles is the same as the endianness of uint64_t. \
+Most likely it is, unless you are compiling for some esoteric platform, \
+in which case you need make sure that this is the case on your own.\
+")
 endif()
 
 cmake_pop_check_state()
