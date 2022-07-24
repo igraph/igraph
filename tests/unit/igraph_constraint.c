5c9602328ea01cc05065b43ffb58fbbae78e259e
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-07-24 10:43:19 +0200

    add multiple edges and loop tests to constraint

--- tests/unit/igraph_constraint.c
+++ tests/unit/igraph_constraint.c
@@ -29,54 +29,62 @@ void call_and_print(igraph_t *graph, igraph_vs_t vids, igraph_vector_t *weights)
 
 
 int main() {
-    igraph_t g_0, g_1, g_lmu, g_lm, g_4_full, g_4_line, g_hole;
-    igraph_vector_t weights_full, weights_line, result;
+    igraph_t g_0, g_1, g_lmu, g_lm, g_4_full, g_4_full_split, g_4_full_loop, g_4_line, g_hole;
+    igraph_vector_t weights_full, weights_full_split, weights_line, result;
 
     igraph_vector_init_real(&weights_line, 6, 1., 0., 0., 1., 0., 1.);
     igraph_vector_init_real(&weights_full, 6, 1., 1., 1., 1., 1., 1.);
+    igraph_vector_init_real(&weights_full_split, 9, .5, .5, 1., 1., 1., 1./3., 1./3., 1./3., 1.);
     igraph_vector_init(&result, 0);
     igraph_small(&g_0, 0, 0, -1);
     igraph_small(&g_1, 1, 0, -1);
     igraph_small(&g_4_full, 4, IGRAPH_UNDIRECTED, 0,1, 0,2, 0,3, 1,2, 1,3, 2,3, -1);
+    igraph_small(&g_4_full_loop, 4, IGRAPH_UNDIRECTED, 0,0, 0,1, 0,2, 0,3, 1,2, 1,3, 2,3, -1);
+    igraph_small(&g_4_full_split, 4, IGRAPH_UNDIRECTED, 0,1, 0,1, 0,2, 0,3, 1,2, 1,3, 1,3, 1,3, 2,3, -1);
     igraph_small(&g_4_line, 4, IGRAPH_UNDIRECTED, 0,1, 1,2, 2,3, -1);
     igraph_small(&g_hole, 9, IGRAPH_UNDIRECTED, 0,1, 0,2, 0,3, 1,2, 1,3, 2,3, 3,4, 4,5, 5,6, 5,7, 5,8, 6,7, 6,8, 7,8, -1);
     igraph_small(&g_lm, 6, 1, 0,1, 0,2, 1,1, 1,3, 2,0, 2,3, 3,4, 3,4, -1);
     igraph_small(&g_lmu, 6, 0, 0,1, 0,2, 1,1, 1,3, 2,0, 2,3, 3,4, 3,4, -1); //undirected
 
-    /*
     printf("No vertices:\n");
     call_and_print(&g_0, igraph_vss_none(), NULL);
 
     printf("One vertex:\n");
     call_and_print(&g_1, igraph_vss_1(0), NULL);
 
-    */
     printf("Line graph, 4 vertices:\n");
     call_and_print(&g_4_line, igraph_vss_all(), NULL);
 
     printf("Full graph, 4 vertices, weights make it a line:\n");
     call_and_print(&g_4_full, igraph_vss_all(), &weights_line);
 
-    /*
     printf("Full graph, 4 vertices, all same weights:\n");
     call_and_print(&g_4_full, igraph_vss_all(), &weights_full);
+
+    printf("Full graph, 4 vertices, all same weights, but split over multiple edges:\n");
+    call_and_print(&g_4_full_split, igraph_vss_all(), &weights_full_split);
 
     printf("Full graph, 4 vertices, no weights:\n");
     call_and_print(&g_4_full, igraph_vss_all(), NULL); 
 
+    printf("Full graph, 4 vertices, no weights, with loop:\n");
+    call_and_print(&g_4_full_loop, igraph_vss_all(), NULL); 
+
     printf("Hole in middle of two clusters:\n");
     call_and_print(&g_hole, igraph_vss_all(), NULL);
 
-    */
     igraph_destroy(&g_0);
     igraph_destroy(&g_1);
     igraph_destroy(&g_lmu);
     igraph_destroy(&g_lm);
     igraph_destroy(&g_4_full);
+    igraph_destroy(&g_4_full_loop);
+    igraph_destroy(&g_4_full_split);
     igraph_destroy(&g_4_line);
     igraph_destroy(&g_hole);
     igraph_vector_destroy(&weights_line);
     igraph_vector_destroy(&weights_full);
+    igraph_vector_destroy(&weights_full_split);
     igraph_vector_destroy(&result);
 
     VERIFY_FINALLY_STACK();
