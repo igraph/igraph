04cd2ed54958d7ccabf4787d7fe38d9f827f10a0
Szabolcs Horvát <szhorvat@gmail.com>
2022-08-23 11:31:53 +0200

    tests: update bridges() tests

--- tests/unit/igraph_bridges.c
+++ tests/unit/igraph_bridges.c
@@ -5,12 +5,8 @@
 #include "test_utilities.inc"
 
 void sort_and_print_vector(igraph_vector_t *v) {
-    long int i, n = igraph_vector_size(v);
     igraph_vector_sort(v);
-    for (i = 0; i < n; i++) {
-        printf(" %li", (long int) VECTOR(*v)[i]);
-    }
-    printf("\n");
+    print_vector_round(v);
 }
 
 int main() {
@@ -18,6 +14,16 @@ int main() {
     igraph_vector_t bridges;
 
     igraph_vector_init(&bridges, 0);
+
+    igraph_empty(&graph, 0, /* directed = */ 0);
+    igraph_bridges(&graph, &bridges);
+    sort_and_print_vector(&bridges);
+    igraph_destroy(&graph);
+
+    igraph_empty(&graph, 1, /* directed = */ 0);
+    igraph_bridges(&graph, &bridges);
+    sort_and_print_vector(&bridges);
+    igraph_destroy(&graph);
 
     igraph_small(&graph, /* num_nodes = */ 7, /* directed = */ 0,
                  0, 1, 1, 2, 0, 2, 0, 3, 3, 4, 4, 5, 3, 5, 4, 6, -1);
