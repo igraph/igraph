1b64606b5543fade6257d8c23e2558881fe00d96
Szabolcs Horvát <szhorvat@gmail.com>
2022-09-03 22:02:45 +0200

    docs: move experimental label to front for layout_merge_dla()

--- src/layout/merge_dla.c
+++ src/layout/merge_dla.c
@@ -34,6 +34,8 @@
  * \function igraph_layout_merge_dla
  * \brief Merges multiple layouts by using a DLA algorithm.
  *
+ * \experimental
+ *
  * First each layout is covered by a circle. Then the layout of the
  * largest graph is placed at the origin. Then the other layouts are
  * placed by the DLA algorithm, larger ones first and smaller ones
@@ -50,8 +52,6 @@
  *
  * </para><para>
  * Time complexity: TODO.
- *
- * \experimental
  */
 
 igraph_error_t igraph_layout_merge_dla(
