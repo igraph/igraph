4112e5084bdb4267c837612cdf8296e87c665c26
Szabolcs Horvát <szhorvat@gmail.com>
2022-09-05 18:29:53 +0200

    refactor: disable file access portions of PRPACK as it triggers security-conscious static analysers

--- src/centrality/prpack/prpack_base_graph.h
+++ src/centrality/prpack/prpack_base_graph.h
@@ -12,9 +12,11 @@ namespace prpack {
         private:
             // helper methods
             void initialize();
+#if 0
             void read_smat(std::FILE* f, const bool weighted);
             void read_edges(std::FILE* f);
             void read_ascii(std::FILE* f);
+#endif
         public:
             // instance variables
             int num_vs;
@@ -29,7 +31,9 @@ namespace prpack {
             prpack_base_graph(const prpack_int64_csc* g);
             prpack_base_graph(const prpack_csr* g);
             prpack_base_graph(const prpack_edge_list* g);
+#if 0
             prpack_base_graph(const char* filename, const char* format, const bool weighted);
+#endif
             prpack_base_graph(int nverts, int nedges, std::pair<int,int>* edges);
             // destructor
             ~prpack_base_graph();
