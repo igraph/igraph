305105e7f87fc19d93b0e55e365b50e27cc3c361
Szabolcs Horvát <szhorvat@gmail.com>
2022-10-03 11:33:58 +0200

    perf: implement lazy version of ECC for k=4

--- src/properties/ecc.c
+++ src/properties/ecc.c
@@ -47,7 +47,8 @@ static igraph_integer_t vector_int_intersection_size_sorted(
     return count;
 }
 
-/* This ECC computation is fast when performing the calculation for all vertices. */
+
+/* Optimized for the case when computing ECC for all edges. */
 static igraph_error_t igraph_i_ecc3_1(
         const igraph_t *graph, igraph_vector_t *res, const igraph_es_t eids,
         igraph_bool_t offset, igraph_bool_t normalize) {
@@ -109,6 +110,8 @@ static igraph_error_t igraph_i_ecc3_1(
     return IGRAPH_SUCCESS;
 }
 
+
+/* Optimized for computing ECC for a small subset of edges. */
 static igraph_error_t igraph_i_ecc3_2(
         const igraph_t *graph, igraph_vector_t *res,
         const igraph_es_t eids, igraph_bool_t offset, igraph_bool_t normalize) {
@@ -186,7 +189,9 @@ static igraph_error_t igraph_i_ecc3_2(
     return IGRAPH_SUCCESS;
 }
 
-static igraph_error_t igraph_i_ecc4(
+
+/* Optimized for the case when computing ECC for all edges. */
+static igraph_error_t igraph_i_ecc4_1(
         const igraph_t *graph, igraph_vector_t *res,
         const igraph_es_t eids, igraph_bool_t offset, igraph_bool_t normalize) {
 
@@ -266,6 +271,114 @@ static igraph_error_t igraph_i_ecc4(
 }
 
 
+/* Optimized for computing ECC for a small subset of edges. */
+static igraph_error_t igraph_i_ecc4_2(
+        const igraph_t *graph, igraph_vector_t *res,
+        const igraph_es_t eids, igraph_bool_t offset, igraph_bool_t normalize) {
+
+    igraph_integer_t no_of_nodes = igraph_vcount(graph);
+    igraph_lazy_adjlist_t al;
+    igraph_vector_bool_t al_retrieved;
+    igraph_eit_t eit;
+    igraph_real_t c = offset ? 1.0 : 0.0;
+
+    IGRAPH_CHECK(igraph_lazy_adjlist_init(graph, &al, IGRAPH_ALL, IGRAPH_NO_LOOPS, IGRAPH_NO_MULTIPLE));
+    IGRAPH_FINALLY(igraph_lazy_adjlist_destroy, &al);
+
+    IGRAPH_CHECK(igraph_eit_create(graph, eids, &eit));
+    IGRAPH_FINALLY(igraph_eit_destroy, &eit);
+
+    IGRAPH_CHECK(igraph_vector_resize(res, IGRAPH_EIT_SIZE(eit)));
+
+    IGRAPH_VECTOR_BOOL_INIT_FINALLY(&al_retrieved, no_of_nodes);
+
+    for (igraph_integer_t i=0;
+         ! IGRAPH_EIT_END(eit);
+         IGRAPH_EIT_NEXT(eit), i++) {
+
+        igraph_integer_t edge = IGRAPH_EIT_GET(eit);
+        igraph_integer_t v1 = IGRAPH_FROM(graph, edge), v2 = IGRAPH_TO(graph, edge);
+
+        igraph_real_t z; /* number of 4-cycles the edge participates in */
+        igraph_real_t s; /* max number of 4-cycles the edge could be part of */
+
+        IGRAPH_ALLOW_INTERRUPTION();
+
+        igraph_integer_t d1, d2;
+        IGRAPH_CHECK(igraph_degree_1(graph, &d1, v1, IGRAPH_ALL, IGRAPH_LOOPS));
+        IGRAPH_CHECK(igraph_degree_1(graph, &d2, v2, IGRAPH_ALL, IGRAPH_LOOPS));
+
+        if (v1 == v2) {
+            z = 0.0;
+            s = 0.0;
+        } else {
+            /* ensure that v1 is the vertex with the smaller degree */
+            if (d1 > d2) {
+                igraph_integer_t tmp = v1;
+                v1 = v2;
+                v2 = tmp;
+
+                tmp = d1;
+                d1 = d2;
+                d2 = tmp;
+            }
+
+            z = 0.0;
+
+            igraph_vector_int_t *a1 = igraph_lazy_adjlist_get(&al, v1);
+            if (! VECTOR(al_retrieved)[v1]) {
+                /* OOM error can only occur when originally retrieving a neighbour list,
+                 * not on subsequent call to lazy_adjlist_get(). */
+                IGRAPH_CHECK_OOM(a1, "Not enough memory for edge clustering coefficient.");
+                igraph_vector_int_sort(a1);
+                VECTOR(al_retrieved)[v1] = true;
+            }
+            const igraph_integer_t n = igraph_vector_int_size(a1);
+
+            for (igraph_integer_t j=0; j < n; j++) {
+                igraph_integer_t v3 = VECTOR(*a1)[j];
+
+                /* It is not possible that v3 == v1 because self-loops have been removed from the adjlist. */
+
+                if (v3 == v2) continue;
+
+                igraph_vector_int_t *a2 = igraph_lazy_adjlist_get(&al, v2);
+                if (! VECTOR(al_retrieved)[v2]) {
+                    /* OOM error can only occur when originally retrieving a neighbour list,
+                     * not on subsequent call to lazy_adjlist_get(). */
+                    IGRAPH_CHECK_OOM(a2, "Not enough memory for edge clustering coefficient.");
+                    igraph_vector_int_sort(a2);
+                    VECTOR(al_retrieved)[v2] = true;
+                }
+
+                igraph_vector_int_t *a3 = igraph_lazy_adjlist_get(&al, v3);
+                if (! VECTOR(al_retrieved)[v3]) {
+                    /* OOM error can only occur when originally retrieving a neighbour list,
+                     * not on subsequent call to lazy_adjlist_get(). */
+                    IGRAPH_CHECK_OOM(a3, "Not enough memory for edge clustering coefficient.");
+                    igraph_vector_int_sort(a3);
+                    VECTOR(al_retrieved)[v3] = true;
+                }
+
+                z += vector_int_intersection_size_sorted(a2, a3) - 1.0;
+            }
+
+            s = (d1 - 1.0) * (d2 - 1.0);
+        }
+
+        VECTOR(*res)[i] = z + c;
+        if (normalize) VECTOR(*res)[i] /= s;
+    }
+
+    igraph_vector_bool_destroy(&al_retrieved);
+    igraph_eit_destroy(&eit);
+    igraph_lazy_adjlist_destroy(&al);
+    IGRAPH_FINALLY_CLEAN(3);
+
+    return IGRAPH_SUCCESS;
+}
+
+
 /**
  * \function igraph_ecc
  * \brief Edge clustering coefficient of some edges.
@@ -338,7 +451,11 @@ igraph_error_t igraph_ecc(const igraph_t *graph, igraph_vector_t *res,
             return igraph_i_ecc3_2(graph, res, eids, offset, normalize);
         }
     case 4:
-        return igraph_i_ecc4(graph, res, eids, offset, normalize);
+        if (igraph_es_is_all(&eids)) {
+            return igraph_i_ecc4_1(graph, res, eids, offset, normalize);
+        } else {
+            return igraph_i_ecc4_2(graph, res, eids, offset, normalize);
+        }
     default:
         IGRAPH_ERROR("Edge clustering coefficient calculation is only implemented for cycle sizes 3 and 4.",
                      IGRAPH_UNIMPLEMENTED);
