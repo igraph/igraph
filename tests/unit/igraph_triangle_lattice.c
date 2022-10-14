9894341f7a01103b6e29da40d08da56dda5aa506
Radoslav Fulek <rado@comitycard.com>
2022-10-14 09:41:04 -0400

    renaming tessellation.c to lattices.c + updating error handling

--- tests/unit/igraph_triangle_lattice.c
+++ tests/unit/igraph_triangle_lattice.c
@@ -24,55 +24,52 @@ int main(void) {
     igraph_vector_int_t dimvector;
 
     /* triangular triangle lattice with a single vertex and no edges*/
-    igraph_vector_int_init(&dimvector, 1);
+    IGRAPH_CHECK(igraph_vector_int_init(&dimvector, 1));
     VECTOR(dimvector)[0] = 1;
 
-    igraph_triangle_lattice(&graph, &dimvector, true, false);
+    IGRAPH_CHECK(igraph_triangle_lattice(&graph, &dimvector, true, false));
     igraph_integer_t number_of_edges = igraph_vector_int_size(&graph.from);
     IGRAPH_ASSERT(number_of_edges == 0);
 
     igraph_destroy(&graph);
 
     /* triangular triangle lattice */
-    igraph_vector_int_init(&dimvector, 1);
+    igraph_vector_int_destroy(&dimvector);
+    IGRAPH_CHECK(igraph_vector_int_init(&dimvector, 1));
     VECTOR(dimvector)[0] = 5;
 
-    igraph_triangle_lattice(&graph, &dimvector, true, false);
+    IGRAPH_CHECK(igraph_triangle_lattice(&graph, &dimvector, true, false));
     number_of_edges = igraph_vector_int_size(&graph.from);
     IGRAPH_ASSERT(number_of_edges == 30);
 
     igraph_destroy(&graph);
 
     /* rectangular triangle lattice */
-    igraph_vector_int_init(&dimvector, 2);
+    igraph_vector_int_destroy(&dimvector);
+    IGRAPH_CHECK(igraph_vector_int_init(&dimvector, 2));
     VECTOR(dimvector)[0] = 4;
     VECTOR(dimvector)[1] = 5;
 
-    igraph_triangle_lattice(&graph, &dimvector, true, true);
+    IGRAPH_CHECK(igraph_triangle_lattice(&graph, &dimvector, true, true));
     number_of_edges = igraph_vector_int_size(&graph.from);
     IGRAPH_ASSERT(number_of_edges == 86);
 
     igraph_destroy(&graph);
 
     /* hexagonal triangle lattice */
-    igraph_vector_int_init(&dimvector, 3);
+    igraph_vector_int_destroy(&dimvector);
+    IGRAPH_CHECK(igraph_vector_int_init(&dimvector, 3));
     VECTOR(dimvector)[0] = 3;
     VECTOR(dimvector)[1] = 4;
     VECTOR(dimvector)[2] = 5;
 
-    igraph_triangle_lattice(&graph, &dimvector, false, true);
+    IGRAPH_CHECK(igraph_triangle_lattice(&graph, &dimvector, false, true));
     number_of_edges = igraph_vector_int_size(&graph.from);
     IGRAPH_ASSERT(number_of_edges == 87);
 
-    igraph_vector_int_destroy(&dimvector);
-
-    /*Erroneous calls*/
-    igraph_set_error_handler(igraph_error_handler_ignore);
-    IGRAPH_ASSERT(igraph_triangle_lattice(&graph, &dimvector, true, true) == IGRAPH_EINVAL);
-
-    igraph_vector_int_init(&dimvector, 1);
+    /*An erroneous call*/
     VECTOR(dimvector)[0] = -3;
-    IGRAPH_ASSERT(igraph_triangle_lattice(&graph, &dimvector, true, true) == IGRAPH_EINVAL);
+    CHECK_ERROR(igraph_triangle_lattice(&graph, &dimvector, true, true), IGRAPH_EINVAL);
 
     igraph_destroy(&graph);
     igraph_vector_int_destroy(&dimvector);
