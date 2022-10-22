8428d4dc270de8e31c4b20f801a80305113e2bc3
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-10-22 17:17:46 +0200

    use isnan for astar

--- src/paths/astar.c
+++ src/paths/astar.c
@@ -138,7 +138,7 @@ igraph_error_t igraph_get_shortest_paths_astar(const igraph_t *graph,
             if (min < 0) {
                 IGRAPH_ERROR("Weight vector must be non-negative", IGRAPH_EINVAL);
             }
-            else if (igraph_is_nan(min)) {
+            else if (isnan(min)) {
                 IGRAPH_ERROR("Weight vector must not contain NaN values", IGRAPH_EINVAL);
             }
         }
