aa10e4b9076bf29eaebeac11f6c9318b045a3e1e
Tamas Nepusz <ntamas@gmail.com>
2022-10-24 21:28:08 +0200

    style: nitpicking

--- src/constructors/lattices.c
+++ src/constructors/lattices.c
@@ -16,8 +16,9 @@
 */
 
 #include "igraph_constructors.h"
-#include "math/safe_intop.h"
+
 #include "core/interruption.h"
+#include "math/safe_intop.h"
 
 /**
  * \ingroup generators
@@ -29,12 +30,12 @@
  * exists. Thus, all vertices have degree at most 6.
  *
  * </para><para>
- *  The vertices of the resulting graph are ordered lexicographically with the 2nd coordinate being
- *  more significant, e.g., (i, j) < (i + 1, j) and (i + 1, j) < (i, j + 1).
+ * The vertices of the resulting graph are ordered lexicographically with the 2nd coordinate being
+ * more significant, e.g., (i, j) < (i + 1, j) and (i + 1, j) < (i, j + 1).
  *
  * \param graph An uninitialized graph object.
  * \param directed Boolean, whether to create a directed graph.
- *        If the \c mutual argument is  not set to true,
+ *        If the \c mutual argument is not set to true,
  *        edges will be directed from lower-index vertices towards
  *        higher-index ones.
  * \param mutual Boolean, if the graph is directed this gives whether
@@ -51,33 +52,35 @@
  *
  * Time complexity:  O(|V|), where |V| is the number of vertices in the generated graph.
  */
-static igraph_error_t triangle_lattice(igraph_t *graph, igraph_bool_t directed, igraph_bool_t mutual,
-                                       const igraph_vector_int_t *row_lengths_vector, const igraph_vector_int_t *row_start_vector)
-{
+static igraph_error_t triangle_lattice(
+    igraph_t *graph, igraph_bool_t directed, igraph_bool_t mutual,
+    const igraph_vector_int_t *row_lengths_vector, const igraph_vector_int_t *row_start_vector
+) {
     igraph_vector_int_t edges = IGRAPH_VECTOR_NULL;
     igraph_integer_t row_count = igraph_vector_int_size(row_lengths_vector);
     igraph_integer_t no_of_nodes;
     igraph_vector_int_t row_lengths_prefix_sum_vector;
     igraph_integer_t i, j;
 
-    if (igraph_vector_int_size(row_lengths_vector) != igraph_vector_int_size(row_start_vector))
-    {
+    if (igraph_vector_int_size(row_lengths_vector) != igraph_vector_int_size(row_start_vector)) {
         IGRAPH_ERRORF(
             "Length of row_lengths_vector vector (%" IGRAPH_PRId ") must match the length of the "
             "row_start_vector (%" IGRAPH_PRId ").",
-            IGRAPH_EINVAL,  igraph_vector_int_size(row_lengths_vector), igraph_vector_int_size(row_start_vector));
+            IGRAPH_EINVAL,
+            igraph_vector_int_size(row_lengths_vector),
+            igraph_vector_int_size(row_start_vector)
+        );
     }
-    for (i = 0; i < row_count; i++)
-    {
-        if (VECTOR(*row_lengths_vector)[i] < 0)
-        {
+
+    for (i = 0; i < row_count; i++) {
+        if (VECTOR(*row_lengths_vector)[i] < 0) {
             IGRAPH_ERRORF(
                 "row_lengths_vector vector must have non-negative coordinates, "
                 "was (%" IGRAPH_PRId ") for the (%" IGRAPH_PRId ")-th row.",
                 IGRAPH_EINVAL, VECTOR(*row_lengths_vector)[i], i);
         }
     }
-    
+
     IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
 
     IGRAPH_VECTOR_INT_INIT_FINALLY(&row_lengths_prefix_sum_vector, row_count + 1);
@@ -105,8 +108,7 @@ static igraph_error_t triangle_lattice(igraph_t *graph, igraph_bool_t directed, 
      /* computing the number of edges in the constructed triangle lattice */
     igraph_integer_t no_of_edges2 = VECTOR(*row_lengths_vector)[row_count - 1] - 1;
     igraph_integer_t multiplier = mutual && directed ? 4 : 2;
-    for (j = 0; j < row_count - 1; j++)
-    {
+    for (j = 0; j < row_count - 1; j++) {
         IGRAPH_SAFE_ADD(no_of_edges2, VECTOR(*row_lengths_vector)[j], &no_of_edges2);
         IGRAPH_SAFE_ADD(no_of_edges2, fmin(ROW_END(j), ROW_END((j + 1))) - fmax(VECTOR(*row_start_vector)[j], VECTOR(*row_start_vector)[j + 1]) + 1,
                         &no_of_edges2);
@@ -118,15 +120,12 @@ static igraph_error_t triangle_lattice(igraph_t *graph, igraph_bool_t directed, 
 
     /* constructing the edge array */
     igraph_integer_t k;
-    for (j = 0; j < row_count; j++)
-    {
+    for (j = 0; j < row_count; j++) {
         IGRAPH_ALLOW_INTERRUPTION();
-        for (i = 0; i < VECTOR(*row_lengths_vector)[j]; i++)
-        {
+        for (i = 0; i < VECTOR(*row_lengths_vector)[j]; i++) {
             k = VECTOR(*row_start_vector)[j] + i;
             ADD_EDGE_IJ_KL_IF_EXISTS(k, j, (k + 1), j);
-            if (j < row_count - 1)
-            {
+            if (j < row_count - 1) {
                 ADD_EDGE_IJ_KL_IF_EXISTS(k, j, k, (j + 1));
                 ADD_EDGE_IJ_KL_IF_EXISTS(k, j, (k - 1), (j + 1));
             }
@@ -142,8 +141,7 @@ static igraph_error_t triangle_lattice(igraph_t *graph, igraph_bool_t directed, 
     return IGRAPH_SUCCESS;
 }
 
-static igraph_error_t triangle_lattice_triangle_shape(igraph_t *graph, igraph_integer_t size, igraph_bool_t directed, igraph_bool_t mutual)
-{
+static igraph_error_t triangle_lattice_triangle_shape(igraph_t *graph, igraph_integer_t size, igraph_bool_t directed, igraph_bool_t mutual) {
     igraph_integer_t row_count = size;
     igraph_vector_int_t row_lengths_vector;
     igraph_vector_int_t row_start_vector;
@@ -152,8 +150,7 @@ static igraph_error_t triangle_lattice_triangle_shape(igraph_t *graph, igraph_in
     IGRAPH_VECTOR_INT_INIT_FINALLY(&row_lengths_vector, row_count);
     IGRAPH_VECTOR_INT_INIT_FINALLY(&row_start_vector, row_count);
 
-    for (i = 0; i < row_count; i++)
-    {
+    for (i = 0; i < row_count; i++) {
         VECTOR(row_lengths_vector)[i] = size - i;
         VECTOR(row_start_vector)[i] = 0;
     }
@@ -167,9 +164,10 @@ static igraph_error_t triangle_lattice_triangle_shape(igraph_t *graph, igraph_in
     return IGRAPH_SUCCESS;
 }
 
-static igraph_error_t triangle_lattice_rectangle_shape(igraph_t *graph, igraph_integer_t size_x,
-                                                       igraph_integer_t size_y, igraph_bool_t directed, igraph_bool_t mutual)
-{
+static igraph_error_t triangle_lattice_rectangle_shape(
+    igraph_t *graph, igraph_integer_t size_x, igraph_integer_t size_y,
+    igraph_bool_t directed, igraph_bool_t mutual
+) {
     igraph_integer_t row_count = size_y;
     igraph_vector_int_t row_lengths_vector;
     igraph_vector_int_t row_start_vector;
@@ -178,8 +176,7 @@ static igraph_error_t triangle_lattice_rectangle_shape(igraph_t *graph, igraph_i
     IGRAPH_VECTOR_INT_INIT_FINALLY(&row_lengths_vector, row_count);
     IGRAPH_VECTOR_INT_INIT_FINALLY(&row_start_vector, row_count);
 
-    for (i = 0; i < row_count; i++)
-    {
+    for (i = 0; i < row_count; i++) {
         VECTOR(row_lengths_vector)[i] = size_x;
         VECTOR(row_start_vector)[i] = (row_count - i) / 2;
     }
@@ -193,9 +190,10 @@ static igraph_error_t triangle_lattice_rectangle_shape(igraph_t *graph, igraph_i
     return IGRAPH_SUCCESS;
 }
 
-static igraph_error_t triangle_lattice_hex_shape(igraph_t *graph, igraph_integer_t size_x,
-                                                 igraph_integer_t size_y, igraph_integer_t size_z, igraph_bool_t directed, igraph_bool_t mutual)
-{
+static igraph_error_t triangle_lattice_hex_shape(
+    igraph_t *graph, igraph_integer_t size_x, igraph_integer_t size_y,
+    igraph_integer_t size_z, igraph_bool_t directed, igraph_bool_t mutual
+) {
     igraph_integer_t row_count = size_y + size_z - 1;
     igraph_vector_int_t row_lengths_vector;
     igraph_vector_int_t row_start_vector;
@@ -210,22 +208,16 @@ static igraph_error_t triangle_lattice_hex_shape(igraph_t *graph, igraph_integer
     igraph_integer_t second_threshold = fmax(size_y - 1, size_z - 1);
     igraph_integer_t sgn_flag = size_y < size_z ? 0 : -1;
 
-    for (i = 0; i < row_count; i++)
-    {
+    for (i = 0; i < row_count; i++) {
         VECTOR(row_lengths_vector)[i] = row_length;
         VECTOR(row_start_vector)[i] = row_start;
 
-        if (i < first_threshold)
-        {
+        if (i < first_threshold) {
             row_length++;
             row_start--;
-        }
-        else if (i < second_threshold)
-        {
+        } else if (i < second_threshold) {
             row_start += sgn_flag;
-        }
-        else
-        {
+        } else {
             row_length--;
         }
     }
@@ -249,16 +241,21 @@ static igraph_error_t triangle_lattice_hex_shape(igraph_t *graph, igraph_integer
  *
  * </para><para>
  * The vertices of the resulting graph are ordered lexicographically with the 2nd coordinate being
- *  more significant, e.g., (i, j) < (i + 1, j) and (i + 1, j) < (i, j + 1)
+ * more significant, e.g., (i, j) < (i + 1, j) and (i + 1, j) < (i, j + 1)
  *
  * \param graph An uninitialized graph object.
  * \param dims Integer vector, defines the shape of the lattice. (Below the "edge length"s are in terms of graph theoretical path lengths.)
- *        If the size of \p dims is 1 the resulting  lattice has a triangular shape with "edge length" dims[0].
- *        If the size of \p dims is 2 the resulting lattice has a "quasi rectangular" shape with "edge length"s dims[0] and dims[1].
- *        If the size of \p dims is 3 the resulting  lattice has a hexagonal shape with "edge length"s dims[0], dims[1] and dims[2].
- *          (see https://github.com/igraph/igraph/issues/1842  for lattice visualizations)
+ *        If \p dims is of length 1, the resulting lattice has a triangular shape
+ *        where each side of the triangle contains \c "dims[0]" vertices.
+ *        If \p dims is of length 2, the resulting lattice has a
+ *        "quasi rectangular" shape with the sides containing \c "dims[0]" and
+ *        \c "dims[1]" vertices, respectively.
+ *        If \p dims is of length 3, the resulting lattice has a hexagonal shape
+ *        where the sides of the hexagon contain \c "dims[0]", \c "dims[1]" and
+ *        \c "dims[2]" vertices. See https://github.com/igraph/igraph/issues/1842 for
+ *        lattice visualizations.
  * \param directed Boolean, whether to create a directed graph.
- *        If the \c mutual argument is  not set to true,
+ *        If the \c mutual argument is not set to true,
  *        edges will be directed from lower-index vertices towards
  *        higher-index ones.
  * \param mutual Boolean, if the graph is directed this gives whether
@@ -270,15 +267,16 @@ static igraph_error_t triangle_lattice_hex_shape(igraph_t *graph, igraph_integer
  * Time complexity:  O(|V|), where |V| is the number of vertices in the generated graph.
  *
  */
-igraph_error_t igraph_triangle_lattice(igraph_t *graph, const igraph_vector_int_t *dims, igraph_bool_t directed, igraph_bool_t mutual)
-{
+igraph_error_t igraph_triangle_lattice(
+    igraph_t *graph, const igraph_vector_int_t *dims, igraph_bool_t directed,
+    igraph_bool_t mutual
+) {
     igraph_integer_t num_dims = igraph_vector_int_size(dims);
     if (igraph_vector_int_any_smaller(dims, 1)) {
         IGRAPH_ERROR("Invalid dimension vector.", IGRAPH_EINVAL);
     }
 
-    switch (num_dims)
-    {
+    switch (num_dims) {
     case 1:
         IGRAPH_CHECK(triangle_lattice_triangle_shape(graph, VECTOR(*dims)[0], directed, mutual));
         break;
@@ -290,8 +288,10 @@ igraph_error_t igraph_triangle_lattice(igraph_t *graph, const igraph_vector_int_
         break;
     default:
         IGRAPH_ERRORF(
-            "The size of dims vector must be either 1, 2, or 3. (%" IGRAPH_PRId ").",
-            IGRAPH_EINVAL, num_dims);
+            "The size of the dimension vector must be 1, 2 or 3, got %" IGRAPH_PRId ".",
+            IGRAPH_EINVAL, num_dims
+        );
     }
+
     return IGRAPH_SUCCESS;
 }
