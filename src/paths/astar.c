c1f0ce28dfa173c9dbb2cb54b202c0dc88f6c450
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-12-24 13:10:56 +0100

    add experimental to A*

--- src/paths/astar.c
+++ src/paths/astar.c
@@ -32,6 +32,8 @@
  * \function igraph_get_shortest_path_astar
  * \brief A* gives the shortest path from one vertex to another, with heuristic.
  *
+ * \experimental
+ *
  * Calculates a single (positively) weighted shortest path from
  * a single vertex to another one, using the A* algorithm.
  *
