16b752ca649bff2f29f9eb8129b3981372772a59
Szabolcs Horvát <szhorvat@gmail.com>
2022-12-29 15:09:42 +0100

    feat: add generic two-way heap, part 2 added header and basic tests

--- src/core/genheap.c
+++ src/core/genheap.c
@@ -1,7 +1,24 @@
+/*
+   IGraph library.
+   Copyright (C) 2022  The igraph development team <igraph@igraph.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "igraph_memory.h"
-#include "igraph_types.h"
-#include "igraph_vector.h"
+
+#include "core/genheap.h"
 
 #include <string.h> /* memcpy */
 
@@ -10,52 +27,17 @@
     #define restrict __restrict
 #endif
 
-typedef int cmp_t(const void *, const void *);
-
-typedef struct igraph_gen2wheap_t {
-    /** Maximum number of items in the heap */
-    igraph_integer_t max_size;
-
-    /** The size of an individual item */
-    size_t item_size;
-
-    /** The items themselves in the heap */
-    /* TODO: currently this is always allocated to have max_size */
-    void *data;
-
-    /** qsort-style comparison function used to order items */
-    cmp_t *cmp;
-
-    /** An integer index associated to each item in the heap; this vector is
-     * always modified in tandem with \c data . Values in this vector are
-     * between 0 and size-1 */
-    igraph_vector_int_t index;
-
-    /**
-     * A _reverse_ index that allows O(1) lookup of the position of a given
-     * value within the \c index member. Note that it uses two special values:
-     * index2[i] == 0 means that \c i is not in \c index at all, while
-     * index2[i] == 1 means that \c i is in \c index but it was _deactivated_.
-     * The semantics of deactivation is up to the user of the data structure
-     * to decide. Other than these two special values, index2[i] == j means
-     * that index[j-2] == i and data[j-2] is the corresponding item in the heap
-     */
-    igraph_vector_int_t index2;
-} igraph_gen2wheap_t;
-
-
 #define PARENT(x)     (((x)+1)/2-1)
 #define LEFTCHILD(x)  (((x)+1)*2-1)
 #define RIGHTCHILD(x) (((x)+1)*2)
 
 #define ELEM(h, x) ((char *) h->data + x * h->item_size)
 
-/* This is a smart indexed heap. In addition to the "normal" indexed heap
-   it allows to access every element through its index in O(1) time.
-   In other words, for this heap the indexing operation is O(1), the
-   normal heap does this in O(n) time.... */
-
-igraph_integer_t igraph_gen2wheap_size(const igraph_gen2wheap_t *h);
+/* This is a smart indexed heap that can hold elements of arbitrary type.
+ * In addition to the "normal" indexed heap, it allows to access every element
+ * through its index in O(1) time. In other words, for this heap the indexing
+ * operation is O(1), the normal heap does this in O(n) time.
+ */
 
 static void swapfunc(char * restrict a, char * restrict b, size_t es) {
     char t;
@@ -124,7 +106,11 @@ static void igraph_i_gen2wheap_sink(igraph_gen2wheap_t *h,
  * Initializes a new two-way heap. The max_size parameter defines the maximum
  * number of items that the heap can hold.
  */
-igraph_error_t igraph_gen2wheap_init(igraph_gen2wheap_t *h, cmp_t *cmp, size_t item_size, igraph_integer_t max_size) {
+igraph_error_t igraph_gen2wheap_init(
+        igraph_gen2wheap_t *h,
+        int (*cmp)(const void *, const void *),
+        size_t item_size, igraph_integer_t max_size
+) {
     h->max_size = max_size;
     /* We start with the biggest */
     IGRAPH_VECTOR_INT_INIT_FINALLY(&h->index2, max_size);
@@ -177,7 +163,7 @@ igraph_bool_t igraph_gen2wheap_empty(const igraph_gen2wheap_t *h) {
  * the same index.
  */
 igraph_error_t igraph_gen2wheap_push_with_index(igraph_gen2wheap_t *h,
-                                  igraph_integer_t idx, const void *elem) {
+                                                igraph_integer_t idx, const void *elem) {
 
     igraph_integer_t size = igraph_vector_int_size(&h->index);
 
@@ -251,7 +237,6 @@ const void *igraph_gen2wheap_get(const igraph_gen2wheap_t *h, igraph_integer_t i
  * that remain in the heap.
  */
 void igraph_gen2wheap_delete_max(igraph_gen2wheap_t *h) {
-
     igraph_integer_t tmpidx = VECTOR(h->index)[0];
     igraph_i_gen2wheap_switch(h, 0, igraph_gen2wheap_size(h) - 1);
     igraph_vector_int_pop_back(&h->index);
