46e4b978fbae74d764070bcaed0d2534d1f8b83d
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2023-01-22 12:15:00 +0100

    write edgelist directly in bipartite projection example

--- examples/simple/igraph_bipartite_projection.c
+++ examples/simple/igraph_bipartite_projection.c
@@ -20,32 +20,28 @@
 #include <stdlib.h>
 
 int main(void) {
-
     igraph_t g, p1, p2;
     igraph_vector_bool_t types;
-    igraph_vector_int_t mult1, mult2, edgelist;
+    igraph_vector_int_t mult1, mult2;
 
     igraph_vector_int_init(&mult1, 0);
     igraph_vector_int_init(&mult2, 0);
-    igraph_vector_int_init(&edgelist, 0);
 
     igraph_small(&g, 0, IGRAPH_UNDIRECTED, 0,1, 1,2, 0,3, 3,2, 2,4, 4,5, -1);
     igraph_vector_bool_init_int(&types, 6, 0, 1, 0, 1, 1, 0);
 
     igraph_bipartite_projection(&g, &types, &p1, &p2, &mult1, &mult2, /*probe1=*/ 1);
 
-    igraph_get_edgelist(&p1, &edgelist, 0);
-    igraph_vector_int_print(&edgelist);
-
-    igraph_get_edgelist(&p2, &edgelist, 0);
-    igraph_vector_int_print(&edgelist);
+    igraph_write_graph_edgelist(&p1, stdout);
+    printf("\n");
+    igraph_write_graph_edgelist(&p2, stdout);
+    printf("\n");
 
     igraph_vector_int_print(&mult1);
     igraph_vector_int_print(&mult2);
 
     igraph_vector_int_destroy(&mult1);
     igraph_vector_int_destroy(&mult2);
-    igraph_vector_int_destroy(&edgelist);
     igraph_destroy(&p1);
     igraph_destroy(&p2);
     igraph_destroy(&g);
