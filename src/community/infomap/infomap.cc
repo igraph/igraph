7b4219729d6fd16f0a480ae6413738427f58bee3
Szabolcs Horvát <szhorvat@gmail.com>
2023-02-10 20:42:09 +0100

    fix: use <math.h> instead of <cmath> for Infomap so that functions are callable without std:: namespace

--- src/community/infomap/infomap.cc
+++ src/community/infomap/infomap.cc
@@ -29,14 +29,15 @@
    homePage: http://www.irit.fr/~Emmanuel.Navarro/
 */
 
-#include <cmath>
+
 #include "igraph_interface.h"
 #include "igraph_community.h"
 #include "core/interruption.h"
 
-
 #include "infomap_Node.h"
 #include "infomap_Greedy.h"
+
+#include <math.h>
 
 /****************************************************************************/
 int infomap_partition(FlowGraph * fgraph, bool rcall) {
