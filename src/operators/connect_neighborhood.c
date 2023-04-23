1dd6c6696a798cd064abff25b1488802a4497986
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2023-04-23 13:40:19 +0200

    add graph_power
    fixes #2063

--- src/operators/connect_neighborhood.c
+++ src/operators/connect_neighborhood.c
@@ -162,3 +162,144 @@ igraph_error_t igraph_connect_neighborhood(igraph_t *graph, igraph_integer_t ord
 
     return IGRAPH_SUCCESS;
 }
+
+/**
+ * \function igraph_graph_power
+ * \brief connect each vertex to its neighborhood.
+ *
+ * This function adds new edges to the input graph. Each vertex is connected
+ * to all vertices reachable by at most \p order steps from it
+ * (unless a connection already existed).  In other words, the \p order power of
+ * the graph is computed.
+ *
+ * </para><para> For undirected graphs reachability is always
+ * symmetric: if vertex A can be reached from vertex B in at
+ * most \p order steps, then the opposite is also true. Only one
+ * undirected (A,B) edge will be added in this case.
+ * \param graph The input graph.
+ * \param res The resulting graph.
+ * \param order Integer constant, it gives the distance within which
+ *    the vertices will be connected to the source vertex.
+ * \param mode Constant, it specifies how the neighborhood search is
+ *    performed for directed graphs. If \c IGRAPH_OUT then vertices
+ *    reachable from the source vertex will be connected, \c IGRAPH_IN
+ *    is the opposite. If \c IGRAPH_ALL then the directed graph is
+ *    considered as an undirected one.
+ * \return Error code.
+ *
+ * \sa \ref igraph_square_lattice() uses this function to connect the
+ * neighborhood of the vertices.
+ *
+ * Time complexity: O(|V|*d^k), |V| is the number of vertices in the
+ * graph, d is the average degree and k is the \p order argument.
+ */
+igraph_error_t igraph_graph_power(igraph_t *graph, igraph_t *res, igraph_integer_t order,
+                                igraph_neimode_t mode) {
+
+    igraph_integer_t no_of_nodes = igraph_vcount(graph);
+    igraph_dqueue_int_t q;
+    igraph_vector_int_t edges;
+    igraph_integer_t i, j, in;
+    igraph_integer_t *added;
+    igraph_vector_int_t neis;
+
+    if (order < 0) {
+        IGRAPH_ERRORF("Order can not be negative, found %" IGRAPH_PRId ".",
+                IGRAPH_EINVAL, order);
+    }
+
+    if (order == 0) {
+        //TODO this handles attributes incorrectly, I guess you want all the graph and vertex attributes to remain the same?
+        IGRAPH_CHECK(igraph_empty(res, igraph_vcount(graph), igraph_is_directed(graph)));
+        return IGRAPH_SUCCESS;
+    }
+
+    IGRAPH_CHECK(igraph_copy(res, graph));
+    if (order == 1) {
+        return IGRAPH_SUCCESS;
+    }
+
+    if (!igraph_is_directed(graph)) {
+        mode = IGRAPH_ALL;
+    }
+
+    IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
+    added = IGRAPH_CALLOC(no_of_nodes, igraph_integer_t);
+    if (added == 0) {
+        IGRAPH_ERROR("Cannot connect neighborhood", IGRAPH_ENOMEM); /* LCOV_EXCL_LINE */
+    }
+    IGRAPH_FINALLY(igraph_free, added);
+    IGRAPH_DQUEUE_INT_INIT_FINALLY(&q, 100);
+    IGRAPH_VECTOR_INT_INIT_FINALLY(&neis, 0);
+
+    for (i = 0; i < no_of_nodes; i++) {
+        added[i] = i + 1;
+        IGRAPH_CHECK(igraph_neighbors(res, &neis, i, mode));
+        in = igraph_vector_int_size(&neis);
+        if (order > 1) {
+            for (j = 0; j < in; j++) {
+                igraph_integer_t nei = VECTOR(neis)[j];
+                added[nei] = i + 1;
+                IGRAPH_CHECK(igraph_dqueue_int_push(&q, nei));
+                IGRAPH_CHECK(igraph_dqueue_int_push(&q, 1));
+            }
+        }
+
+        while (!igraph_dqueue_int_empty(&q)) {
+            igraph_integer_t actnode = igraph_dqueue_int_pop(&q);
+            igraph_integer_t actdist = igraph_dqueue_int_pop(&q);
+            igraph_integer_t n;
+            IGRAPH_CHECK(igraph_neighbors(res, &neis, actnode, mode));
+            n = igraph_vector_int_size(&neis);
+
+            if (actdist < order - 1) {
+                for (j = 0; j < n; j++) {
+                    igraph_integer_t nei = VECTOR(neis)[j];
+                    if (added[nei] != i + 1) {
+                        added[nei] = i + 1;
+                        IGRAPH_CHECK(igraph_dqueue_int_push(&q, nei));
+                        IGRAPH_CHECK(igraph_dqueue_int_push(&q, actdist + 1));
+                        if (mode != IGRAPH_ALL || i < nei) {
+                            if (mode == IGRAPH_IN) {
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, nei));
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
+                            } else {
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, nei));
+                            }
+                        }
+                    }
+                }
+            } else {
+                for (j = 0; j < n; j++) {
+                    igraph_integer_t nei = VECTOR(neis)[j];
+                    if (added[nei] != i + 1) {
+                        added[nei] = i + 1;
+                        if (mode != IGRAPH_ALL || i < nei) {
+                            if (mode == IGRAPH_IN) {
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, nei));
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
+                            } else {
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
+                                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, nei));
+                            }
+                        }
+                    }
+                }
+            }
+
+        } /* while q not empty */
+    } /* for i < no_of_nodes */
+
+    igraph_vector_int_destroy(&neis);
+    igraph_dqueue_int_destroy(&q);
+    igraph_free(added);
+    IGRAPH_FINALLY_CLEAN(3);
+
+    IGRAPH_CHECK(igraph_add_edges(res, &edges, 0));
+
+    igraph_vector_int_destroy(&edges);
+    IGRAPH_FINALLY_CLEAN(1);
+
+    return IGRAPH_SUCCESS;
+}
---
+++ tests/unit/igraph_graph_power.c
@@ -0,0 +1,70 @@
+/* IGraph library.
+   Copyright (C) 2022  The igraph development team <igraph@igraph.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <igraph.h>
+#include "test_utilities.h"
+
+void print_and_destroy(igraph_t *g, igraph_integer_t order, igraph_neimode_t mode) {
+    igraph_t res;
+    igraph_graph_power(g, &res, order, mode);
+    // TODO should be print_graph_canon
+    print_graph(&res);
+    igraph_destroy(&res);
+}
+
+int main(void) {
+    igraph_t g;
+
+    printf("Graph with no vertices:\n");
+    igraph_small(&g, IGRAPH_UNDIRECTED, 0, -1);
+    print_and_destroy(&g, 10, IGRAPH_ALL);
+    igraph_destroy(&g);
+
+    printf("Directed graph with loops and multiple edges, order 0, IGRAPH_OUT:\n");
+    igraph_small(&g, 6, IGRAPH_DIRECTED, 0,1, 0,2, 1,1, 1,3, 2,3, 3,4, 3,4, -1);
+    print_and_destroy(&g, 0, IGRAPH_OUT);
+    printf("Same graph, order 1:\n");
+    print_and_destroy(&g, 1, IGRAPH_OUT);
+
+    printf("Same graph, order 2:\n");
+    print_and_destroy(&g, 2, IGRAPH_OUT);
+
+    printf("Same starting graph, order 2, IGRAPH_IN:\n");
+    print_and_destroy(&g, 2, IGRAPH_IN);
+
+    printf("Same starting graph, order 2, IGRAPH_ALL:\n");
+    print_and_destroy(&g, 2, IGRAPH_ALL);
+
+    printf("Same starting graph, order 3, IGRAPH_OUT:\n");
+    print_and_destroy(&g, 3, IGRAPH_OUT);
+
+    printf("Same starting graph, order 12, IGRAPH_IN:\n");
+    print_and_destroy(&g, 12, IGRAPH_IN);
+
+    printf("Same starting graph, but undirected, order 3, IGRAPH_OUT:\n");
+    igraph_destroy(&g);
+    igraph_small(&g, 6, IGRAPH_UNDIRECTED, 0,1, 0,2, 1,1, 1,3, 2,3, 3,4, 3,4, -1);
+    print_and_destroy(&g, 3, IGRAPH_OUT);
+
+    VERIFY_FINALLY_STACK();
+
+    printf("Check negative order error.\n");
+    CHECK_ERROR(igraph_graph_power(&g, NULL, -1, IGRAPH_OUT), IGRAPH_EINVAL);
+    igraph_destroy(&g);
+
+    return 0;
+}
