8a013cd718f32931ddc0c3058023bcd2863f23d2
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2023-04-30 10:04:12 +0200

    use CHECK_OOM in graph_power and connect_neighborhood

--- src/operators/connect_neighborhood.c
+++ src/operators/connect_neighborhood.c
@@ -84,9 +84,8 @@ igraph_error_t igraph_connect_neighborhood(igraph_t *graph, igraph_integer_t ord
 
     IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
     added = IGRAPH_CALLOC(no_of_nodes, igraph_integer_t);
-    if (added == 0) {
-        IGRAPH_ERROR("Cannot connect neighborhood", IGRAPH_ENOMEM); /* LCOV_EXCL_LINE */
-    }
+    IGRAPH_CHECK_OOM(added, "Cannot connect neighborhood.");
+
     IGRAPH_FINALLY(igraph_free, added);
     IGRAPH_DQUEUE_INT_INIT_FINALLY(&q, 100);
     IGRAPH_VECTOR_INT_INIT_FINALLY(&neis, 0);
@@ -226,9 +225,8 @@ igraph_error_t igraph_graph_power(const igraph_t *graph, igraph_t *res, igraph_i
 
     IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
     added = IGRAPH_CALLOC(no_of_nodes, igraph_integer_t);
-    if (added == 0) {
-        IGRAPH_ERROR("Cannot take the graph power.", IGRAPH_ENOMEM); /* LCOV_EXCL_LINE */
-    }
+    IGRAPH_CHECK_OOM(added, "Cannot take the graph power.");
+
     IGRAPH_FINALLY(igraph_free, added);
     IGRAPH_DQUEUE_INT_INIT_FINALLY(&q, 100);
     IGRAPH_VECTOR_INT_INIT_FINALLY(&neis, 0);
