ca5a04f89fdb7e6d3748f67c3410059b6085249d
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2023-05-01 19:25:47 +0200

    rename print_and_destroy in graph_power test

--- tests/unit/igraph_graph_power.c
+++ tests/unit/igraph_graph_power.c
@@ -18,7 +18,7 @@
 #include <igraph.h>
 #include "test_utilities.h"
 
-void print_and_destroy(igraph_t *g, igraph_integer_t order) {
+void print_result(igraph_t *g, igraph_integer_t order) {
     igraph_t res;
     igraph_graph_power(g, &res, order);
     print_graph_canon(&res);
@@ -34,37 +34,37 @@ int main(void) {
 
     printf("Graph with no vertices:\n");
     igraph_small(&g, IGRAPH_UNDIRECTED, 0, -1);
-    print_and_destroy(&g, 10);
+    print_result(&g, 10);
     igraph_destroy(&g);
 
     printf("Basic example, A->B->C, directed, order 2:\n");
     igraph_small(&g, 3, IGRAPH_DIRECTED, 0,1, 1,2, -1);
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
     igraph_destroy(&g);
 
     printf("Basic example, A->B->C, undirected, order 2:\n");
     igraph_small(&g, 3, IGRAPH_UNDIRECTED, 0,1, 1,2, -1);
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
     igraph_destroy(&g);
 
     printf("Basic example, A->B<-C, directed, order 2:\n");
     igraph_small(&g, 3, IGRAPH_DIRECTED, 0,1, 2,1, -1);
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
     igraph_destroy(&g);
 
     printf("Basic example, A<-B<-C, directed, order 2:\n");
     igraph_small(&g, 3, IGRAPH_DIRECTED, 1,0, 2,1, -1);
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
     igraph_destroy(&g);
 
     printf("Basic example, A->B->C->A, directed, order 2:\n");
     igraph_small(&g, 3, IGRAPH_DIRECTED, 0,1, 1,2, 2,0, -1);
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
     igraph_destroy(&g);
 
     printf("Directed graph with loops and multiple edges, order 0:\n");
     igraph_small(&g, 6, IGRAPH_DIRECTED, 0,1, 0,2, 1,1, 1,3, 2,3, 3,4, 3,4, -1);
-    print_and_destroy(&g, 0);
+    print_result(&g, 0);
     igraph_destroy(&g);
 
     printf("Directed graph with loops and multiple edges, order 0, with attributes set:\n");
@@ -74,24 +74,24 @@ int main(void) {
     SETEAB(&g, "bool_edge_attr", 0, 1);
     SETVAB(&g, "bool_vertex_attr", 0, 1);
 
-    print_and_destroy(&g, 0);
+    print_result(&g, 0);
 
     printf("Same graph, order 1:\n");
-    print_and_destroy(&g, 1);
+    print_result(&g, 1);
 
     printf("Same starting graph, order 2:\n");
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
 
     printf("Same starting graph, order 3:\n");
-    print_and_destroy(&g, 3);
+    print_result(&g, 3);
 
     printf("Same starting graph, order 12:\n");
-    print_and_destroy(&g, 12);
+    print_result(&g, 12);
 
     printf("Same starting graph, but undirected, order 2:\n");
     igraph_destroy(&g);
     igraph_small(&g, 6, IGRAPH_UNDIRECTED, 0,1, 0,2, 1,1, 1,3, 2,3, 3,4, 3,4, -1);
-    print_and_destroy(&g, 2);
+    print_result(&g, 2);
 
     VERIFY_FINALLY_STACK();
 
