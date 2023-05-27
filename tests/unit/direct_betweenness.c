193a68b4a191b88f1dce6bac3bebf6a6875b6d16
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2023-05-27 09:53:53 +0200

    Start working on direct betweenness
    Fixes #1796

---
+++ tests/unit/direct_betweenness.c
@@ -0,0 +1,18 @@
+//igraph/src/centrality/betweenness.c 500
+//
+#include "test_utilities.h"
+
+igraph_error_t igraph_direct_betweenness_cutoff(const igraph_t *graph, igraph_vector_t *res,
+                              const igraph_vs_t vids, igraph_bool_t directed,
+                              const igraph_vector_t *weights, igraph_real_t cutoff) {
+    igraph_vector_int_list_t vertices, edges;
+
+    if (!weights) {
+        igraph_get_all_shortest_paths(graph, &vertices, edges, NULL, igraph_integer_t from, const igraph_vs_t vids, mode) {
+
+    return IGRAPH_SUCCESS;
+}
+
+int main() {
+
+}
