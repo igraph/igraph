52cd60d9d1bc00a3c272b00b2a247b1df870ff90
Szabolcs Horvát <szhorvat@gmail.com>
2023-10-30 15:22:23 +0000

    refactor: minor readability cleanup for connected components

--- src/connectivity/components.c
+++ src/connectivity/components.c
@@ -108,12 +108,9 @@ static igraph_error_t igraph_i_connected_components_weak(
 ) {
 
     igraph_integer_t no_of_nodes = igraph_vcount(graph);
+    igraph_integer_t no_of_clusters;
     bool *already_added;
-    igraph_integer_t first_node, act_cluster_size = 0, no_of_clusters = 0;
-
     igraph_dqueue_int_t q = IGRAPH_DQUEUE_NULL;
-
-    igraph_integer_t i;
     igraph_vector_int_t neis = IGRAPH_VECTOR_NULL;
 
     /* Memory for result, csize is dynamically allocated */
@@ -155,7 +152,10 @@ static igraph_error_t igraph_i_connected_components_weak(
 
     /* The algorithm */
 
-    for (first_node = 0; first_node < no_of_nodes; ++first_node) {
+    no_of_clusters = 0;
+    for (igraph_integer_t first_node = 0; first_node < no_of_nodes; ++first_node) {
+        igraph_integer_t act_cluster_size;
+
         if (already_added[first_node]) {
             continue;
         }
@@ -172,7 +172,7 @@ static igraph_error_t igraph_i_connected_components_weak(
             igraph_integer_t act_node = igraph_dqueue_int_pop(&q);
             IGRAPH_CHECK(igraph_neighbors(graph, &neis, act_node, IGRAPH_ALL));
             igraph_integer_t nei_count = igraph_vector_int_size(&neis);
-            for (i = 0; i < nei_count; i++) {
+            for (igraph_integer_t i = 0; i < nei_count; i++) {
                 igraph_integer_t neighbor = VECTOR(neis)[i];
                 if (already_added[neighbor]) {
                     continue;
@@ -216,16 +216,10 @@ static igraph_error_t igraph_i_connected_components_strong(
 ) {
     igraph_integer_t no_of_nodes = igraph_vcount(graph);
     igraph_vector_int_t next_nei = IGRAPH_VECTOR_NULL;
-
-    igraph_integer_t i, n, num_seen;
+    igraph_integer_t num_seen;
     igraph_dqueue_int_t q = IGRAPH_DQUEUE_NULL;
-
     igraph_integer_t no_of_clusters = 0;
-    igraph_integer_t act_cluster_size;
-
     igraph_vector_int_t out = IGRAPH_VECTOR_NULL;
-    const igraph_vector_int_t* tmp;
-
     igraph_adjlist_t adjlist;
 
     /* Memory for result, csize is dynamically allocated */
@@ -270,7 +264,9 @@ static igraph_error_t igraph_i_connected_components_strong(
     IGRAPH_FINALLY(igraph_adjlist_destroy, &adjlist);
 
     num_seen = 0;
-    for (i = 0; i < no_of_nodes; i++) {
+    for (igraph_integer_t i = 0; i < no_of_nodes; i++) {
+        const igraph_vector_int_t *tmp;
+
         IGRAPH_ALLOW_INTERRUPTION();
 
         tmp = igraph_adjlist_get(&adjlist, i);
@@ -323,6 +319,7 @@ static igraph_error_t igraph_i_connected_components_strong(
     num_seen = 0;
 
     while (!igraph_vector_int_empty(&out)) {
+        igraph_integer_t act_cluster_size;
         igraph_integer_t grandfather = igraph_vector_int_pop_back(&out);
 
         if (VECTOR(next_nei)[grandfather] != 0) {
@@ -345,9 +342,9 @@ static igraph_error_t igraph_i_connected_components_strong(
 
         while (!igraph_dqueue_int_empty(&q)) {
             igraph_integer_t act_node = igraph_dqueue_int_pop_back(&q);
-            tmp = igraph_adjlist_get(&adjlist, act_node);
-            n = igraph_vector_int_size(tmp);
-            for (i = 0; i < n; i++) {
+            const igraph_vector_int_t *tmp = igraph_adjlist_get(&adjlist, act_node);
+            const igraph_integer_t n = igraph_vector_int_size(tmp);
+            for (igraph_integer_t i = 0; i < n; i++) {
                 igraph_integer_t neighbor = VECTOR(*tmp)[i];
                 if (VECTOR(next_nei)[neighbor] != 0) {
                     continue;
