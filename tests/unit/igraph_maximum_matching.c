220e579774d3f793d16bc9ff23a8edb0f6e16087
Egan Schafer <eganschafer@gmail.com>
2024-01-25 08:46:05 -0700

    tests: added tests for maximum matching

--- tests/unit/igraph_maximum_matching.c
+++ tests/unit/igraph_maximum_matching.c
@@ -17,10 +17,133 @@
 */
 
 #include <igraph.h>
-#include "test_utilities.inc"
+#include "test_utilities.h"
+
+int test_trivial_graphs() {
+    int result = 0;
+    igraph_t graph;
+    igraph_vector_int_t edges;
+    igraph_vector_int_t matching;
+    igraph_integer_t matching_size;
+
+    igraph_vector_int_init(&edges,0);
+    //test null graph
+    igraph_create(&graph, &edges, 0, 0);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 0) {
+        printf("Null graph had %" IGRAPH_PRId " matching size.\n", matching_size);
+        result = 1;
+    }
+    //test singleton graph
+    igraph_create(&graph, &edges, 1, false);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 0) {
+        printf("Singleton graph had %" IGRAPH_PRId " matching size.\n", matching_size);
+        result = 2;
+    }
+    return result;
+}
+
+int test_path_graphs() {
+    int result = 0;
+    igraph_t graph;
+    igraph_vector_int_t matching;
+    igraph_integer_t matching_size;
+    //test path graphs lengths 1-3
+    igraph_ring(&graph, 2, IGRAPH_UNDIRECTED, 0, 1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 1) {
+        printf("Length 1 path graph had %" IGRAPH_PRId " matching size.\n", matching_size);
+        result = 1;
+    }
+    igraph_ring(&graph, 3, IGRAPH_UNDIRECTED, 0, 1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 2) {
+        printf("Length 2 path graph had %" IGRAPH_PRId " matching size.\n", matching_size);
+        result = 2;
+    }
+    igraph_ring(&graph, 4, IGRAPH_UNDIRECTED, 0, 1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 3) {
+        printf("Length 3 path graph had %" IGRAPH_PRId " matching size.\n", matching_size);
+        result = 3;
+    }
+    return result;
+}
+
+int test_bipartite_graphs() {
+    int result = 0;
+    igraph_t graph;
+    igraph_vector_int_t matching;
+    igraph_integer_t matching_size;
+    //test two bipartite graphs
+    igraph_small(&graph, 9, IGRAPH_UNDIRECTED, 1,2, 2,3, 2,4, 2,5, 3,6, 4,7, 5,8, 6,9, -1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 4) {
+        printf("Bipartite graph 1 had %" IGRAPH_PRId " matching size, expected 4.\n", matching_size);
+        result = 1;
+    }
+    igraph_small(&graph, 6, IGRAPH_UNDIRECTED, 1,2, 1,3, 2,4, 3,4, 3,5, 4,6, 5,6, -1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 3) {
+        printf("Bipartite graph 2 had %" IGRAPH_PRId " matching size, expected 3.\n", matching_size);
+        result = 2;
+    }
+    return result;
+}
+
+int test_general_graphs() {
+    int result = 0;
+    igraph_t graph;
+    igraph_vector_int_t matching;
+    igraph_integer_t matching_size;
+    //test two non-bipartite graphs
+    igraph_small(&graph, 10, IGRAPH_UNDIRECTED, 1,2, 1,3, 2,9, 3,4, 3,5, 4,6, 5,7, 5,8, 7,8, 8,9, 8,10, -1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 5) {
+        printf("General graph 1 had %" IGRAPH_PRId " matching size, expected 5.\n", matching_size);
+        result = 1;
+    }
+    igraph_small(&graph, 6, IGRAPH_UNDIRECTED, 1,2, 1,3, 2,4, 3,5, 4,5, 5,6, -1);
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 3) {
+        printf("General graph 2 had %" IGRAPH_PRId " matching size, expected 3.\n", matching_size);
+        result = 2;
+    }
+    return result;
+}
+
+int test_petersen_graph() {
+    int result = 0;
+    igraph_t graph;
+    igraph_vector_int_t matching;
+    igraph_integer_t matching_size;
+    //test petersen graph
+    igraph_famous(&graph, "Petersen");
+    igraph_maximum_matching(&graph, &matching_size, NULL, &matching, NULL);
+    if (matching_size != 5) {
+        printf("Petersen graph had %" IGRAPH_PRId " matching size, expected 5.\n", matching_size);
+        result = 1;
+    }
+    return result;
+}
 
 int main() {
-
-    //TODO: implement tests
-    return 0;
+    int result = 0;
+    if (test_trivial_graphs()) {
+        result = 1;
+    }
+    if (test_path_graphs()) {
+        result = 1;
+    }
+    if (test_bipartite_graphs()) {
+        result = 1;
+    }
+    if (test_general_graphs()) {
+        result = 1;
+    }
+    if (test_petersen_graph()) {
+        result = 1;
+    }
+    return result;
 }
