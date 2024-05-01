2409c65a16a8166594618d41f6f19d5f0876be2b
Szabolcs Horvát <szhorvat@gmail.com>
2024-05-01 22:03:23 +0000

    Update strength benchmark

--- tests/benchmarks/igraph_strength.c
+++ tests/benchmarks/igraph_strength.c
@@ -24,16 +24,53 @@ int main(void) {
     igraph_vector_init(&strength, 0);
     igraph_vector_init(&weights, 0);
 
-#define N 10000
+    /* igraph_strength() uses an optimized, cache friendly code path when given
+     * a vertex selector for which igraph_vs_is_all() returns true (this is
+     * currently igraph_vs_all()). We pass both igraph_vss_all() and
+     * igraph_vss_range(0, igraph_vcount(graph)) as vertex selectors
+     * to compare the performance of the two code paths.
+     *
+     * NOTE: While currently igraph_vs_is_all() does not return true for
+     * a range-type vertex selector, this may change in the future.
+     * An altrenative is to to use igraph_vs_vector(), with the vector
+     * initialized to a range.
+     */
+
+#define N 1000
 #define M 10
-#define REP 100
+#define REP 10000
 
     igraph_barabasi_game(&g, N, 1, M, NULL, true, 0, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE_MULTIPLE, NULL);
     rand_weight_vec(&weights, &g);
 
-    BENCH(" 1 igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+    BENCH(" 1a igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+          REPEAT(igraph_strength(&g, &strength, igraph_vss_all(), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
+          );
+    BENCH(" 1b igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+          REPEAT(igraph_strength(&g, &strength, igraph_vss_range(0, igraph_vcount(&g)), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
+          );
+    printf("\n");
+
+    igraph_destroy(&g);
+
+#undef N
+#undef M
+#undef REP
+
+#define N 10000
+#define M 10
+#define REP 1000
+
+    igraph_barabasi_game(&g, N, 1, M, NULL, true, 0, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE_MULTIPLE, NULL);
+    rand_weight_vec(&weights, &g);
+
+    BENCH(" 2a igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
           REPEAT(igraph_strength(&g, &strength, igraph_vss_all(), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
     );
+    BENCH(" 2b igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+          REPEAT(igraph_strength(&g, &strength, igraph_vss_range(0, igraph_vcount(&g)), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
+    );
+    printf("\n");
 
     igraph_destroy(&g);
 
@@ -48,9 +85,13 @@ int main(void) {
     igraph_barabasi_game(&g, N, 1, M, NULL, true, 0, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE_MULTIPLE, NULL);
     rand_weight_vec(&weights, &g);
 
-    BENCH(" 2 igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+    BENCH(" 3a igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
           REPEAT(igraph_strength(&g, &strength, igraph_vss_all(), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
     );
+    BENCH(" 3b igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+          REPEAT(igraph_strength(&g, &strength, igraph_vss_range(0, igraph_vcount(&g)), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
+    );
+    printf("\n");
 
     igraph_destroy(&g);
 
@@ -65,9 +106,13 @@ int main(void) {
     igraph_barabasi_game(&g, N, 1, M, NULL, true, 0, IGRAPH_UNDIRECTED, IGRAPH_BARABASI_PSUMTREE_MULTIPLE, NULL);
     rand_weight_vec(&weights, &g);
 
-    BENCH(" 3 igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+    BENCH(" 4a igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
           REPEAT(igraph_strength(&g, &strength, igraph_vss_all(), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
     );
+    BENCH(" 4b igraph_strength(), preferential attachment n=" TOSTR(N) ", m=" TOSTR(M) ", " TOSTR(REP) "x",
+          REPEAT(igraph_strength(&g, &strength, igraph_vss_range(0, igraph_vcount(&g)), IGRAPH_ALL, IGRAPH_LOOPS, &weights), REP)
+    );
+    printf("\n");
 
     igraph_destroy(&g);
 
