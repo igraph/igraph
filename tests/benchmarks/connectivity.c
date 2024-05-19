cbbc138050a442be78239ffdf6a20360d95f99e9
Szabolcs Horvát <szhorvat@gmail.com>
2024-05-19 19:48:01 +0000

    bench: cosmetic cleanup in benchmarks

--- tests/benchmarks/connectivity.c
+++ tests/benchmarks/connectivity.c
@@ -14,49 +14,49 @@ void run_bench(igraph_integer_t vcount, igraph_real_t meandeg, igraph_integer_t 
     igraph_vector_int_init(&ivec, igraph_vcount(&g));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 1 Weakly connected?   vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 1 Weakly connected?   vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
               vcount, meandeg, 10*rep);
 
     igraph_invalidate_cache(&g);
     BENCH(msg, REPEAT(igraph_is_connected(&g, &b, IGRAPH_WEAK), rep));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 2 Weak components.    vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 2 Weak components.    vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
              vcount, meandeg, rep);
 
     igraph_invalidate_cache(&g);
     BENCH(msg, REPEAT(igraph_connected_components(&g, &ivec, NULL, NULL, IGRAPH_WEAK), rep));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 3 Strongly connected? vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 3 Strongly connected? vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
              vcount, meandeg, 10*rep);
 
     igraph_invalidate_cache(&g);
     BENCH(msg, REPEAT(igraph_is_connected(&g, &b, IGRAPH_STRONG), rep));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 4 Strong components.  vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 4 Strong components.  vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
              vcount, meandeg, rep);
 
     igraph_invalidate_cache(&g);
     BENCH(msg, REPEAT(igraph_connected_components(&g, &ivec, NULL, NULL, IGRAPH_STRONG), rep));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 5 Biconnected?        vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 5 Biconnected?        vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
              vcount, meandeg, rep);
 
     igraph_invalidate_cache(&g);
     BENCH(msg, REPEAT(igraph_is_biconnected(&g, &b), rep));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 6 Bridges.            vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 6 Bridges.            vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
              vcount, meandeg, rep);
 
     igraph_invalidate_cache(&g);
     BENCH(msg, REPEAT(igraph_bridges(&g, &ivec), rep));
 
     snprintf(msg, sizeof(msg) / sizeof(msg[0]),
-             "> 7 Articulation pts.   vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
+             " 7 Articulation pts.   vcount=%" IGRAPH_PRId ", meandeg=%g, %" IGRAPH_PRId "x",
              vcount, meandeg, rep);
 
     igraph_invalidate_cache(&g);
