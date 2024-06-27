6d0325bc6891d0294bef9970819ac5e870cd3ab1
Szabolcs Horvát <szhorvat@gmail.com>
2024-06-27 15:58:49 +0000

    feat: igraph_vector_is_all_finite()

--- include/igraph_vector.h
+++ include/igraph_vector.h
@@ -146,6 +146,7 @@ IGRAPH_EXPORT igraph_error_t igraph_vector_complex_zapsmall(igraph_vector_comple
 IGRAPH_EXPORT igraph_error_t igraph_vector_is_nan(const igraph_vector_t *v,
                                                   igraph_vector_bool_t *is_nan);
 IGRAPH_EXPORT IGRAPH_FUNCATTR_PURE igraph_bool_t igraph_vector_is_any_nan(const igraph_vector_t *v);
+IGRAPH_EXPORT IGRAPH_FUNCATTR_PURE igraph_bool_t igraph_vector_is_all_finite(const igraph_vector_t *v);
 
 IGRAPH_PRIVATE_EXPORT igraph_error_t igraph_vector_order2(igraph_vector_t *v);
 IGRAPH_PRIVATE_EXPORT igraph_error_t igraph_vector_rank(const igraph_vector_t *v, igraph_vector_int_t *res,
