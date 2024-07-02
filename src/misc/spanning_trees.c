197b46904237e8d027972e1bb6ba28f4cd095182
Szabolcs Horvát <szhorvat@gmail.com>
2024-07-02 22:44:24 +0000

    feat: complete spanning tree with Kruskal’s algorithm

--- src/misc/spanning_trees.c
+++ src/misc/spanning_trees.c
@@ -313,6 +313,8 @@ igraph_error_t igraph_minimum_spanning_tree_prim(
 }
 
 
+/* Kruskal's algorithm */
+
 static igraph_integer_t get_comp(igraph_vector_int_t *comp, igraph_integer_t i) {
     igraph_integer_t k = i;
     for (;;) {
@@ -327,16 +329,77 @@ static igraph_integer_t get_comp(igraph_vector_int_t *comp, igraph_integer_t i) 
 }
 
 static void merge_comp(igraph_vector_int_t *comp, igraph_integer_t i, igraph_integer_t j) {
-    VECTOR(*comp)[i] = get_comp(comp, j);
+    igraph_integer_t ci = get_comp(comp, i);
+    igraph_integer_t cj = get_comp(comp, j);
+    VECTOR(*comp)[ci] = cj;
 }
 
-static igraph_error_t igraph_i_minimum_spanning_tree_kruskal(
-    const igraph_t *graph, igraph_vector_int_t *res, const igraph_vector_t *weights) {
+/**
+ * \ingroup structural
+ * \function igraph_minimum_spanning_tree_kruskal
+ * \brief A minimum spanning tree of a weighted graph using Kruskal's method.
+ *
+ * Finds a spanning tree or spanning forest for which the sum of edge
+ * weights is the smallest. This function uses Kruskal's method for carrying
+ * out the computation.
+ *
+ * </para><para>
+ * Directed graphs are considered as undirected for this computation.
+ *
+ * </para><para>
+ * Reference:
+ *
+ * </para><para>
+ * Kruskal, J. B.:
+ * On the shortest spanning subtree of a graph and the traveling salesman problem,
+ * Proc. Amer. Math. Soc. 7 (1956), 48-50
+ * https://doi.org/10.1090%2FS0002-9939-1956-0078686-7
+ *
+ * \param graph The graph object. Edge directions will be ignored.
+ * \param res An initialized vector, the IDs of the edges that constitute
+ *        a spanning tree will be returned here. Use
+ *        \ref igraph_subgraph_from_edges() to extract the spanning tree as
+ *        a separate graph object.
+ * \param weights A vector containing the weights of the edges
+ *        in the same order as the simple edge iterator visits them
+ *        (i.e. in increasing order of edge IDs).
+ * \return Error code:
+ *         \c IGRAPH_ENOMEM, not enough memory.
+ *         \c IGRAPH_EINVAL, length of weight vector does not
+ *           match number of edges, or NaN in weights.
+ *
+ * Time complexity: O(|E| log |V|),
+ * |V| is the number of vertices,
+ * |E| the number of edges in the
+ * graph.
+ *
+ * \sa \ref igraph_minimum_spanning_tree() for a common interface to all
+ * minimum spanning tree algorithms.
+ *
+ * \example examples/simple/igraph_minimum_spanning_tree.c
+ */
+
+igraph_error_t igraph_minimum_spanning_tree_kruskal(
+        const igraph_t *graph,
+        igraph_vector_int_t *res,
+        const igraph_vector_t *weights) {
 
     const igraph_integer_t no_of_nodes = igraph_vcount(graph);
     const igraph_integer_t no_of_edges = igraph_ecount(graph);
     igraph_vector_int_t idx, comp;
     igraph_integer_t tree_edge_count;
+
+    if (weights == NULL) {
+        return igraph_minimum_spanning_tree_unweighted(graph, res);
+    }
+
+    if (igraph_vector_size(weights) != igraph_ecount(graph)) {
+        IGRAPH_ERROR("Weight vector length does not match number of edges.", IGRAPH_EINVAL);
+    }
+
+    if (igraph_vector_is_any_nan(weights)) {
+        IGRAPH_ERROR("Weigths must not contain NaN values.", IGRAPH_EINVAL);
+    }
 
     IGRAPH_VECTOR_INT_INIT_FINALLY(&idx, no_of_edges);
     IGRAPH_CHECK(igraph_vector_qsort_ind(weights, &idx, IGRAPH_ASCENDING));
@@ -372,6 +435,7 @@ static igraph_error_t igraph_i_minimum_spanning_tree_kruskal(
 
     return IGRAPH_SUCCESS;
 }
+
 
 /* igraph_random_spanning_tree */
 
