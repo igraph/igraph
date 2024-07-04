8a184199730fb835b0c6944f3c3af44f367077dd
Vincent Traag <vincent@traag.net>
2024-07-04 13:38:16 +0200

    Make Infomap vendored optional dependency

--- etc/cmake/dependencies.cmake
+++ etc/cmake/dependencies.cmake
@@ -9,11 +9,11 @@ include(FindThreads)
 
 macro(find_dependencies)
   # Declare the list of dependencies that _may_ be vendored
-  set(VENDORABLE_DEPENDENCIES BLAS GLPK LAPACK ARPACK GMP PLFIT)
+  set(VENDORABLE_DEPENDENCIES BLAS GLPK LAPACK ARPACK GMP PLFIT INFOMAP)
 
   # Declare optional dependencies associated with IGRAPH_..._SUPPORT flags
-  # Note that GLPK is both vendorable and optional
-  set(OPTIONAL_DEPENDENCIES GLPK OpenMP)
+  # Note that GLPK and INFOMAP are both vendorable and optional
+  set(OPTIONAL_DEPENDENCIES GLPK OpenMP INFOMAP)
 
   # Declare configuration options for dependencies
   tristate(IGRAPH_USE_INTERNAL_GMP "Compile igraph with internal Mini-GMP" AUTO)
@@ -46,7 +46,6 @@ macro(find_dependencies)
         set(IGRAPH_USE_INTERNAL_${LIBNAME_UPPER} ON)
       endif()
     endif()
-
     if(IGRAPH_USE_INTERNAL_${LIBNAME_UPPER})
       list(APPEND VENDORED_DEPENDENCIES ${DEPENDENCY})
     else()
@@ -85,6 +84,10 @@ macro(find_dependencies)
     else()
       list(REMOVE_ITEM REQUIRED_DEPENDENCIES GLPK)
     endif()
+  endif()
+
+  if(NOT IGRAPH_INFOMAP_SUPPORT)
+    list(REMOVE_ITEM VENDORED_DEPENDENCIES INFOMAP)
   endif()
 
   if(IGRAPH_GRAPHML_SUPPORT)
@@ -132,6 +135,7 @@ macro(find_dependencies)
 
   # Export some aliases that will be used in config.h
   set(HAVE_GLPK ${GLPK_FOUND})
+  set(HAVE_INFOMAP ${INFOMAP_FOUND})
   set(HAVE_GMP ${GMP_FOUND})
   set(HAVE_LIBXML ${LIBXML2_FOUND})
 
