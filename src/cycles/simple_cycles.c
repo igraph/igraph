9b82ffb5d2bb01f494fdcf814ec21ff149c33f74
Szabolcs Horvát <szhorvat@gmail.com>
2024-07-23 00:58:38 +0200

    remove some unused headers

--- src/cycles/simple_cycles.c
+++ src/cycles/simple_cycles.c
@@ -19,13 +19,9 @@
 #include "igraph_cycles.h"
 
 #include "igraph_adjlist.h"
-#include "igraph_components.h"
-#include "igraph_dqueue.h"
 #include "igraph_error.h"
 #include "igraph_interface.h"
 #include "igraph_stack.h"
-#include "igraph_structural.h"
-#include "../../tests/unit/test_utilities.h"
 
 #include "core/interruption.h"
 
