b73007869f1a9be671696480383e74db9de99aec
Szabolcs Horvát <szhorvat@gmail.com>
2024-11-03 12:59:13 +0000

    revert some header reformatting

--- include/igraph_cycles.h
+++ include/igraph_cycles.h
@@ -12,18 +12,28 @@
 __BEGIN_DECLS
 
 IGRAPH_EXPORT igraph_error_t igraph_fundamental_cycles(
-    const igraph_t *graph, igraph_vector_int_list_t *result,
-    igraph_integer_t start_vid, igraph_integer_t bfs_cutoff,
+    const igraph_t *graph,
+    igraph_vector_int_list_t *result,
+    igraph_integer_t start_vid,
+    igraph_integer_t bfs_cutoff,
     const igraph_vector_t *weights);
 
 IGRAPH_EXPORT igraph_error_t igraph_minimum_cycle_basis(
-    const igraph_t *graph, igraph_vector_int_list_t *result,
-    igraph_integer_t bfs_cutoff, igraph_bool_t complete,
-    igraph_bool_t use_cycle_order, const igraph_vector_t *weights);
+    const igraph_t *graph,
+    igraph_vector_int_list_t *result,
+    igraph_integer_t bfs_cutoff,
+    igraph_bool_t complete,
+    igraph_bool_t use_cycle_order,
+    const igraph_vector_t *weights);
+
+IGRAPH_EXPORT igraph_error_t igraph_find_cycle(
+    const igraph_t *graph,
+    igraph_vector_int_t *vertices,
+    igraph_vector_int_t *edges,
+    igraph_neimode_t mode);
 
 struct igraph_simple_cycle_search_state_t;
-typedef struct igraph_simple_cycle_search_state_t
-    igraph_simple_cycle_search_state_t;
+typedef struct igraph_simple_cycle_search_state_t igraph_simple_cycle_search_state_t;
 
 IGRAPH_EXPORT igraph_error_t igraph_simple_cycle_search_state_init(
     igraph_simple_cycle_search_state_t *state, const igraph_t *graph);
@@ -59,12 +69,6 @@ IGRAPH_EXPORT igraph_error_t igraph_simple_cycles_search_callback(
 IGRAPH_EXPORT igraph_error_t igraph_simple_cycles_search_all(
     const igraph_t *graph, igraph_vector_int_list_t *v_result,
     igraph_vector_int_list_t *e_result, igraph_integer_t max_cycle_length);
-
-
-IGRAPH_EXPORT igraph_error_t igraph_find_cycle(const igraph_t *graph,
-                                               igraph_vector_int_t *vertices,
-                                               igraph_vector_int_t *edges,
-                                               igraph_neimode_t mode);
 
 __END_DECLS
 
