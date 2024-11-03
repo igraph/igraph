edcb8900771ced6e6a417832624745b3e5c7dc1f
Szabolcs Horvát <szhorvat@gmail.com>
2024-11-03 13:00:05 +0000

    fix reverting header reformatting

--- include/igraph_cycles.h
+++ include/igraph_cycles.h
@@ -12,25 +12,25 @@
 __BEGIN_DECLS
 
 IGRAPH_EXPORT igraph_error_t igraph_fundamental_cycles(
-    const igraph_t *graph,
-    igraph_vector_int_list_t *result,
-    igraph_integer_t start_vid,
-    igraph_integer_t bfs_cutoff,
-    const igraph_vector_t *weights);
+        const igraph_t *graph,
+        igraph_vector_int_list_t *result,
+        igraph_integer_t start_vid,
+        igraph_integer_t bfs_cutoff,
+        const igraph_vector_t *weights);
 
 IGRAPH_EXPORT igraph_error_t igraph_minimum_cycle_basis(
-    const igraph_t *graph,
-    igraph_vector_int_list_t *result,
-    igraph_integer_t bfs_cutoff,
-    igraph_bool_t complete,
-    igraph_bool_t use_cycle_order,
-    const igraph_vector_t *weights);
+        const igraph_t *graph,
+        igraph_vector_int_list_t *result,
+        igraph_integer_t bfs_cutoff,
+        igraph_bool_t complete,
+        igraph_bool_t use_cycle_order,
+        const igraph_vector_t *weights);
 
 IGRAPH_EXPORT igraph_error_t igraph_find_cycle(
-    const igraph_t *graph,
-    igraph_vector_int_t *vertices,
-    igraph_vector_int_t *edges,
-    igraph_neimode_t mode);
+        const igraph_t *graph,
+        igraph_vector_int_t *vertices,
+        igraph_vector_int_t *edges,
+        igraph_neimode_t mode);
 
 struct igraph_simple_cycle_search_state_t;
 typedef struct igraph_simple_cycle_search_state_t igraph_simple_cycle_search_state_t;
