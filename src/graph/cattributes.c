5cbcb33b9ce4b7470197f991be06472bd25fe24b
Szabolcs Horvát <szhorvat@gmail.com>
2025-03-23 22:33:52 +0000

    refactor: remove unused header

--- src/graph/cattributes.c
+++ src/graph/cattributes.c
@@ -25,8 +25,6 @@
 #include "igraph_interface.h"
 #include "igraph_random.h"
 
-#include "internal/hacks.h" /* strdup */
-
 #include <string.h>
 
 /* An attribute is either a numeric vector (vector_t), a boolean vector
