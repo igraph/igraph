5127b0d91f3afa3b40395203d0024deae236769d
Gulshan Kumar <gksept2005@gmail.com>
2025-03-31 15:58:36 +0530

    Added tests for mycielski graph-->passes

--- tests/unit/mycielski_graph.c
+++ tests/unit/mycielski_graph.c
@@ -20,3 +20,59 @@
 
 #include "test_utilities.h"
 
+int main(void) {
+    igraph_t res;
+    igraph_t expected_res;
+    igraph_bool_t is_isomorph;
+
+    //should be a vertex
+    igraph_mycielski_graph(&res, 1);
+    igraph_small(
+        &expected_res, 1, /* directed = */ 0,
+        -1
+    );
+    igraph_isomorphic(&res, &expected_res, &is_isomorph);
+    IGRAPH_ASSERT(is_isomorph);
+    igraph_destroy(&res);
+    igraph_destroy(&expected_res);
+
+    // should be a path
+    igraph_mycielski_graph(&res, 2);
+    igraph_small(
+        &expected_res, 2, /* directed = */ 0,
+        /* edge 0 */ 0, 1,
+        -1
+    );
+    igraph_isomorphic(&res, &expected_res, &is_isomorph);
+    IGRAPH_ASSERT(is_isomorph);
+    igraph_destroy(&res);
+    igraph_destroy(&expected_res);
+
+    // should a 5-cycle
+    igraph_mycielski_graph(&res, 3);
+    igraph_small(
+        &expected_res, 5, /* directed = */ 0,
+        /* edge 0 */ 0, 1,
+        /* edge 1 */ 1, 2,
+        /* edge 2 */ 2, 3,
+        /* edge 3 */ 3, 4,
+        /* edge 4 */ 4, 0,
+        -1
+    );
+    igraph_isomorphic(&res, &expected_res, &is_isomorph);
+    IGRAPH_ASSERT(is_isomorph);
+    igraph_destroy(&res);
+    igraph_destroy(&expected_res);
+
+    // should be a grotzsch graph
+    igraph_mycielski_graph(&res, 4);
+    igraph_famous(&expected_res, "grotzsch");
+    igraph_isomorphic(&res, &expected_res, &is_isomorph);
+    IGRAPH_ASSERT(is_isomorph);
+    igraph_destroy(&res);
+    igraph_destroy(&expected_res);
+
+    VERIFY_FINALLY_STACK();
+
+    return 0;
+}
