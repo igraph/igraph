4db05529bab22cb82f83dad051297de894b8e44e
Gulshan Kumar <gksept2005@gmail.com>
2025-04-26 09:53:58 +0530

    all product tests in single file

--- tests/unit/igraph_product.c
+++ tests/unit/igraph_product.c
@@ -180,13 +180,38 @@ void test_multigraph(void) {
     igraph_destroy(&product);
 }
 
+// K2 X G(5,2) = G(10,3)
+void test_petersen(void) {
+    igraph_t k2, g_5_2, g_10_3, product;
+    igraph_bool_t is_iso;
+
+    igraph_full(&k2, 2, IGRAPH_UNDIRECTED, false);
+    igraph_generalized_petersen(&g_5_2, 5, 2);
+    igraph_generalized_petersen(&g_10_3, 10, 3);
+
+    igraph_product(&product, &k2, &g_5_2, IGRAPH_PRODUCT_TENSOR);
+
+    igraph_isomorphic(&product, &g_10_3, &is_iso);
+
+    IGRAPH_ASSERT(is_iso);
+
+    igraph_destroy(&k2);
+    igraph_destroy(&g_5_2);
+    igraph_destroy(&g_10_3);
+    igraph_destroy(&product);
+}
+
 int main(void) {
+    // CARTESIAN PRODUCT TEST
     test_grid_vs_square_lattice();
     test_cylinder_vs_cartesian();
     test_cube_vs_cartesian();
     test_torus_vs_cartesian();
     test_self_loop();
     test_multigraph();
+
+    // TENSOR PRODUCT TEST
+    test_petersen();
 
     VERIFY_FINALLY_STACK();
 
