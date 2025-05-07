f5cacdd0b773b14ca3280a28ef360da967ed8942
devlo90 <devlohani14251425@gmail.com>
2025-05-07 11:24:49 +0530

    fixed conditional logic

--- src/misc/bipartite.c
+++ src/misc/bipartite.c
@@ -22,7 +22,6 @@
 */
 
 #include "igraph_bipartite.h"
-
 #include "igraph_adjlist.h"
 #include "igraph_interface.h"
 #include "igraph_constructors.h"
@@ -1039,13 +1038,20 @@ static igraph_error_t gnp_bipartite_large(
             j += gap;
 
             /* Handle edge creation based on directedness and mode */
-            if (!directed || mode == IGRAPH_OUT) {
+            if (!directed) {
+                /* For undirected graphs, always add a single edge */
+                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
+                IGRAPH_CHECK(igraph_vector_int_push_back(&edges, j + n1));
+            } else if (mode == IGRAPH_OUT) {
+                /* For directed graphs with OUT mode, edges point from bottom to top */
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, j + n1));
             } else if (mode == IGRAPH_IN) {
+                /* For directed graphs with IN mode, edges point from top to bottom */
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, j + n1));
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
             } else { /* mode == IGRAPH_ALL */
+                /* For directed graphs with ALL mode, add both directions */
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, i));
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, j + n1));
                 IGRAPH_CHECK(igraph_vector_int_push_back(&edges, j + n1));
