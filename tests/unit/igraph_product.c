b3b5e749aa851a101db6405a10ff1d5e1bfa3af4
Gulshan Kumar <72340125+gulshan-123@users.noreply.github.com>
2025-05-18 11:59:08 +0530

    Update igraph_product.c
    By mistake removed tab

--- tests/unit/igraph_product.c
+++ tests/unit/igraph_product.c
@@ -1,5 +1,5 @@
 /*
-IGraph library.
+   IGraph library.
    Copyright (C) 2025  The igraph development team <igraph@igraph.org>
 
    This program is free software; you can redistribute it and/or modify
