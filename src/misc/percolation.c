126f6b6768725817c10b55ff7f9796a8f88e29bb
Zepeacedust <arnor0903@gmail.com>
2025-06-20 12:39:13 +0000

    Revert "Added vertex count to percolation and verified that edge references do not exceed it"
    This reverts commit 36a629f80dadbe968d2c715bce0dd4407283e383.

--- src/misc/percolation.c
+++ src/misc/percolation.c
@@ -37,23 +37,16 @@ static igraph_error_t igraph_i_percolate_edge(igraph_vector_int_t *links, igraph
     return IGRAPH_SUCCESS;
 }
 
-static igraph_error_t igraph_i_edge_list_percolation(const igraph_vector_int_t *edges, igraph_vector_int_t* output, igraph_integer_t v_count) {
+static igraph_error_t igraph_i_edge_list_percolation(const igraph_vector_int_t *edges, igraph_vector_int_t* output) {
     igraph_integer_t biggest = 0;
-
-    igraph_integer_t lower, upper;
-
-    igraph_vector_int_minmax(edges, &lower, &upper);
-
-    if (v_count < 0) v_count = upper;
-
-    if (lower < 0 || v_count < upper) IGRAPH_ERROR("Vertex count given is lower than highest referenced.",IGRAPH_EINVVID);
+    igraph_integer_t vert_count = igraph_vector_int_max(edges);
 
     igraph_vector_int_t sizes;
-    IGRAPH_CHECK(igraph_vector_int_init(&sizes, v_count));
+    IGRAPH_CHECK(igraph_vector_int_init(&sizes, vert_count));
     IGRAPH_FINALLY(igraph_vector_int_destroy, &sizes);
 
     igraph_vector_int_t links;
-    IGRAPH_CHECK(igraph_vector_int_init(&links, v_count));
+    IGRAPH_CHECK(igraph_vector_int_init(&links, vert_count));
     IGRAPH_FINALLY(igraph_vector_int_destroy, &links);
 
     int edge_count = igraph_vector_int_size(edges) / 2;
