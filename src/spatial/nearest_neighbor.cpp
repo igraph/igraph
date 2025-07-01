a0679f5d3a7e19b83037180914876ec3c28e9a9a
Zepeacedust <arnor0903@gmail.com>
2025-07-01 10:38:26 +0000

    add stub for function

--- src/spatial/nearest_neighbor.cpp
+++ src/spatial/nearest_neighbor.cpp
@@ -1,1 +1,8 @@
 
+#include "igraph_datatype.h"
+#include "igraph_error.h"
+#include "igraph_types.h"
+
+igraph_error_t igraph_nearest_neighbor_graph(igraph_t *graph, const igraph_matrix_real_t *points, igraph_integer_t dimensionality, igraph_metric_enum_t metric, igraph_real_t cutoff, igraph_integer_t number) {
+    return IGRAPH_SUCCESS;
+}
