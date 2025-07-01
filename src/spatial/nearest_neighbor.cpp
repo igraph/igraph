bb61c123b2ac3338bf602fe0d78d220308e31a93
Zepeacedust <arnor0903@gmail.com>
2025-07-01 17:32:02 +0000

    Start adding structure

--- src/spatial/nearest_neighbor.cpp
+++ src/spatial/nearest_neighbor.cpp
@@ -1,8 +1,59 @@
 
+#include "igraph_operators.h"
+#include "igraph_spatial.h"
 #include "igraph_datatype.h"
+#include "igraph_matrix.h"
 #include "igraph_error.h"
 #include "igraph_types.h"
+#include "igraph_vector.h"
+#include "nanoflann.hpp"
+#include <cstdint>
 
-igraph_error_t igraph_nearest_neighbor_graph(igraph_t *graph, const igraph_matrix_real_t *points, igraph_integer_t dimensionality, igraph_metric_enum_t metric, igraph_real_t cutoff, igraph_integer_t number) {
+class igraph_point_adaptor {
+    igraph_integer_t dimension;
+    const igraph_matrix_t *points;
+    size_t kdtree_get_point_count() const {
+        return igraph_matrix_nrow(points);
+    }
+    igraph_real_t kdtree_get_pt(const size_t idx, const size_t dim) const {
+        return 0;
+    }
+
+    // indicates that it should use default
+    bool kdtree_get_bbox(void* bb) {
+        return false;
+    }
+
+public:
+    igraph_point_adaptor(const igraph_matrix_t *points) {
+        this -> dimension = igraph_matrix_ncol(points);
+        this -> points = points;
+    }
+};
+
+class L2_igraph_adaptor {
+public:
+    using ElementType = igraph_real_t;
+    using DistanceType = nanoflann::metric_L2;
+
+} ;
+
+igraph_error_t igraph_nearest_neighbor_graph(igraph_t *graph,
+        const igraph_matrix_t *points,
+        igraph_metric_t metric,
+        igraph_real_t cutoff,
+        igraph_integer_t number) {
+
+    igraph_integer_t dimensionality = igraph_matrix_ncol(points);
+
+    igraph_point_adaptor adaptor(points);
+
+    using my_tree = nanoflann::KDTreeSingleIndexAdaptor<L2_igraph_adaptor, igraph_point_adaptor, 0, uint32_t>;
+
+    my_tree tree(dimensionality, adaptor, 10);
+
+
+    tree.buildIndex();
+
     return IGRAPH_SUCCESS;
 }
