3b2c587ab954f4cf73bfeca0b29fe4dee5a091d1
Zepeacedust <arnor0903@gmail.com>
2025-07-02 13:49:21 +0000

    Add more infrastructure, potential callbacks to construct graph

--- src/spatial/nearest_neighbor.cpp
+++ src/spatial/nearest_neighbor.cpp
@@ -1,11 +1,15 @@
 
+#include "cliques/cliquer/graph.h"
+#include "igraph_operators.h"
 #include "igraph_spatial.h"
 #include "igraph_datatype.h"
 #include "igraph_matrix.h"
 #include "igraph_error.h"
 #include "igraph_types.h"
 #include "nanoflann/nanoflann.hpp"
+#include <cstddef>
 #include <cstdint>
+#include <string.h>
 
 class igraph_point_adaptor {
     igraph_integer_t dimension;
@@ -36,6 +40,24 @@ public:
 
 } ;
 
+class GraphBuildingResultSet {
+    private:
+        igraph_integer_t current_vertex = 0;
+        igraph_t *graph;
+    public:
+        using DistanceType = igraph_real_t;
+        GraphBuildingResultSet(igraph_t * graph, igraph_integer_t neighbors, igraph_real_t distance) {
+           this->graph = graph;
+        }
+        bool addPoint(igraph_real_t distance, igraph_integer_t index) {
+            igraph_add_edge(graph, current_vertex, index);
+            return true;
+        }
+        void select_vertex(igraph_integer_t i) {
+            current_vertex = i;
+        }
+};
+
 extern "C" igraph_error_t igraph_nearest_neighbor_graph(igraph_t *graph,
         const igraph_matrix_t *points,
         igraph_metric_t metric,
@@ -46,12 +68,28 @@ extern "C" igraph_error_t igraph_nearest_neighbor_graph(igraph_t *graph,
 
     igraph_point_adaptor adaptor(points);
 
+    igraph_integer_t point_count = igraph_matrix_nrow(points);
+
     using my_tree = nanoflann::KDTreeSingleIndexAdaptor<L2_igraph_adaptor, igraph_point_adaptor, 0, uint32_t>;
 
     my_tree tree(dimensionality, adaptor, 10);
 
-
     tree.buildIndex();
+
+    igraph_vector_t current_point;
+    IGRAPH_VECTOR_INIT_FINALLY(&current_point, 0);
+
+    GraphBuildingResultSet results(graph, 2, 100);
+
+
+    for (igraph_integer_t i = 0; i < point_count; i++) {
+        IGRAPH_CHECK(igraph_matrix_get_row(points, &current_point, i));
+        results.select_vertex(i);
+        tree.findNeighbors(results, VECTOR(current_point), nanoflann::SearchParameters(0, false));
+    }
+
+    igraph_vector_destroy(&current_point);
+    IGRAPH_FINALLY_CLEAN(1);
 
     return IGRAPH_SUCCESS;
 }
