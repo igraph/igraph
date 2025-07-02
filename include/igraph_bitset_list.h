0743a7b17772a0841cbea8646f2d3657220489f0
Szabolcs Horvát <szhorvat@gmail.com>
2025-07-02 18:01:12 +0000

    chore: clean up copyright headers in public headers

--- include/igraph_bitset_list.h
+++ include/igraph_bitset_list.h
@@ -1,6 +1,6 @@
 /*
    IGraph library.
-   Copyright (C) 2024-2025  The igraph development team
+   Copyright (C) 2024-2025  The igraph development team <igraph@igraph.org>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -13,9 +13,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc.,  51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301 USA
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #ifndef IGRAPH_BITSET_LIST_H
