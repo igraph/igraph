a7909740615e49713f0da6a27f7ccf30cc52ab32
Szabolcs Horvát <szhorvat@gmail.com>
2025-07-06 10:14:25 +0000

    cleanup

--- src/spatial/nearest_neighbor.cpp
+++ src/spatial/nearest_neighbor.cpp
@@ -1,18 +1,36 @@
-#include "igraph_interface.h"
-#include "igraph_operators.h"
+/*
+   IGraph library.
+   Copyright (C) 2025  The igraph development team <igraph@igraph.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "igraph_spatial.h"
+
+#include "igraph_interface.h"
 #include "igraph_matrix.h"
 #include "igraph_error.h"
 #include "igraph_types.h"
 #include "igraph_vector.h"
+
 #include "nanoflann/nanoflann.hpp"
-#include <cstddef>
-#include <cstdint>
-#include <string.h>
+
 
 class igraph_point_adaptor {
     igraph_integer_t dimension;
     const igraph_matrix_t *points;
+
 public:
     size_t kdtree_get_point_count() const {
         return igraph_matrix_nrow(points);
@@ -48,8 +66,8 @@ class GraphBuildingResultSet {
 private:
     igraph_integer_t current_vertex = 0;
     igraph_integer_t current_added = 0;
-    igraph_integer_t max_neighbors;
     igraph_real_t max_distance;
+    igraph_integer_t max_neighbors;
     igraph_vector_int_t edges;
     igraph_t *graph;
 public:
@@ -57,10 +75,12 @@ public:
     GraphBuildingResultSet(igraph_t * graph, igraph_integer_t neighbors, igraph_real_t distance) : max_distance(distance), max_neighbors(neighbors) {
         this->graph = graph;
     }
+
     igraph_error_t initialize() {
         IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
         return IGRAPH_SUCCESS;
     }
+
     bool addPoint(igraph_real_t distance, igraph_integer_t index) {
         if (index == current_vertex) {
             return true;
@@ -72,23 +92,27 @@ public:
         }
         return true;
     }
+
     ~GraphBuildingResultSet() {
         igraph_vector_int_destroy(&edges);
         IGRAPH_FINALLY_CLEAN(1);
     }
+
     void select_vertex(igraph_integer_t i) {
         current_vertex = i;
         current_added = 0;
     }
-    void sort () {
 
-    }
+    void sort () { }
+
     bool full () {
         return false;
     }
+
     igraph_real_t worstDist() {
         return max_distance;
     }
+
     igraph_error_t dumpGraph() {
         IGRAPH_CHECK(igraph_add_edges(graph, &edges, NULL));
         return IGRAPH_SUCCESS;
