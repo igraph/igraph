612cfb5a2b93d9fa748dc4d40d4a3973f8e07d34
Szabolcs Horvát <szhorvat@gmail.com>
2025-07-30 13:10:24 +0800

    header cleanup

--- src/spatial/nearest_neighbor.cpp
+++ src/spatial/nearest_neighbor.cpp
@@ -16,12 +16,12 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "igraph_constructors.h"
-#include "igraph_interface.h"
 #include "igraph_spatial.h"
 
+#include "igraph_constructors.h"
 #include "igraph_conversion.h"
 #include "igraph_error.h"
+#include "igraph_interface.h"
 #include "igraph_matrix.h"
 #include "igraph_types.h"
 #include "igraph_vector.h"
