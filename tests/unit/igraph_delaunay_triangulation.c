b6db928ccb4723c37845c033060cd49fcb8b9f98
Zepeacedust <arnor0903@gmail.com>
2025-08-12 10:32:54 +0000

    Add header to tests. Add return value.

--- tests/unit/igraph_delaunay_triangulation.c
+++ tests/unit/igraph_delaunay_triangulation.c
@@ -1,3 +1,21 @@
+/*
+   IGraph library.
+   Copyright (C) 2025  The igraph development team <igraph@igraph.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <igraph.h>
 
 #include "test_utilities.h"
@@ -139,4 +157,6 @@ int main(void) {
     printf("4 points on a line\n");
     CHECK_ERROR(delaunay(&points_all_colinear[0], 4, 2, true),IGRAPH_EINVAL);
     VERIFY_FINALLY_STACK();
+
+    return 0;
 }
