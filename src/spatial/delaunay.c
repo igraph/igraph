f66016b9767acce59a5140433b18f818f1dd70f1
Zepeacedust <arnor0903@gmail.com>
2025-08-14 13:52:00 +0000

    Add explicit error on insufficient points

--- src/spatial/delaunay.c
+++ src/spatial/delaunay.c
@@ -285,6 +285,11 @@ igraph_error_t igraph_i_delaunay_edges(igraph_vector_int_t *edges, const igraph_
 igraph_error_t igraph_delaunay_graph(igraph_t *graph, const igraph_matrix_t *points) {
     igraph_vector_int_t edges;
     const igraph_integer_t numpoints = igraph_matrix_nrow(points);
+    const igraph_integer_t dims = igraph_matrix_ncol(points);
+
+    if (dims >= numpoints) {
+        IGRAPH_ERRORF("Not enough points to create simplex, need at least %" IGRAPH_PRId ".", IGRAPH_EINVAL, dims);
+    }
 
     IGRAPH_VECTOR_INT_INIT_FINALLY(&edges, 0);
     IGRAPH_CHECK(igraph_i_delaunay_edges(&edges, points));
