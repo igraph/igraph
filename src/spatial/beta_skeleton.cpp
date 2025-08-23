f7ad22931a6762802633934b7649a741c79d0f16
Szabolcs Horvát <szhorvat@gmail.com>
2025-08-23 13:57:43 +0000

    more debug print

--- src/spatial/beta_skeleton.cpp
+++ src/spatial/beta_skeleton.cpp
@@ -596,7 +596,7 @@ public:
         igraph_real_t beta = 2 * ap2 / denom;
 
         if (beta < 1 + tol) {
-            printf("XXX Dropping edge (%d, %d) with calculated beta of %g.\n", (int) ai, (int) bi, beta);
+            printf("XXX Dropping edge (%d, %d) with beta=%g, denom=%g, ab2=%g, ap2=%g, bp2=%g\n", (int) ai, (int) bi, beta, denom, ab2, ap2, bp2);
         }
 
         return beta < 1 + tol ? 0 : beta;
