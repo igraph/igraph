1ebd988a9f9cb74691f1b5674563889c34b424cf
tomasy-j <janczyk.tom@gmail.com>
2026-05-18 09:31:10 -0700

    update lattice layout examples

--- examples/simple/igraph_layout_lattice.c
+++ examples/simple/igraph_layout_lattice.c
@@ -19,17 +19,21 @@
 #include <igraph.h>
 
 int main(void) {
-
-    // igraph init
-    igraph_setup();
     igraph_matrix_t res_m;
     igraph_vector_int_t dimvector;
+    igraph_t graph;
+    igraph_vector_bool_t periodic;
 
-    // init matrix and set to 1 row and 1 col - 
-    // igraph_layout_square function will resize it anyway
+    /* Initialize the library. */
+    igraph_setup();
+
+    /*
+    Initialize matrix and set it to 1 row and 1 col -
+    igraph_layout_square function will resize it anyway
+    */
     igraph_matrix_init(&res_m, 1, 1);
 
-    // pick dimensions for the lattice
+    /* Pick dimensions for the lattice */
     igraph_vector_int_init(&dimvector, 4);
     VECTOR(dimvector)[0] = 4;
     VECTOR(dimvector)[1] = 3;
@@ -38,8 +42,27 @@ int main(void) {
 
     igraph_layout_square(NULL, &res_m, &dimvector);
 
+    IGRAPH_ASSERT(igraph_layout_square(NULL, &res_m, &dimvector) == IGRAPH_SUCCESS);
+    printf("Square lattice layout coordinates:\n");
+    igraph_matrix_print(&res_m);
+
+
+    /* Get layout coordinates on an initialized square lattice graph */ 
+    igraph_vector_bool_init(&periodic, 4);
+    igraph_vector_bool_fill(&periodic, false);
+    igraph_square_lattice(
+        &graph, &dimvector, /* nei= */ 0, IGRAPH_UNDIRECTED,
+        /* mutual= */ false, &periodic
+    );
+
+    IGRAPH_ASSERT(igraph_layout_square(&graph, &res_m, &dimvector) == IGRAPH_SUCCESS);
+    printf("Square lattice layout coordinates:\n");
+    igraph_matrix_print(&res_m);
+
     igraph_matrix_destroy(&res_m);
     igraph_vector_int_destroy(&dimvector);
+    igraph_vector_bool_destroy(&periodic);
+    igraph_destroy(&graph);
     return 0;
 }
 
